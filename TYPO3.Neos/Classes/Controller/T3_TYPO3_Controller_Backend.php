0f39e10156beb3f98efe48817e10c6f58dde065c
Robert Lemke <robert@typo3.org>
2008-02-13 18:07:30 +0000

    * FLOW3: Fixed whitespaces and inline comments where I came across * FLOW3: (MVC) Added two new exceptions: NoSuchController and InvalidController (for Dispatcher) * FLOW3: (MVC) Added an initial implementation of a router for web requests. Tests are still in the Web Request Builder test case but will be refactored soon. * TYPO3: A bit of cleanup and reviving of the Page controller * Testing: Changed "POST" to "post" in MainLayout.html. That fixes #264, but in general it's too early to look at errors of this kind because the new Testrunner will still change a lot. Original-Commit-Hash: 984b039f4a2a82d1cbee3e89a47c3b43f9752c3c

--- TYPO3.Neos/Classes/Controller/T3_TYPO3_Controller_Backend.php
+++ TYPO3.Neos/Classes/Controller/T3_TYPO3_Controller_Backend.php
@@ -15,42 +15,26 @@ declare(encoding = 'utf-8');
  *                                                                        */
 
 /**
- * TYPO3's frontend page controller
+ * The TYPO3 Backend controller
  *
  * @package   TYPO3
  * @version   $Id:T3_TYPO3_Controller_Page.php 262 2007-07-13 10:51:44Z robert $
  * @copyright Copyright belongs to the respective authorst
  * @license   http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
  */
-class T3_TYPO3_Controller_Page extends T3_FLOW3_MVC_Controller_ActionController {
-
-	/**
-	 * @var T3_TypoScript_ParserInterface
-	 */
-	protected $typoScriptParser;
+class T3_TYPO3_Controller_Backend extends T3_FLOW3_MVC_Controller_ActionController {
 
 	protected $supportedRequestTypes = array('T3_FLOW3_MVC_Web_Request');
 
 	/**
-	 * Initalizes the page controller
-	 *
-	 * @param T3_TypoScript_Parser
-	 */
-	public function setTypoScriptParser(T3_TypoScript_ParserInterface $typoScriptParser) {
-		$this->typoScriptParser = $typoScriptParser;
-		$this->typoScriptParser->setDefaultNamespace('T3_TYPO3_TypoScript');
-	}
-
-	/**
-	 * Processes a web- request and returns the rendered page as a response
+	 * The default action
 	 *
 	 * @param  T3_FLOW3_MVC_Web_Request $request: The request to process
 	 * @param  T3_FLOW3_MVC_Response $response: The response
 	 * @author Robert Lemke <robert@typo3.org>
 	 */
 	public function defaultAction() {
-		$this->view->typoScriptObjectTree = $this->typoScriptParser->parse(file_get_contents(dirname(__FILE__) . '/../../Tests/Fixtures/PreliminaryPageConfiguration.ts'));
-		$this->response->setContent($this->view->render());
+		$this->response->setContent('TYPO3 Backend');
 	}
 }
 ?>
--- TYPO3.Neos/Classes/View/T3_TYPO3_View_Default.php
+++ TYPO3.Neos/Classes/View/T3_TYPO3_View_Default.php
@@ -12,40 +12,41 @@ declare(encoding = 'utf-8');
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
  * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
  * Public License for more details.                                       *
- *                                                                        */ 
+ *                                                                        */
 
 /**
  * TypoScript View for a Page
- * 
- * @package		CMS
- * @version 	$Id:T3_TYPO3_View_Page.php 262 2007-07-13 10:51:44Z robert $
- * @copyright	Copyright belongs to the respective authorst
- * @license		http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
+ *
+ * @package   TYPO3
+ * @version   $Id:T3_TYPO3_View_Page.php 262 2007-07-13 10:51:44Z robert $
+ * @copyright Copyright belongs to the respective authorst
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
  */
-class T3_TYPO3_View_Page extends T3_FLOW3_MVC_View_Abstract {
+class T3_TYPO3_View_Default extends T3_FLOW3_MVC_View_Abstract {
+
+	/**
+	 * @var array
+	 */
+	public $typoScriptObjectTree;
 
 	/**
 	 * Renders a page from the given TypoScript
 	 *
-	 * @param  array				$typoScriptObjectTree: The TypoScript tree (model)
-	 * @return string				The rendered content
+	 * @param  array $typoScriptObjectTree: The TypoScript tree (model)
+	 * @return string The rendered content
 	 * @author Robert Lemke <robert@typo3.org>
 	 */
 	public function render() {
-		throw new LogicException('Not properly implemented.');
-		
-		foreach ($typoScriptObjectTree as $name => $typoScriptObject) {
+		foreach ($this->typoScriptObjectTree as $name => $typoScriptObject) {
 			if ($typoScriptObject instanceof T3_TYPO3_TypoScript_Page) {
 				$typoScriptPageObjectName = $name;
 			}
 		}
 		if (!isset($typoScriptPageObjectName)) {
-			return 'Error: No TypoScript Page object has been defined for this view.';		
+			return 'Error: No TypoScript Page object has been defined for this view.';
 		} else {
-			return $typoScriptObjectTree[$typoScriptPageObjectName]->getRenderedContent();			
+			return $this->typoScriptObjectTree[$typoScriptPageObjectName]->getRenderedContent();
 		}
 	}
-
 }
-
 ?>
