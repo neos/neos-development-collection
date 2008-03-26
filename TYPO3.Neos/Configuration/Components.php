77cab92322fb03015e0a177c33ea25830cc74bbf
Robert Lemke <robert@typo3.org>
2008-03-26 18:07:32 +0000

    Global change: Component configurations file format is now .php and not .ini anymore!
    * FLOW3: Refactored the component configuration parsing and moved it to a dedicated configuration builder.
    * Adapted the Components.ini of all v5 dist packages.
    Original-Commit-Hash: 48ea78daf5a866c367e7532f67cfc4c41e0bda16

---
+++ TYPO3.Neos/Configuration/Components.php
@@ -0,0 +1,14 @@
+<?php
+declare(ENCODING="utf-8");
+
+/*                                                                        *
+ * Component Configuration for the TYPO3 Package                          *
+ *                                                                        */
+
+/**
+ * @package TYPO3
+ * @version $Id:$
+ */
+
+
+?>
