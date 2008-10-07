e9a14f142a47c23dcb7d9f2f293845bc287656fb
Bastian Waidelich <bastian@typo3.org>
2008-10-07 13:52:58 +0000

    * FLOW3: (MVC) Refactored and added tests for the Route Class. * FLOW3: (MVC) Renamed all occurrences of urlPattern to uriPattern in routing classes (we decided to stick with "URI" rather than "URL" from now on). * TYPO3CR: Updated Routes.php to set uriPattern instead of urlPattern. * TYPO3: Updated Routes.php to set uriPattern instead of urlPattern. Removed obsolete dynamic Route Part from fallback uriPattern. Original-Commit-Hash: b520d83bbb4df7cade0d4018f9167c18fff3e004

--- TYPO3.TYPO3CR/Configuration/Routes.php
+++ TYPO3.TYPO3CR/Configuration/Routes.php
@@ -7,7 +7,7 @@ declare(ENCODING="utf-8");
  *                                                                        */
 
 $c->TYPO3CRAdmin
-	->setUrlPattern('typo3cr/[@controller]/[@action]')
+	->setUriPattern('typo3cr/[@controller]/[@action]')
 	->setControllerComponentNamePattern('F3::@package::Admin::Controller::@controllerController')
 	->setDefaults(
 		array(
