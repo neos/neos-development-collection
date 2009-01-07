eb8f3ad47947241df9829397f920ad9a7633e930
Karsten Dambekalns <karsten@typo3.org>
2009-01-07 12:30:04 +0000

    TYPO3CR: * changed license to LGPL v3 or later, fixes #2420 Original-Commit-Hash: d3e07e5e07399bac40fb07f59604553315355bad

--- TYPO3.TYPO3CR/Classes/FLOW3/Persistence/F3_TYPO3CR_FLOW3_Persistence_QueryFactory.php
+++ TYPO3.TYPO3CR/Classes/FLOW3/Persistence/F3_TYPO3CR_FLOW3_Persistence_QueryFactory.php
@@ -3,16 +3,23 @@ declare(ENCODING = 'utf-8');
 namespace F3\TYPO3CR\FLOW3\Persistence;
 
 /*                                                                        *
- * This script is part of the TYPO3 project - inspiring people to share!  *
+ * This script belongs to the FLOW3 package "TYPO3CR".                    *
  *                                                                        *
- * TYPO3 is free software; you can redistribute it and/or modify it under *
- * the terms of the GNU General Public License version 2 as published by  *
- * the Free Software Foundation.                                          *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License as published by the *
+ * Free Software Foundation, either version 3 of the License, or (at your *
+ * option) any later version.                                             *
  *                                                                        *
  * This script is distributed in the hope that it will be useful, but     *
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
- * Public License for more details.                                       *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/licenses/lgpl.html                      *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
 /**
@@ -27,7 +34,7 @@ namespace F3\TYPO3CR\FLOW3\Persistence;
  * @package TYPO3CR
  * @subpackage FLOW3
  * @version $Id$
- * @license http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser Public License, version 3 or later
  */
 class QueryFactory implements \F3\FLOW3\Persistence\QueryFactoryInterface {
 
