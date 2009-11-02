c0ecca1695d9735f62d0070bca9190fb99081b36
Karsten Dambekalns <karsten@typo3.org>
2009-11-02 15:45:58 +0000

    [~TASK] TYPO3CR: Added one more @api annotation at class level. Original-Commit-Hash: b4430bd4b9cb085aa46c24cfe50358e11c7e1e7a

--- TYPO3.TYPO3CR/Classes/Query/QOM/StaticOperand.php
+++ TYPO3.TYPO3CR/Classes/Query/QOM/StaticOperand.php
@@ -28,6 +28,7 @@ namespace F3\TYPO3CR\Query\QOM;
  *
  * @version $Id$
  * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ * @api
  * @scope prototype
  */
 class StaticOperand extends \F3\TYPO3CR\Query\QOM\Operand implements \F3\PHPCR\Query\QOM\StaticOperandInterface {
