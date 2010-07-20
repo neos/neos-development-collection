78ee076ae6daa981408f11b6be39ccbc3f7b824c
Sebastian Kurfürst <sebastian@typo3.org>
2010-07-20 08:28:35 +0000

    * Created a defaultSettings object which can hold default settings for everything. * Added a setting to enable/disable the top ribbon. Original-Commit-Hash: d42b12d04cff8dec4188b56ce49943f4832529a9

--- TYPO3.Neos/Resources/Public/Backend/Aloha/WebContent/core/core.js
+++ TYPO3.Neos/Resources/Public/Backend/Aloha/WebContent/core/core.js
@@ -72,6 +72,17 @@ GENTICS.Aloha.prototype.dictionaries = {};
 GENTICS.Aloha.prototype.settings = {};
 
 /**
+ * settings object containing the default values of all settings.
+ *
+ * The GENTICS.Aloha.settings object is merged on top of these default settings.
+ *
+ * @cfg {Object} object Aloha's default settings
+ */
+GENTICS.Aloha.prototype.defaultSettings = {
+	ribbonEnabled: true
+};
+
+/**
  * Initialize Aloha
  * called automatically by the loader
  * @hide
@@ -94,7 +105,12 @@ GENTICS.Aloha.prototype.init = function () {
 			that.activeEditable = null;
 		}
 	});
-	
+
+	// Merge user-defined settings onto the default settings.
+	var settings = this.defaultSettings;
+	jQuery.extend(settings, this.settings);
+	this.settings = settings;
+
 	// initialize the base path to the aloha files
 	if (typeof this.settings.base == 'undefined' || !this.settings.base) {
 		this.settings.base = GENTICS.Aloha.autobase;
@@ -121,9 +137,11 @@ GENTICS.Aloha.prototype.init = function () {
 	// initialize all plugins
 	this.PluginRegistry.init();
 	// TODO call init on all other Aloha Core objects (messageline, etc.)
-	
-	// intitialize the ribbon
-	this.Ribbon.init();
+
+	if (this.settings.ribbonEnabled) {
+		// intitialize the ribbon
+		this.Ribbon.init();
+	}
 
 	// initialize the floatingmenu
 	this.FloatingMenu.init();
