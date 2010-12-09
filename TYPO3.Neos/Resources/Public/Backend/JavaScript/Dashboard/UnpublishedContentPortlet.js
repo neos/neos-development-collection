b7ee198e872bb8cd8c20fb9b2d4c025ec2e6e960
Christopher Hlubek <hlubek@networkteam.com>
2010-12-09 12:46:59 +0100

    [~TASK] TYPO3 (UI): Extend dashboard and add workspace module
    Change-Id: Idb248f16efb7b708ee3ba8b6dd90e2c685feeff6
    Original-Commit-Hash: 3243213e64a45ee3f01fde587a2899f288c9a61d

--- TYPO3.Neos/Resources/Public/Backend/JavaScript/Dashboard/UnpublishedContentPortlet.js
+++ TYPO3.Neos/Resources/Public/Backend/JavaScript/Dashboard/UnpublishedContentPortlet.js
@@ -39,7 +39,6 @@ F3.TYPO3.Dashboard.UnpublishedContentPortlet = Ext.extend(Ext.ux.Portlet, {
 			tools: [{
 				id: 'gear',
 				handler: function(event) {
-					// console.log(arguments);
 					var menu = new Ext.menu.Menu({
 						// TODO re-use edit action
 						items: [{
@@ -62,6 +61,7 @@ F3.TYPO3.Dashboard.UnpublishedContentPortlet = Ext.extend(Ext.ux.Portlet, {
 			items: {
 				itemId: 'contentView',
 				xtype: 'F3.TYPO3.Dashboard.UnpublishedContentView',
+				ref: 'contentView',
 				height: 400
 			},bbar: [{
 				itemId: 'publishAll',
@@ -85,11 +85,85 @@ F3.TYPO3.Dashboard.UnpublishedContentPortlet = Ext.extend(Ext.ux.Portlet, {
 		Ext.apply(this, config);
 		F3.TYPO3.Dashboard.UnpublishedContentPortlet.superclass.initComponent.call(this);
 
-		F3.TYPO3.Login.LoginModule.on('updatedWorkspaceStatus', function(status) {
-			this.getComponent('contentView').store.load();
+			// Reload store if workspace status changed
+		F3.TYPO3.Workspace.WorkspaceModule.on('updatedWorkspaceStatus', function(status) {
+			if (status.changed) {
+				this.getComponent('contentView').store.load();
+			}
 		}, this);
 
-		this.getComponent('contentView').on('selectionchange', function(dataView, selections) {
+			// Create toolbar for hover over content
+		var fadeOutTimeout = null;
+		this.contentToolbar = new Ext.Panel({
+			items: [{
+				xtype: 'button',
+				text: 'Publish',
+				handler: function() {
+					var record;
+					if (this.contentToolbar.contentIndex !== null) {
+						record = this.contentView.store.getAt(this.contentToolbar.contentIndex);
+						// TODO Publish individual record
+					}
+				},
+				scope: this
+			}, {
+				xtype: 'button',
+				text: 'Discard',
+				handler: function() {
+					var record;
+					if (this.contentToolbar.contentIndex !== null) {
+						record = this.contentView.store.getAt(this.contentToolbar.contentIndex);
+						// TODO Discard individual record
+					}
+				},
+				scope: this
+			}],
+			layout: 'hbox',
+			layoutConfig: {
+				align: 'middle',
+				pack: 'end',
+				padding: '6'
+			},
+			width: 120,
+			height: 20,
+			floating: true,
+			border: false,
+			shadow: false,
+			renderTo: document.body,
+			hidden: true
+		});
+		this.contentToolbar.el.on('mouseenter', function() {
+			Ext.fly(this.contentToolbar.currentNode).addClass('x-view-over');
+			if (fadeOutTimeout) {
+				clearTimeout(fadeOutTimeout);
+			}
+		}, this);
+		this.contentToolbar.el.on('mouseleave', function() {
+			Ext.fly(this.contentToolbar.currentNode).removeClass('x-view-over');
+			if (fadeOutTimeout) {
+				fadeOutTimeout = setTimeout(this.contentToolbar.hide.createDelegate(this.contentToolbar), 50);
+			}
+		}, this);
+		this.contentView.on('mouseenter', function(view, index, node, event) {
+			if (fadeOutTimeout) {
+				clearTimeout(fadeOutTimeout);
+			}
+			var nodeEl = Ext.fly(node);
+			if (nodeEl) {
+				this.contentToolbar.contentIndex = index;
+					// FIXME Check if this is safe
+				this.contentToolbar.currentNode = node;
+				this.contentToolbar.show();
+				this.contentToolbar.setPosition(nodeEl.getRight() - (120 + 1), nodeEl.getTop() + 1);
+				this.contentToolbar.setHeight(nodeEl.getHeight() - 2);
+			}
+		}, this);
+		this.contentView.on('mouseleave', function() {
+			fadeOutTimeout = setTimeout(this.contentToolbar.hide.createDelegate(this.contentToolbar), 50);
+		}, this);
+
+			// Update button status when  selection changes
+		this.contentView.on('selectionchange', function(dataView, selections) {
 			if (selections.length > 0) {
 				this.getBottomToolbar().getComponent('publishAll').hide();
 				this.getBottomToolbar().getComponent('publishSelected').show();
@@ -101,13 +175,14 @@ F3.TYPO3.Dashboard.UnpublishedContentPortlet = Ext.extend(Ext.ux.Portlet, {
 	},
 
 	_publishAll: function() {
-		F3.TYPO3.Login.Service.publishWorkspace(function() {
+		F3.TYPO3.Workspace.Service.publishUserWorkspace(function() {
 			this.getComponent('contentView').store.load();
 		}, this);
 	},
 
 	_publishSelected: function() {
-		console.log('Not implemented...');
+		var records = this.contentView.getSelectedRecords();
+		// console.log('Not implemented...');
 	}
 
 });
--- TYPO3.Neos/Resources/Public/Backend/JavaScript/Login/Service.js
+++ TYPO3.Neos/Resources/Public/Backend/JavaScript/Login/Service.js
@@ -12,39 +12,6 @@ Ext.ns("F3.TYPO3.Login");
 F3.TYPO3.Login.Service = Ext.apply(new Ext.util.Observable, {
 
 	/**
-	 * Publishes the current user workspace
-	 *
-	 * @return {void}
-	 */
-	publishWorkspace: function(callback, scope) {
-		F3.TYPO3_Service_ExtDirect_V1_Controller_WorkspaceController.publish(F3.TYPO3.Configuration.Application.workspaceName, 'live', function(result) {
-			F3.TYPO3.Login.Service.updateWorkspaceStatus();
-			F3.TYPO3.Login.LoginModule.fireEvent('publishedWorkspace');
-			if (Ext.isFunction(callback)) callback.call(scope, result);
-		});
-	},
-
-	/**
-	 * Returns the status of the current workspace
-	 *
-	 * @return {}
-	 */
-	updateWorkspaceStatus: function(callback, scope) {
-		F3.TYPO3_Service_ExtDirect_V1_Controller_WorkspaceController.getStatus(F3.TYPO3.Configuration.Application.workspaceName, function(result) {
-			var status = result.data;
-				// TODO Move the saved nodeCount to some context
-			if (!F3.TYPO3.Login.LoginModule.nodeCount || F3.TYPO3.Login.LoginModule.nodeCount != status.nodeCount) {
-				F3.TYPO3.Login.LoginModule.nodeCount = status.nodeCount;
-				status.changed = true;
-			} else {
-				status.changed = false;
-			}
-			F3.TYPO3.Login.LoginModule.fireEvent('updatedWorkspaceStatus', status);
-			if (Ext.isFunction(callback)) callback.call(scope, result);
-		});
-	},
-
-	/**
 	 * Logout the current user
 	 *
 	 * @return {void}
