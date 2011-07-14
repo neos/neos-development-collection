3bb31c6a8f9f9335982fc850f7c1b19891cbd226
Sebastian Kurfuerst <sebastian@typo3.org>
2011-07-14 15:00:17 +0200

    [FEATURE] (UI): Adding jQuery UI
    Change-Id: Ib73c4f11b8330129b09f7954dc51508d86a312b6
    Original-Commit-Hash: 7a9d7be5754cfabe84e6e7e0a9b2ba197a4246b3

--- TYPO3.Neos/Resources/Public/JavaScript/contentmodule-main.js
+++ TYPO3.Neos/Resources/Public/JavaScript/contentmodule-main.js
@@ -3,11 +3,14 @@ require({
 		'aloha': Aloha.settings.basePath + 'aloha',
 		'util': Aloha.settings.basePath + 'util',
 		'vendor': Aloha.settings.basePath + 'vendor',
-		'dep': Aloha.settings.basePath + 'dep'
+		'dep': Aloha.settings.basePath + 'dep',
+		'Library': Aloha.settings.basePath + '../../../'
 	}
 },
 [
 	'order!vendor/jquery-1.6.1',
+	'order!Library/jquery-ui/js/jquery-ui-1.8.14.custom.min',
+	'css!Library/jquery-ui/css/ui-darkness/jquery-ui-1.8.14.custom.css',
 	'order!libs/sproutcore',
 	'order!aloha',
 	'order!phoenix/contentmodule'],
---
+++ TYPO3.Neos/Resources/Public/Library/README.txt
@@ -0,0 +1,6 @@
+jQuery UI: version 1.8.14
+- UI Core: all
+- Interactions: all
+- Widgets: all
+- Effects: core, fade, highlight
+Theme: UI Darkness
