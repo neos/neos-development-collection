1b47ad0c8921d7b56ec797a3b8efafd4c0bf6045
Karsten Dambekalns <karsten@typo3.org>
2011-09-29 11:45:44 +0200

    [TASK] Simplify license header in PHP files
    Change-Id: Ic2ebee33d4ce51c50a82f41254591d4642afd8a8
    Related: #29415
    Original-Commit-Hash: db8b7ed64f9205ece93b141835a748f97f030cb3

--- TYPO3.TypoScript/Tests/Unit/Fixtures/Text.php
+++ TYPO3.TypoScript/Tests/Unit/Fixtures/Text.php
@@ -5,18 +5,8 @@ namespace TYPO3\TypoScript\Fixtures;
  * This script belongs to the FLOW3 package "TypoScript"                  *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU General Public License as published by the Free   *
- * Software Foundation, either version 3 of the License, or (at your      *
- * option) any later version.                                             *
- *                                                                        *
- * This script is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
- * Public License for more details.                                       *
- *                                                                        *
- * You should have received a copy of the GNU General Public License      *
- * along with the script.                                                 *
- * If not, see http://www.gnu.org/licenses/gpl.html                       *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
