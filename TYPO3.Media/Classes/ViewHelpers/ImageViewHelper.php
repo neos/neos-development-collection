95083f4f3027a6f5cfa2d5241b6c34cc11abe3b8
Adrian Föder <adrian@foeder.de>
2012-03-07 11:30:38 +0100

    [FEATURE] Image ViewHelper
    Provide Image ViewHelpers that are able to render <img>
    tags and image paths to possibly resized variants of the
    given Image object.
    Usage:
    <m:image image="{imageObject}" maximumWidth="80" alt="sample" />
    {m:uri.image(image: imageObject, maximumWidth: 80)}
    
    Change-Id: I4b99540de9b9e60bf41c86cb12e3dd7339146f97
    Resolves: #33413
    Original-Commit-Hash: 136d7707c243619d0028be97be653eb679d74487

---
+++ TYPO3.Media/Classes/ViewHelpers/ImageViewHelper.php
@@ -0,0 +1,122 @@
+<?php
+namespace TYPO3\Media\ViewHelpers;
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3.Media".                *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * Renders an <img> HTML tag from a given TYPO3.Media's Image instance
+ *
+ * = Examples =
+ *
+ * <code title="Rendering an image as-is">
+ * <m:image image="{imageObject}" alt="a sample image without scaling" />
+ * </code>
+ * <output>
+ * (depending on the image, no scaling applied)
+ * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="120" height="180" alt="a sample image without scaling" />
+ * </output>
+ *
+ *
+ * <code title="Rendering an image with scaling at a given width only">
+ * <m:image image="{imageObject}" maximumWidth="80" alt="sample" />
+ * </code>
+ * <output>
+ * (depending on the image; scaled down to a maximum width of 80 pixels, keeping the aspect ratio)
+ * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="80" height="120" alt="sample" />
+ * </output>
+ *
+ *
+ * <code title="Rendering an image with scaling at given width and height, keeping aspect ratio">
+ * <m:image image="{imageObject}" maximumWidth="80" maximumHeight="80" alt="sample" />
+ * </code>
+ * <output>
+ * (depending on the image; scaled down to a maximum width and height of 80 pixels, keeping the aspect ratio)
+ * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="53" height="80" alt="sample" />
+ * </output>
+ *
+ *
+ * <code title="Rendering an image with crop-scaling at given width and height">
+ * <m:image image="{imageObject}" maximumWidth="80" maximumHeight="80" allowCropping="true" alt="sample" />
+ * </code>
+ * <output>
+ * (depending on the image; scaled down to a width and height of 80 pixels, possibly changing aspect ratio)
+ * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="80" height="80" alt="sample" />
+ * </output>
+ *
+ * <code title="Rendering an image with allowed up-scaling at given width and height">
+ * <m:image image="{imageObject}" maximumWidth="5000" allowUpScaling="true" alt="sample" />
+ * </code>
+ * <output>
+ * (depending on the image; scaled up or down to a width 5000 pixels, keeping aspect ratio)
+ * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="80" height="80" alt="sample" />
+ * </output>
+ *
+ */
+class ImageViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper {
+
+	/**
+	 * @var \TYPO3\FLOW3\Resource\Publishing\ResourcePublisher
+	 * @FLOW3\Inject
+	 */
+	protected $resourcePublisher;
+
+	/**
+	 * name of the tag to be created by this view helper
+	 *
+	 * @var string
+	 */
+	protected $tagName = 'img';
+
+	/**
+	 * @return void
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerUniversalTagAttributes();
+		$this->registerTagAttribute('alt', 'string', 'Specifies an alternate text for an image', TRUE);
+		$this->registerTagAttribute('ismap', 'string', 'Specifies an image as a server-side image-map. Rarely used. Look at usemap instead', FALSE);
+		$this->registerTagAttribute('usemap', 'string', 'Specifies an image as a client-side image-map', FALSE);
+	}
+
+
+	/**
+	 * Renders an HTML tag from a given image.
+	 *
+	 * @param \TYPO3\Media\Domain\Model\Image $image If specified, this resource object is used instead of the path and package information
+	 * @param integer $maximumWidth Desired maximum height of the image
+	 * @param integer $maximumHeight Desired maximum width of the image
+	 * @param boolean $allowCropping Whether the image should be cropped if the given sizes would hurt the aspect ratio
+	 * @param boolean $allowUpScaling Whether the resulting image size might exceed the size of the original image
+	 *
+	 * @return string an <img...> html tag
+	 */
+	public function render(\TYPO3\Media\Domain\Model\Image $image = NULL, $maximumWidth = NULL, $maximumHeight = NULL, $allowCropping = FALSE, $allowUpScaling = FALSE) {
+		$ratioMode = ($allowCropping ? \TYPO3\Media\Domain\Model\ImageInterface::RATIOMODE_OUTBOUND : \TYPO3\Media\Domain\Model\ImageInterface::RATIOMODE_INSET);
+		if ($allowUpScaling !== TRUE && $maximumWidth > $image->getWidth()) {
+			$maximumWidth = $image->getWidth();
+		}
+		if ($allowUpScaling !== TRUE && $maximumHeight > $image->getHeight()) {
+			$maximumHeight = $image->getHeight();
+		}
+		$thumbnailImageVariant = $image->getThumbnail($maximumWidth, $maximumHeight, $ratioMode);
+
+		$this->tag->addAttributes(array(
+			'width' => $thumbnailImageVariant->getWidth(),
+			'height' => $thumbnailImageVariant->getHeight(),
+			'src' => $this->resourcePublisher->getPersistentResourceWebUri($thumbnailImageVariant->getResource()),
+		));
+
+		return $this->tag->render();
+	}
+
+}
+?>
---
+++ TYPO3.Media/Classes/ViewHelpers/Uri/ImageViewHelper.php
@@ -0,0 +1,71 @@
+<?php
+namespace TYPO3\Media\ViewHelpers\Uri;
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3.Media".                *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ *  of the License, or (at your option) any later version.                *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * Renders the src path of a thumbnail image of a given TYPO3.Media Image instance
+ *
+ * = Examples =
+ *
+ * <code title="Rendering an image path as-is">
+ * {m:uri.image(image: imageObject)}
+ * </code>
+ * <output>
+ * (depending on the image)
+ * _Resources/Persistent/b29[...]95d.jpeg
+ * </output>
+ *
+ *
+ * <code title="Rendering an image path with scaling at a given width only">
+ * {m:uri.image(image: imageObject, maximumWidth: 80)}
+ * </code>
+ * <output>
+ * (depending on the image; has scaled keeping the aspect ratio)
+ * _Resources/Persistent/b29[...]95d.jpeg
+ * </output>
+ *
+ * @see \TYPO3\Media\ViewHelpers\ImageViewHelper
+ */
+class ImageViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @var \TYPO3\FLOW3\Resource\Publishing\ResourcePublisher
+	 * @FLOW3\Inject
+	 */
+	protected $resourcePublisher;
+
+	/**
+	 * Renders the path to a thumbnail image, created from a given image.
+	 *
+	 * @param \TYPO3\Media\Domain\Model\Image $image If specified, this resource object is used instead of the path and package information
+	 * @param integer $maximumWidth Desired maximum height of the image
+	 * @param integer $maximumHeight Desired maximum width of the image
+	 * @param boolean $allowCropping Whether the image should be cropped if the given sizes would hurt the aspect ratio
+	 * @param boolean $allowUpScaling Whether the resulting image size might exceed the size of the original image
+	 *
+	 * @return string the relative image path, to be used as src attribute for <img /> tags
+	 */
+	public function render(\TYPO3\Media\Domain\Model\Image $image = NULL, $maximumWidth = NULL, $maximumHeight = NULL, $allowCropping = FALSE, $allowUpScaling = FALSE) {
+		$ratioMode = ($allowCropping ? \TYPO3\Media\Domain\Model\ImageInterface::RATIOMODE_OUTBOUND : \TYPO3\Media\Domain\Model\ImageInterface::RATIOMODE_INSET);
+		if ($allowUpScaling !== TRUE && $maximumWidth > $image->getWidth()) {
+			$maximumWidth = $image->getWidth();
+		}
+		if ($allowUpScaling !== TRUE && $maximumHeight > $image->getHeight()) {
+			$maximumHeight = $image->getHeight();
+		}
+		$thumbnailImageVariant = $image->getThumbnail($maximumWidth, $maximumHeight, $ratioMode);
+		return $this->resourcePublisher->getPersistentResourceWebUri($thumbnailImageVariant->getResource());
+	}
+
+}
+?>
