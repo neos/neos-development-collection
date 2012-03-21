293a8d5b8f6a2be49db11926e4b93816078cc8e2
Rens Admiraal <rens.admiraal@typo3.org>
2012-03-22 00:16:24 +0100

    [TASK] Move backend 'Container' to ViewHelper
    Simplifies the way of adding the Phoenix container to a website.
    
    Related: PHX-44
    
    Change-Id: Ic2b4d6dffea45e2388e46d83aa539aad87c394ab
    Original-Commit-Hash: a7717635a193a290090ffbefa90090db8e2da301

---
+++ TYPO3.Neos/Classes/ViewHelpers/Backend/ContainerViewHelper.php
@@ -0,0 +1,49 @@
+<?php
+namespace TYPO3\TYPO3\ViewHelpers\Backend;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3".                      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * ViewHelper for the backend 'container'. Renders the required HTML to integrate
+ * the Phoenix backend into a website.
+ */
+class ContainerViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @var array
+	 */
+	protected $settings;
+
+	/**
+	 * @param array $settings
+	 */
+	public function injectSettings(array $settings) {
+		$this->settings = $settings;
+	}
+
+	/**
+	 * @param \TYPO3\TYPO3CR\Domain\Model\NodeInterface $node
+	 */
+	public function render(\TYPO3\TYPO3CR\Domain\Model\NodeInterface $node) {
+		$view = new \TYPO3\Fluid\View\StandaloneView($this->controllerContext->getRequest());
+		$view->setTemplatePathAndFilename('resource://TYPO3.TYPO3/Private/Templates/Backend/Content/Container.html');
+
+		$view->assignMultiple(array(
+			'node' => $node,
+			'modules' => $this->settings['modules']
+		));
+
+		return $view->render();
+	}
+}
+?>
