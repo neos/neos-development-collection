eba69d524dc40ec488e2189555e864b729a6936e
Rens Admiraal <rens.admiraal@typo3.org>
2012-03-23 11:28:12 +0100

    [CLEANUP] Use the same license block in all classes
    This change makes sure the same license block is used
    in all classes
    
    Change-Id: I5203215f048c7cbc4b6f598038e52b4b8fff10c9
    Original-Commit-Hash: c7428ff0ae63211f8c949a07323aefceffeda8c4

--- TYPO3.Neos/Classes/Controller/Module/StandardController.php
+++ TYPO3.Neos/Classes/Controller/Module/StandardController.php
@@ -2,7 +2,7 @@
 namespace TYPO3\TYPO3\Controller\Module;
 
 /*                                                                        *
- * This script belongs to the FLOW3 package "TYPO3".                      *
+ * This script belongs to the FLOW3 package "TYPO3.TYPO3".                *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU General Public License, either version 3 of the   *
--- TYPO3.Neos/Tests/Functional/Controller/Backend/BackendControllerSecurityTest.php
+++ TYPO3.Neos/Tests/Functional/Controller/Backend/BackendControllerSecurityTest.php
@@ -2,11 +2,11 @@
 namespace TYPO3\TYPO3\Tests\Functional\Controller\Backend;
 
 /*                                                                        *
- * This script belongs to the FLOW3 package "TYPO3".                      *
+ * This script belongs to the FLOW3 package "TYPO3.TYPO3".                *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU Lesser General Public License, either version 3   *
- *  of the License, or (at your option) any later version.                *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
