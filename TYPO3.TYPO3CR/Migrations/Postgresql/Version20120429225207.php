fcf80ad1fb01aec91d4c1b421c188c7d2d2c1f12
Karsten Dambekalns <karsten@typo3.org>
2012-04-30 10:32:05 +0200

    [TASK] Add migrations to create unique indexes
    Identity annotations now create an unique index, this change
    adds a migration to adjust the database schema accordingly.
    
    Change-Id: I00c9b8c86c6002f9f64c3ab77a5e3f2fc1766d29
    Related: #29547
    Original-Commit-Hash: 69d54c03400a8b4ea0f08892c0c4bdcca932c52c

---
+++ TYPO3.TYPO3CR/Migrations/Postgresql/Version20120429225207.php
@@ -0,0 +1,33 @@
+<?php
+namespace TYPO3\FLOW3\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Create unique indexes for identity properties
+ */
+class Version20120429225207 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql");
+
+		$this->addSql("CREATE UNIQUE INDEX flow3_identity_typo3_typo3cr_domain_model_workspace ON typo3_typo3cr_domain_model_workspace (name)");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql");
+
+		$this->addSql("DROP INDEX flow3_identity_typo3_typo3cr_domain_model_workspace");
+	}
+}
+
+?>
