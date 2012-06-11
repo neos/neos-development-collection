c26887cf17cf150fb78fdc06b7daaf5d2d1347c1
Christian Müller <christian@kitsunet.de>
2012-06-11 15:48:11 +0200

    [TASK] Initial Commit
    First working version of the site kickstarter.
    
    Change-Id: Ia2e2eb0112a85119b2f6a1b340052d3cd1267b33
    Original-Commit-Hash: cb18a693e5e808c410f5b83c83544b321c46a1bb

---
+++ TYPO3.Neos.Kickstarter/Classes/Command/SiteCommandController.php
@@ -0,0 +1,64 @@
+<?php
+namespace TYPO3\SiteKickstarter\Command;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "SiteKickstarter".            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use \TYPO3\FLOW3\Annotations as FLOW3;
+use \TYPO3\FLOW3\Utility\Files as Files;
+
+/**
+ * Command controller for the Kickstart generator
+ *
+ */
+class SiteCommandController extends \TYPO3\FLOW3\Cli\CommandController {
+
+	/**
+	 * @var \TYPO3\FLOW3\Package\PackageManagerInterface
+	 * @FLOW3\Inject
+	 */
+	protected $packageManager;
+
+	/**
+	 * @var \TYPO3\SiteKickstarter\Service\GeneratorService
+	 * @FLOW3\Inject
+	 */
+	protected $generatorService;
+
+	/**
+	 * Kickstart a new site package
+	 *
+	 * This command generates a new site package with basic TypoScript and Sites.xml
+	 *
+	 * @param string $packageKey The packageKey for your site
+	 * @param string $siteName The siteName of your site
+	 * @return string
+	 */
+	public function kickstartCommand($packageKey, $siteName) {
+		if (!$this->packageManager->isPackageKeyValid($packageKey)) {
+			$this->outputLine('Package key "%s" is not valid. Only UpperCamelCase with alphanumeric characters and underscore, please!', array($packageKey));
+			$this->quit(1);
+		}
+
+		if ($this->packageManager->isPackageAvailable($packageKey)) {
+			$this->outputLine('Package "%s" already exists.', array($packageKey));
+			$this->quit(2);
+		}
+		$this->packageManager->createPackage($packageKey, NULL, Files::getUnixStylePath(Files::concatenatePaths(array(FLOW3_PATH_PACKAGES, 'Sites'))));
+
+		$generatedFiles = array();
+		$generatedFiles += $this->generatorService->generateSitesXml($packageKey, $siteName);
+		$generatedFiles += $this->generatorService->generateSitesTypoScript($packageKey, $siteName);
+		$generatedFiles += $this->generatorService->generateSitesTemplate($packageKey, $siteName);
+		$this->outputLine(implode(PHP_EOL, $generatedFiles));
+	}
+}
+
+?>
---
+++ TYPO3.Neos.Kickstarter/Classes/Service/GeneratorService.php
@@ -0,0 +1,78 @@
+<?php
+namespace TYPO3\SiteKickstarter\Service;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "SiteKickstarter".            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * Service to generate site packages
+ *
+ */
+class GeneratorService extends \TYPO3\Kickstart\Service\GeneratorService {
+
+	public function generateSitesXml($packageKey, $siteName) {
+
+		$templatePathAndFilename = 'resource://TYPO3.SiteKickstarter/Private/Generator/Content/Sites.xml';
+
+		$contextVariables = array();
+		$contextVariables['packageKey'] = $packageKey;
+		$contextVariables['siteName'] = $siteName;
+		$packageKeyDomainPart = substr(strrchr($packageKey, '.'), 1);
+		$contextVariables['siteNodeName'] = strtolower($packageKeyDomainPart);
+
+		$fileContent = $this->renderTemplate($templatePathAndFilename, $contextVariables);
+
+		$sitesXmlPathAndFilename = $this->packageManager->getPackage($packageKey)->getResourcesPath() . 'Private/Content/Sites.xml';
+		$this->generateFile($sitesXmlPathAndFilename, $fileContent);
+
+		return $this->generatedFiles;
+	}
+
+	public function generateSitesTypoScript($packageKey, $siteName) {
+
+		$templatePathAndFilename = 'resource://TYPO3.SiteKickstarter/Private/Generator/TypoScripts/Root.ts2';
+
+		$contextVariables = array();
+		$contextVariables['packageKey'] = $packageKey;
+		$contextVariables['siteName'] = $siteName;
+		$packageKeyDomainPart = substr(strrchr($packageKey, '.'), 1);
+		$contextVariables['siteNodeName'] = $packageKeyDomainPart;
+
+		$fileContent = $this->renderTemplate($templatePathAndFilename, $contextVariables);
+
+		$sitesTypoScriptPathAndFilename = $this->packageManager->getPackage($packageKey)->getResourcesPath() . 'Private/TypoScripts/Root.ts2';
+		$this->generateFile($sitesTypoScriptPathAndFilename, $fileContent);
+
+		return $this->generatedFiles;
+	}
+
+	public function generateSitesTemplate($packageKey, $siteName) {
+
+		$templatePathAndFilename = 'resource://TYPO3.SiteKickstarter/Private/Generator/Template/SiteTemplate.html';
+
+		$contextVariables = array();
+		$contextVariables['siteName'] = $siteName;
+		$contextVariables['typo3ViewHelper'] = '{namespace typo3=TYPO3\TYPO3\ViewHelpers}';
+		$contextVariables['typoScriptViewHelper'] = '{namespace ts=TYPO3\TypoScript\ViewHelpers}';
+		$packageKeyDomainPart = substr(strrchr($packageKey, '.'), 1);
+		$contextVariables['siteNodeName'] = lcfirst($packageKeyDomainPart);
+
+		$fileContent = $this->renderTemplate($templatePathAndFilename, $contextVariables);
+
+		$sitesTypoScriptPathAndFilename = $this->packageManager->getPackage($packageKey)->getResourcesPath() . 'Private/Templates/Page/Default.html';
+		$this->generateFile($sitesTypoScriptPathAndFilename, $fileContent);
+
+		return $this->generatedFiles;
+	}
+}
+
+?>
