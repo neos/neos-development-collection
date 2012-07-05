c0e733936486a265d59f52f7de5bb2c9a42d05c1
Sebastian Kurfuerst <sebastian@typo3.org>
2012-07-05 18:58:14 +0200

    [FEATURE] Implement TypoScriptView
    The TypoScript view can be used as replacement of Fluid TemplateView in standard FLOW3 applications.
    
    Resolves: #38630
    Change-Id: Iae51179fd9a374b5e835049040b58374dd647cc2
    Original-Commit-Hash: ba60d198a4658e59b21b4af53a442a37ef00853e

---
+++ TYPO3.TypoScript/Tests/Functional/View/TypoScriptViewTest.php
@@ -0,0 +1,81 @@
+<?php
+namespace TYPO3\TypoScript\Tests\Functional\View;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3.TypoScript".           *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ * Testcase for the TypoScript View
+ *
+ */
+class TypoScriptViewTest extends \TYPO3\FLOW3\Tests\FunctionalTestCase {
+
+	/**
+	 * @var TYPO3\FLOW3\Mvc\View\ViewInterface
+	 */
+	protected $mockFallbackView;
+
+	/**
+	 * @var \TYPO3\FLOW3\Mvc\Controller\ControllerContext
+	 */
+	protected $mockControllerContext;
+
+	/**
+	 * Initializer
+	 */
+	public function setUp() {
+		$this->mockFallbackView = $this->getMock('TYPO3\FLOW3\Mvc\View\ViewInterface');
+	}
+
+	/**
+	 * @test
+	 */
+	public function typoScriptViewIsUsedForRendering() {
+		$view = $this->buildView('Foo\Bar\Controller\TestController', 'index');
+		$this->assertEquals('TestRenderer called', $view->render());
+	}
+
+	/**
+	 * @test
+	 */
+	public function ifNoTypoScriptViewIsFoundThenFallbackViewIsExecuted() {
+		$view = $this->buildView('Foo\Bar\Controller\TestController', 'nonExisting');
+		$this->mockFallbackView->expects($this->once())->method('render')->will($this->returnValue('FallbackView called'));
+		$this->mockFallbackView->expects($this->once())->method('setControllerContext')->with($this->mockControllerContext);
+
+		$this->assertEquals('FallbackView called', $view->render());
+	}
+
+	/**
+	 * @test
+	 */
+	public function typoScriptViewOutputsVariable() {
+		$view = $this->buildView('Foo\Bar\Controller\TestController', 'index');
+		$view->assign('test', 'Hallo Welt');
+		$this->assertEquals('TestRenderer calledHallo Welt', $view->render());
+	}
+
+	protected function buildView($controllerObjectName, $controllerActionName) {
+		$request = $this->getMockBuilder('TYPO3\FLOW3\Mvc\ActionRequest')->disableOriginalConstructor()->getMock();
+		$request->expects($this->any())->method('getControllerObjectName')->will($this->returnValue($controllerObjectName));
+		$request->expects($this->any())->method('getControllerActionName')->will($this->returnValue($controllerActionName));
+		$this->mockControllerContext = $this->getMockBuilder('TYPO3\FLOW3\Mvc\Controller\ControllerContext')->disableOriginalConstructor()->getMock();
+		$this->mockControllerContext->expects($this->any())->method('getRequest')->will($this->returnValue($request));
+
+		$view = new \TYPO3\TypoScript\View\TypoScriptView();
+		$view->setControllerContext($this->mockControllerContext);
+		$this->inject($view, 'fallbackView', $this->mockFallbackView);
+
+		\TYPO3\FLOW3\Reflection\ObjectAccess::setProperty($view, 'typoScriptPathPattern', __DIR__ . '/Fixtures/TypoScript', TRUE);
+
+		return $view;
+	}
+}
+?>
