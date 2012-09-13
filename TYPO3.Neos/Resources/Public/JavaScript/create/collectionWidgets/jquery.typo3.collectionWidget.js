48f117d9e876de23af93728df7e80a5222404b0e
Rens Admiraal <rens.admiraal@typo3.org>
2012-09-13 09:49:59 +0200

    [TASK] Prevent console.log from createjs
    Createjs wrote a console.log if create can't find localStorage or a
    url for storage. This change introduces a dummy function which will
    be used by createjs.
    
    Change-Id: Ibfec5ecd8e93e0acd80e1477be67f7c103f8d354
    Original-Commit-Hash: 9f89eaf8c9d21ef65a6795dd0e88cd32a21254fe

--- TYPO3.Neos/Resources/Public/JavaScript/create/collectionWidgets/jquery.typo3.collectionWidget.js
+++ TYPO3.Neos/Resources/Public/JavaScript/create/collectionWidgets/jquery.typo3.collectionWidget.js
@@ -8,6 +8,18 @@ define(
 		(function ($, undefined) {
 			$.widget('typo3.typo3CollectionWidget', $.Midgard.midgardCollectionAddBetween, {
 
+				/**
+				 * The midgardCollectionAddBetween widget tries to detect the correct way to store
+				 * changes in the _create() method. This will fail on finding creates localStorage
+				 * widget, and our model does not have a url set. By introducing this dummy function
+				 * we prevent an error console.log() from create. This method is called without
+				 * further arguments, and does not change behaviour.
+				 */
+				_create: function() {
+					this.options.model.url = function() {};
+					this._super();
+				},
+
 				enable: function() {
 					this.addHandles();
 				},
