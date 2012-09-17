c372e091125c4bd47203c7d6a2f5d2c5b390b97a
Karsten Dambekalns <karsten@typo3.org>
2012-09-17 11:39:07 +0200

    [TASK] Whitespace fix in class docblock
    Change-Id: Iff2b0f987cba9a30cccbcc5d7ce219055634afd2
    Original-Commit-Hash: 0c99ec7cf7616abc7762a2e005580bdb7a51dd4c

--- TYPO3.Media/Classes/ViewHelpers/ImageViewHelper.php
+++ TYPO3.Media/Classes/ViewHelpers/ImageViewHelper.php
@@ -1,11 +1,12 @@
 <?php
 namespace TYPO3\Media\ViewHelpers;
+
 /*                                                                        *
  * This script belongs to the FLOW3 package "TYPO3.Media".                *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
- *  of the License, or (at your option) any later version.                *
+ * of the License, or (at your option) any later version.                 *
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
