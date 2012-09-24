8842db5d861c65b382340fee6274d222a09a70be
Karsten Dambekalns <karsten@typo3.org>
2012-09-24 12:47:13 +0200

    [TASK] Make PSR-0 and composer compatible
    Moves all classes into a PSR-0 directory structure and adds a composer
    manifest instead of Package.xml.
    
    Change-Id: I8339f78dca132e1f6a1e09b10d58925dfd76800d
    Migration: TYPO3.FLOW3-201209201112
    Related: #41031
    Original-Commit-Hash: 0d00d52b557232c03ecb53d20b23602b1bcda0d1

--- TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Package.php
+++ TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Package.php
@@ -2,7 +2,7 @@
 namespace TYPO3\TYPO3CR;
 
 /*                                                                        *
- * This script belongs to the FLOW3 framework.                            *
+ * This script belongs to the FLOW3 package "TYPO3CR".                    *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
