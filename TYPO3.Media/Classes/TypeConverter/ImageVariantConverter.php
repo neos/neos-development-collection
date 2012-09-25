fd56577325e6cec557774efd99e7e447951ba392
Christian Müller <christian@kitsunet.de>
2012-09-25 08:50:24 +0200

    [TASK] Provide ImageVariant TypeConverter
    The TypeConverter is needed for TYPO3 Phoenix as the gettable
    properties do not match the settable properties. Phoenix will
    send all properties back so the TypeConverter needs to sort out
    which properties are needed: Only the originalImage and
    processingInstructions.
    
    The bug in Phoenix only appears when editing *another property* than
    the image itself in an already-existing image content element,
    f.e. the "title" property.
    
    In this case, all serialized properties of the ImageVariant, also the
    computed properties, are sent back to the server which caused the error.
    
    If the user modified the image itself, the client-side cropping / scaling
    code built up a JSON representation of the ImageVariant which only contains
    originalImage and processingInstructions; so no Property Mapping error occured
    in this case.
    
    Change-Id: Ie0b8168c10de95444dccfc72e5953e4358a7eca7
    Original-Commit-Hash: 072d998a1c194bf8d619677ae2ca9ee4a11c736e

---
+++ TYPO3.Media/Classes/TypeConverter/ImageVariantConverter.php
@@ -0,0 +1,85 @@
+<?php
+namespace TYPO3\Media\TypeConverter;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "TYPO3.Media".                *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * This converter transforms arrays to \TYPO3\Media\Domain\Model\ImageVariant objects
+ *
+ * @api
+ * @FLOW3\Scope("singleton")
+ */
+class ImageVariantConverter extends \TYPO3\FLOW3\Property\TypeConverter\AbstractTypeConverter {
+
+	/**
+	 * @var array
+	 */
+	protected $sourceTypes = array('array');
+
+	/**
+	 * @var string
+	 */
+	protected $targetType = 'TYPO3\Media\Domain\Model\ImageVariant';
+
+	/**
+	 * @var integer
+	 */
+	protected $priority = 1;
+
+	/**
+	 * Convert all properties in the source array
+	 *
+	 * @param mixed $source
+	 * @return array
+	 */
+	public function getSourceChildPropertiesToBeConverted($source) {
+		return array(
+			'originalImage' => $source['originalImage'],
+			'processingInstructions' => $source['processingInstructions']
+		);
+	}
+
+	/**
+	 * Define types of to be converted child properties
+	 *
+	 * @param string $targetType
+	 * @param string $propertyName
+	 * @param \TYPO3\FLOW3\Property\PropertyMappingConfigurationInterface $configuration
+	 * @return string
+	 */
+	public function getTypeOfChildProperty($targetType, $propertyName, \TYPO3\FLOW3\Property\PropertyMappingConfigurationInterface $configuration) {
+		if ($propertyName === 'originalImage') {
+			return '\TYPO3\Media\Domain\Model\Image';
+		}
+		if ($propertyName === 'processingInstructions') {
+			return 'array';
+		}
+	}
+
+	/**
+	 * Convert an object from $source to an \TYPO3\Media\Domain\Model\ImageVariant
+	 *
+	 * @param array $source
+	 * @param string $targetType must be 'TYPO3\Media\Domain\Model\ImageVariant'
+	 * @param array $convertedChildProperties
+	 * @param \TYPO3\FLOW3\Property\PropertyMappingConfigurationInterface $configuration
+	 * @return \TYPO3\Media\Domain\Model\ImageVariant|\TYPO3\FLOW3\Validation\Error The converted Image, a Validation Error or NULL
+	 */
+	public function convertFrom($source, $targetType, array $convertedChildProperties = array(), \TYPO3\FLOW3\Property\PropertyMappingConfigurationInterface $configuration = NULL) {
+		if (!isset($convertedChildProperties['originalImage']) || !$convertedChildProperties['originalImage'] instanceof \TYPO3\Media\Domain\Model\Image) {
+			return NULL;
+		}
+		return new \TYPO3\Media\Domain\Model\ImageVariant($convertedChildProperties['originalImage'], $convertedChildProperties['processingInstructions']);
+	}
+}
+?>
