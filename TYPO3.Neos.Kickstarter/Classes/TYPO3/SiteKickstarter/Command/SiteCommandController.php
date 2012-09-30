0fbae020ca39af8c2e1e3b5d23d8934ff5ce2b32
Karsten Dambekalns <karsten@typo3.org>
2012-09-30 11:06:45 +0200

    [TASK] Adjust to FLOW3 renamed to TYPO3 Flow
    Adjusts the code, comments, documentation as needed.
    
    Change-Id: Ibfb7a5bbcbdb755120fa1788fc69ce2f328e49c7
    Related: #41466
    Migration: TYPO3.Flow-201209251426
    Original-Commit-Hash: 0dbdfc9c9f4b8378dce432bee8ca97554b562f00

--- TYPO3.Neos.Kickstarter/Classes/TYPO3/SiteKickstarter/Command/SiteCommandController.php
+++ TYPO3.Neos.Kickstarter/Classes/TYPO3/SiteKickstarter/Command/SiteCommandController.php
@@ -2,7 +2,7 @@
 namespace TYPO3\SiteKickstarter\Command;
 
 /*                                                                        *
- * This script belongs to the FLOW3 package "SiteKickstarter".            *
+ * This script belongs to the TYPO3 Flow package "SiteKickstarter".       *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU General Public License, either version 3 of the   *
@@ -11,24 +11,24 @@ namespace TYPO3\SiteKickstarter\Command;
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
 
-use \TYPO3\FLOW3\Annotations as FLOW3;
-use \TYPO3\FLOW3\Utility\Files as Files;
+use \TYPO3\Flow\Annotations as Flow;
+use \TYPO3\Flow\Utility\Files as Files;
 
 /**
  * Command controller for the Kickstart generator
  *
  */
-class SiteCommandController extends \TYPO3\FLOW3\Cli\CommandController {
+class SiteCommandController extends \TYPO3\Flow\Cli\CommandController {
 
 	/**
-	 * @var \TYPO3\FLOW3\Package\PackageManagerInterface
-	 * @FLOW3\Inject
+	 * @var \TYPO3\Flow\Package\PackageManagerInterface
+	 * @Flow\Inject
 	 */
 	protected $packageManager;
 
 	/**
 	 * @var \TYPO3\SiteKickstarter\Service\GeneratorService
-	 * @FLOW3\Inject
+	 * @Flow\Inject
 	 */
 	protected $generatorService;
 
@@ -51,7 +51,7 @@ class SiteCommandController extends \TYPO3\FLOW3\Cli\CommandController {
 			$this->outputLine('Package "%s" already exists.', array($packageKey));
 			$this->quit(2);
 		}
-		$this->packageManager->createPackage($packageKey, NULL, Files::getUnixStylePath(Files::concatenatePaths(array(FLOW3_PATH_PACKAGES, 'Sites'))));
+		$this->packageManager->createPackage($packageKey, NULL, Files::getUnixStylePath(Files::concatenatePaths(array(FLOW_PATH_PACKAGES, 'Sites'))));
 
 		$generatedFiles = array();
 		$generatedFiles += $this->generatorService->generateSitesXml($packageKey, $siteName);
