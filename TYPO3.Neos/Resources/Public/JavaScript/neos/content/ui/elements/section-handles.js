8bd73611cc805001725594f14dda52fae0a814b2
Aske Ertmann <aertmann@gmail.com>
2013-01-30 11:32:07 +0100

    [BUGFIX] Make section handles visibility respect removed elements
    This also fixes the issue that the logic if it should be shown or not
    only happened when the handle was initialize and not when the vie
    collection changed. This means the handle will appear after removing
    all the content and disappear again when a content element is added.
    
    Change-Id: I5ccb4ab9fdd703e7fc9b261bed59ec4a114a0c07
    Original-Commit-Hash: e4dd10ca0585643e5c6bb1b62648c9ad8cd9aba8

--- TYPO3.Neos/Resources/Public/JavaScript/neos/content/ui/elements/section-handles.js
+++ TYPO3.Neos/Resources/Public/JavaScript/neos/content/ui/elements/section-handles.js
@@ -3,26 +3,76 @@
 define(
 [
 	'jquery',
+	'vie/entity',
 	'neos/content/ui/elements/contentelement-handles'
 ],
-function ($, ContentElementHandle) {
+function ($, EntityWrapper, ContentElementHandle) {
 	if (window._requirejsLoadingTrace) window._requirejsLoadingTrace.push('neos/content/ui/section-handles');
 
 	return ContentElementHandle.extend({
+		/**
+		 * @var {Boolean}
+		 */
 		_showRemove: false,
+
+		/**
+		 * @var {Boolean}
+		 */
 		_showCut: false,
+
+		/**
+		 * @var {Boolean}
+		 */
 		_showCopy: false,
 
+		/**
+		 * @var {Boolean}
+		 */
 		_nestedContentElementsAvailable: false,
 
+		/**
+		 * @var {Object} jQuery object for the element to which the handles should be added
+		 */
+		_element: null,
+
+		/**
+		 * @var {Object} A jQuery widget of type typo3.typo3CollectionWidget
+		 */
+		_collection: null,
+
+		/**
+		 * @var {Integer} The position in the collection on which paste / new actions should place the new entity
+		 */
+		_entityCollectionIndex: null,
+
+		/**
+		 * @return {void}
+		 */
 		didInsertElement: function() {
 			this._super();
 
-			if (this.get('_element').find('.t3-contentelement').length > 0) {
-				this.set('_nestedContentElementsAvailable', true);
-			}
+			this._areNestedContentElementsAvailable();
+
+			this.getPath('_collection.options.collection').on('change', this._areNestedContentElementsAvailable, this);
 		},
 
+		/**
+		 * @return {void}
+		 */
+		_areNestedContentElementsAvailable: function() {
+			var availableContentElements = 0;
+			this.getPath('_collection.options.collection.models').forEach(function(entity) {
+				var attributes = EntityWrapper.extractAttributesFromVieEntity(entity);
+				if (attributes['_removed'] !== true) {
+					availableContentElements++;
+				}
+			}, this);
+			this.set('_nestedContentElementsAvailable', availableContentElements > 0 ? true : false);
+		},
+
+		/**
+		 * @return {Boolean}
+		 */
 		_isShown: function() {
 			if (!this.get('_nestedContentElementsAvailable')) {
 				return true;
@@ -30,11 +80,14 @@ function ($, ContentElementHandle) {
 			return (this.get('_nodePath') === T3.Content.Model.NodeSelection.getPath('selectedNode.nodePath'));
 		}.property('T3.Content.Model.NodeSelection.selectedNode', '_nodePath', '_nestedContentElementsAvailable'),
 
+		/**
+		 * @return {void}
+		 */
 		_toggleVisibilityOnShownChange: function() {
 			if (this.get('_isShown')) {
-				this.$().css('visibility', 'visible');
+				this.$().show();
 			} else {
-				this.$().css('visibility', 'hidden');
+				this.$().hide();
 			}
 		}.observes('_isShown')
 	});
