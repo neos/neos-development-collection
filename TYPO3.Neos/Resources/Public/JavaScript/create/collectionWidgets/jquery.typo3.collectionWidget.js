f397cec98f3ddcb4bf9449cd990d6a0c7ee03175
Aske Ertmann <aertmann@gmail.com>
2013-03-26 18:39:41 +0100

    [BUGFIX] Copy/paste breaks after pasting an element
    The issue was caused by the handle bar not being destroyed
    when the collection was re-initialized leading to Ember
    throwning an error due to some elements removed from the
    dom having bindings.
    
    Change-Id: I53ba089cf117987e4612490d7c645730a1450e06
    Fixes: #45486
    Original-Commit-Hash: 6100c4148d7a6bb3fb60e898a5288244a007fbf1

--- TYPO3.Neos/Resources/Public/JavaScript/create/collectionWidgets/jquery.typo3.collectionWidget.js
+++ TYPO3.Neos/Resources/Public/JavaScript/create/collectionWidgets/jquery.typo3.collectionWidget.js
@@ -7,7 +7,6 @@ define(
 	function($) {
 		(function ($, undefined) {
 			$.widget('typo3.typo3CollectionWidget', $.Midgard.midgardCollectionAddBetween, {
-
 				/**
 				 * The midgardCollectionAddBetween widget tries to detect the correct way to store
 				 * changes in the _create() method. This will fail on finding creates localStorage
@@ -16,8 +15,13 @@ define(
 				 * further arguments, and does not change behaviour.
 				 */
 				_create: function() {
+					this.handles = [];
 					this.options.model.url = function() {};
 					this._super();
+				},
+
+				_destroy: function() {
+					this.removeHandles();
 				},
 
 				enable: function() {
@@ -25,13 +29,28 @@ define(
 				},
 
 				addHandles: function() {
-					T3.Content.UI.Util.AddContentElementHandleBars($(this.options.view.el), 0, this, true);
+					var that = this,
+						handle = T3.Content.UI.Util.AddContentElementHandleBars($(this.options.view.el), 0, this, true);
+					if (handle) {
+						this.handles.push(handle);
+					}
 
 					_.each(this.options.collection.models, function(entity, iterator) {
-						var id = entity.id.substring(1, entity.id.length - 1);
-						T3.Content.UI.Util.AddContentElementHandleBars($('[about="' + id + '"]').first(), iterator + 1, this, false);
-						T3.Content.UI.Util.AddNotInlineEditableOverlay($('[about="' + id + '"]').first(), entity);
+						var id = entity.id.substring(1, entity.id.length - 1),
+							$element = $('[about="' + id + '"]').first(),
+							handle = T3.Content.UI.Util.AddContentElementHandleBars($element, iterator + 1, this, false);
+						if (handle) {
+							that.handles.push(handle);
+						}
+						T3.Content.UI.Util.AddNotInlineEditableOverlay($element, entity);
 					}, this);
+				},
+
+				removeHandles: function() {
+					_.each(this.handles, function(handle) {
+						handle.destroy();
+					});
+					this.handles = [];
 				}
 			});
 		})($);
