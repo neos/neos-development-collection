f68a609b9d39ebf3253c436c992b04e8e4929fdb
Christopher Hlubek <hlubek@networkteam.com>
2013-05-13 16:16:37 +0200

    [TASK] Integrate Behat and implement tests for login
    See documentation for setting up and running Behat tests.
    
    Change-Id: I716542fb6e8efaf94b9272dd621fe000e7bf9299
    Original-Commit-Hash: c893fff6c192711cc9cac9218175794f216b6799

---
+++ TYPO3.Neos/Tests/Behavior/features/bootstrap/FeatureContext.php
@@ -0,0 +1,180 @@
+<?php
+
+use Behat\Gherkin\Node\TableNode,
+	Behat\MinkExtension\Context\MinkContext;
+use TYPO3\Flow\Utility\Arrays;
+use PHPUnit_Framework_Assert as Assert;
+
+require_once(__DIR__ . '/../../../../../Flowpack.Behat/Tests/Behat/FlowContext.php');
+
+/**
+ * Features context
+ */
+class FeatureContext extends MinkContext {
+
+	/**
+	 * @var \TYPO3\Flow\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * Initializes the context
+	 *
+	 * @param array $parameters Context parameters (configured through behat.yml)
+	 */
+	public function __construct(array $parameters) {
+		$this->useContext('flow', new \Flowpack\Behat\Tests\Behat\FlowContext($parameters));
+		$this->objectManager = $this->getSubcontext('flow')->getObjectManager();
+	}
+
+	/**
+	 * @Given /^I am not authenticated$/
+	 */
+	public function iAmNotAuthenticated() {
+		// Do nothing, every scenario has a new session
+	}
+
+	/**
+	 * @Then /^I should see a login form$/
+	 */
+	public function iShouldSeeALoginForm() {
+		$this->assertSession()->fieldExists('Username');
+		$this->assertSession()->fieldExists('Password');
+	}
+
+	/**
+	 * @Given /^the following users exist:$/
+	 */
+	public function theFollowingUsersExist(TableNode $table) {
+		$rows = $table->getHash();
+		/** @var \TYPO3\Neos\Domain\Factory\UserFactory $userFactory */
+		$userFactory = $this->objectManager->get('TYPO3\Neos\Domain\Factory\UserFactory');
+		/** @var \TYPO3\Party\Domain\Repository\PartyRepository $partyRepository */
+		$partyRepository = $this->objectManager->get('TYPO3\Party\Domain\Repository\PartyRepository');
+		/** @var \TYPO3\Flow\Security\AccountRepository $accountRepository */
+		$accountRepository = $this->objectManager->get('TYPO3\Flow\Security\AccountRepository');
+		foreach ($rows as $row) {
+			$roleIdentifiers = array_map(function($role) {
+				return 'TYPO3.Neos:' . $role;
+			}, Arrays::trimExplode(',', $row['roles']));
+			$user = $userFactory->create($row['username'], $row['password'], $row['firstname'], $row['lastname'], $roleIdentifiers);
+
+			$partyRepository->add($user);
+			$accounts = $user->getAccounts();
+			foreach ($accounts as $account) {
+				$accountRepository->add($account);
+			}
+		}
+		$this->getSubcontext('flow')->persistAll();
+	}
+
+	/**
+	 * @Given /^I am authenticated with "([^"]*)" and "([^"]*)" for the backend$/
+	 */
+	public function iAmAuthenticatedWithAndForTheBackend($username, $password) {
+		$this->visit('/neos/login');
+		$this->fillField('Username', $username);
+		$this->fillField('Password', $password);
+		$this->pressButton('Login');
+	}
+
+	/**
+	 * @Then /^I should be on the "([^"]*)" page$/
+	 */
+	public function iShouldBeOnThePage($page) {
+		switch ($page) {
+			case 'Login':
+				$this->assertSession()->addressEquals('/neos/login');
+				break;
+			default:
+				throw new \Behat\Behat\Exception\PendingException();
+		}
+	}
+
+	/**
+	 * @Then /^I should be in the "([^"]*)" module$/
+	 */
+	public function iShouldBeInTheModule($moduleName) {
+		switch ($moduleName) {
+			case 'Content':
+				$this->assertSession()->elementTextContains('css', '.t3-topbar .t3-active a', 'Content');
+				break;
+			default:
+				throw new \Behat\Behat\Exception\PendingException();
+		}
+	}
+
+	/**
+	 * @When /^I follow "([^"]*)" in the main menu$/
+	 */
+	public function iFollowInTheMainMenu($link) {
+		$this->assertElementOnPage('ul.nav');
+		$this->getSession()->getPage()->find('css', 'ul.nav')->findLink($link)->click();
+	}
+
+	/**
+	 * @Given /^I should be logged in as "([^"]*)"$/
+	 */
+	public function iShouldBeLoggedInAs($name) {
+		$this->assertSession()->elementTextContains('css', '.t3-ui .t3-button-logout a', $name);
+	}
+
+	/**
+	 * @Then /^I should not be logged in$/
+	 */
+	public function iShouldNotBeLoggedIn() {
+		$this->assertSession()->elementNotExists('css', '.t3-ui .t3-button-logout');
+	}
+
+	/**
+	 * @Given /^I should see the page title "([^"]*)"$/
+	 */
+	public function iShouldSeeThePageTitle($title) {
+		$this->assertSession()->elementTextContains('css', 'title', $title);
+	}
+
+	/**
+	 * @Then /^I should not see the top bar$/
+	 */
+	public function iShouldNotSeeTheInspectorPanel() {
+		$this->assertElementOnPage('.t3-topbar');
+		Assert::assertFalse($this->getSession()->getPage()->find('css', '.t3-topbar')->isVisible());
+	}
+
+	/**
+	 * @Given /^the button "([^"]*)" should be active$/
+	 */
+	public function theButtonShouldBeActive($buttonName) {
+		$button = $this->getSession()->getPage()->findButton($buttonName);
+		if ($button === NULL) {
+			throw new \Behat\Mink\Exception\ElementNotFoundException($this->getSession(), 'button', 'id|name|label|value', $buttonName);
+		}
+
+		Assert::assertTrue($button->hasClass('pressed'), 'Button should be pressed');
+	}
+
+	/**
+	 * @Given /^I imported the site "([^"]*)"$/
+	 */
+	public function iImportedTheSite($packageKey) {
+		/** @var \TYPO3\TYPO3CR\Domain\Repository\NodeRepository $nodeRepository */
+		$nodeRepository = $this->objectManager->get('TYPO3\TYPO3CR\Domain\Repository\NodeRepository');
+		$contentContext = new \TYPO3\Neos\Domain\Service\ContentContext('live');
+		\TYPO3\Flow\Reflection\ObjectAccess::setProperty($nodeRepository, 'context', $contentContext, TRUE);
+
+		/** @var \TYPO3\Neos\Domain\Service\SiteImportService $siteImportService */
+		$siteImportService = $this->objectManager->get('TYPO3\Neos\Domain\Service\SiteImportService');
+		$siteImportService->importFromPackage($packageKey);
+
+		$this->getSubcontext('flow')->persistAll();
+	}
+
+	/**
+	 * @param string $path
+	 * @return string
+	 */
+	public function locatePath($path) {
+		return parent::locatePath($this->getSubcontext('flow')->resolvePath($path));
+	}
+
+}
