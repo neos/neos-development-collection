6a8ccdcf7c0576edc4d406952040896b0c731ce7
Sebastian Kurfuerst <sebastian@typo3.org>
2013-07-02 23:11:45 +0200

    [BUGFIX] Remove jQuery UI Tooltip from Aloha top bar
    Change-Id: I13d6ad73d7283979d47a6b596a7553503ea5a1ca
    Reviewed-on: https://review.typo3.org/21822
    Reviewed-by: Rens Admiraal
    Tested-by: Rens Admiraal
    Reviewed-by: Sebastian Kurfuerst
    Original-Commit-Hash: fc82ae3a58daf88f56dda1c9c59853c0098d99fb

--- TYPO3.Neos/Resources/Public/JavaScript/LibraryExtensions/UiAlohaPlugin/button.js
+++ TYPO3.Neos/Resources/Public/JavaScript/LibraryExtensions/UiAlohaPlugin/button.js
@@ -1,11 +1,23 @@
 define([
-	'originalButton'
+	'originalButton',
+	'ui/utils'
 ], function (
-	OriginalButton
+	OriginalButton,
+	Utils
 ) {
 	'use strict';
 
+	// We patch this class to remove jQuery tooltip, replacing it by standard in-browser tooltips.
 	return OriginalButton.extend({
+		init: function () {
+			this.createButtonElement();
+			Utils.makeButton(this.buttonElement, this)
+				.button('widget')
+				.click(jQuery.proxy(function () {
+					this._onClick();
+				}, this));
+		},
+
 		adoptParent: function (container) {
 		}
 	});
