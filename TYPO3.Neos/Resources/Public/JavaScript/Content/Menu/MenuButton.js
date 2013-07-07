f48fd0a6039f4a3a3491d9c9952ae0b6fbb5b561
Aske Ertmann <aertmann@gmail.com>
2013-07-07 16:58:29 +0200

    [BUGFIX] Remove auto expanding of menu component
    Change-Id: I362594aeecfae7c1053fc43749622b85c4a8d7e9
    Related: #48068
    Reviewed-on: https://review.typo3.org/22132
    Reviewed-by: Aske Ertmann
    Tested-by: Aske Ertmann
    Reviewed-by: Tim Kandel
    Tested-by: Tim Kandel
    Reviewed-by: Mattias Nilsson
    Tested-by: Mattias Nilsson
    Reviewed-by: Rens Admiraal
    Tested-by: Rens Admiraal
    Original-Commit-Hash: ab4bed3e97efdf29f9f10b6173ec4a4420d53209

--- TYPO3.Neos/Resources/Public/JavaScript/Content/Menu/MenuButton.js
+++ TYPO3.Neos/Resources/Public/JavaScript/Content/Menu/MenuButton.js
@@ -2,24 +2,15 @@ define(
 	[
 		'emberjs',
 		'Library/jquery-with-dependencies',
-		'../Components/ToggleButton',
-		'Shared/LocalStorage'
-	], function(Ember, $, ToggleButton, LocalStorage) {
+		'../Components/ToggleButton'
+	], function(Ember, $, ToggleButton) {
 		return ToggleButton.extend({
 			elementId: ['neos-menu-button'],
 			title: 'Toggle menu',
 
-			init: function() {
-				this._super();
-				if (LocalStorage.getItem('menuMode') !== false) {
-					this.set('pressed', true);
-				}
-			},
-
 			toggle: function() {
 				var state = !this.get('pressed');
 				this.set('pressed', state);
-				LocalStorage.setItem('menuMode', state);
 			},
 
 			_onPressedChanged: function() {
