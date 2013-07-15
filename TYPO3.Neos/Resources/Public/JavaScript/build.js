5b0c7cc27290c4267a1f6608147891a615b6f100
Christopher Hlubek <hlubek@networkteam.com>
2013-07-15 22:31:47 +0200

    [BUGFIX] Fix JavaScript build with r.js
    Removes a misplaces semicolon inside build.js.
    
    Change-Id: I202299d980b1bc7307a8c35c9f5f6ca852cc6a02
    Reviewed-on: https://review.typo3.org/22336
    Tested-by: Mr. Hudson
    Reviewed-by: Christopher Hlubek
    Tested-by: Christopher Hlubek
    Original-Commit-Hash: 1fad961456ff216bb98f8bc056453a0b2a5c2a96

--- TYPO3.Neos/Resources/Public/JavaScript/build.js
+++ TYPO3.Neos/Resources/Public/JavaScript/build.js
@@ -1,6 +1,6 @@
 /**
  * WARNING: if changing any of the statements below, make sure to also
- * update them inside contentmodule-main.js!
+ * update them inside ContentModuleBootstrap.js!
  *
  * To start a build, run "r.js -o build.js" from within the current directory.
  */
@@ -24,4 +24,4 @@
 
 	// if you un-comment the line below, you get an un-optimized version.
 	//optimize: "none"
-});
+})
