c8f63b3b00ca2d24a89e9a1ed15c476a90ef8fbd
Aske Ertmann <aertmann@gmail.com>
2013-08-06 17:32:47 +0200

    [TASK] Add license information to composer manifest
    Change-Id: I27074546a01312f5fbf5d4b908eb8a5f9d49542e
    Resolves: #50835
    Reviewed-on: https://review.typo3.org/22863
    Tested-by: Karsten Dambekalns
    Reviewed-by: Marc Neuhaus
    Reviewed-by: Karsten Dambekalns
    Original-Commit-Hash: 3a3ffc2905fc218cc2a9bfdabdd1948e87a592c2

--- TYPO3.TypoScript/Classes/TYPO3/TypoScript/View/FluidView.php
+++ TYPO3.TypoScript/Classes/TYPO3/TypoScript/View/FluidView.php
@@ -5,8 +5,8 @@ namespace TYPO3\TypoScript\View;
  * This script belongs to the TYPO3 Flow package "TypoScript".            *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU Lesser General Public License, either version 3   *
- * of the License, or (at your option) any later version.                 *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
  *                                                                        *
  * The TYPO3 project - inspiring people to share!                         *
  *                                                                        */
