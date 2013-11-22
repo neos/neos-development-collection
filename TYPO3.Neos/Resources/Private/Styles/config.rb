b704b7fb043b2cc20fa16411d00d90cccd0c3be5
Bastian Waidelich <bastian@typo3.org>
2013-11-22 16:48:16 +0100

    [TASK] Compatibility with Ruby < v1.9
    Adds checks to the ``config.rb`` compass configuration file to prevent
    older Ruby versions from stumbling upon the introduced ``Encoding``
    constants.
    
    Change-Id: I80f45e414084eb9b08656d908fc921f26624b945
    Reviewed-on: https://review.typo3.org/25646
    Reviewed-by: Bastian Waidelich
    Tested-by: Bastian Waidelich
    Reviewed-by: Aske Ertmann
    Tested-by: Aske Ertmann
    Reviewed-by: Rens Admiraal
    Tested-by: Rens Admiraal
    Original-Commit-Hash: 54cb62cec12d4201ea10275e377709ef3d7fa26a

--- TYPO3.Neos/Resources/Private/Styles/config.rb
+++ TYPO3.Neos/Resources/Private/Styles/config.rb
@@ -1,5 +1,7 @@
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+if RUBY_VERSION =~ /1.9/
+	Encoding.default_external = Encoding::UTF_8
+	Encoding.default_internal = Encoding::UTF_8
+end
 relative_assets = true
 css_dir = "../../Public/Styles"
 sass_dir = "."
