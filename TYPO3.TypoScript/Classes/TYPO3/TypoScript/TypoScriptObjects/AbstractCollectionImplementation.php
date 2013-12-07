15d4597427fef9c16913b3ff8fe2d22ec176f4cd
Christian Müller <christian@kitsunet.de>
2013-12-07 23:15:04 +0100

    [TASK] Refactor Collection to enable versatile content collection
    Change-Id: I7ac83a8daa62b547d151eba73527cfabce07c4cc
    Reviewed-on: https://review.typo3.org/26020
    Reviewed-by: Christian Mueller
    Tested-by: Christian Mueller
    Tested-by: Berit Hlubek
    Reviewed-by: Rens Admiraal
    Tested-by: Rens Admiraal
    Reviewed-by: Sebastian Kurfuerst
    Reviewed-by: Christopher Hlubek
    Tested-by: Christopher Hlubek
    Original-Commit-Hash: 106d19c6eea613057abead908c1fa5f6b6642767

---
+++ TYPO3.TypoScript/Classes/TYPO3/TypoScript/TypoScriptObjects/AbstractCollectionImplementation.php
@@ -0,0 +1,136 @@
+<?php
+namespace TYPO3\TypoScript\TypoScriptObjects;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TypoScript".            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Abstract implementation of a collection renderer for TypoScript.
+ */
+abstract class AbstractCollectionImplementation extends AbstractTypoScriptObject {
+
+	/**
+	 * Render the array collection by triggering the itemRenderer for every element
+	 *
+	 * @var array or array-like
+	 */
+	protected $collection;
+
+	/**
+	 * The number of rendered nodes, filled only after evaluate() was called.
+	 *
+	 * @var integer
+	 */
+	protected $numberOfRenderedNodes;
+
+	/**
+	 * @var string
+	 */
+	protected $itemName;
+
+	/**
+	 * If set iteration data (index, cycle, isFirst, isLast) is available in context with the name given.
+	 *
+	 * @var string
+	 */
+	protected $iterationName;
+
+	/**
+	 * @param array $collection
+	 * @return void
+	 */
+	public function setCollection($collection) {
+		$this->collection = $collection;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getCollection() {
+		return $this->tsValue('collection');
+	}
+
+
+	/**
+	 * @param string $itemName
+	 * @return void
+	 */
+	public function setItemName($itemName) {
+		$this->itemName = $itemName;
+	}
+
+	/**
+	 * @param string $iterationName
+	 * @return void
+	 */
+	public function setIterationName($iterationName) {
+		$this->iterationName = $iterationName;
+	}
+
+	/**
+	 * Evaluate the collection nodes
+	 *
+	 * @return string
+	 * @throws \TYPO3\TypoScript\Exception
+	 */
+	public function evaluate() {
+		$collection = $this->getCollection();
+
+		$output = '';
+		if ($collection === NULL) {
+			return '';
+		}
+		$this->numberOfRenderedNodes = 0;
+		$itemName = $this->tsValue('itemName');
+		if ($itemName === NULL) {
+			throw new \TYPO3\TypoScript\Exception('The Collection needs an itemName to be set.', 1344325771);
+		}
+		$iterationName = $this->tsValue('iterationName');
+		$collectionTotalCount = count($collection);
+		foreach ($collection as $collectionElement) {
+			$context = $this->tsRuntime->getCurrentContext();
+			$context[$itemName] = $collectionElement;
+			if ($iterationName !== NULL) {
+				$context[$iterationName] = $this->prepareIterationInformation($collectionTotalCount);
+			}
+
+			$this->tsRuntime->pushContextArray($context);
+			$output .= $this->tsRuntime->render($this->path . '/itemRenderer');
+			$this->tsRuntime->popContext();
+			$this->numberOfRenderedNodes++;
+		}
+
+		return $output;
+	}
+
+	/**
+	 * @param integer $collectionCount
+	 * @return array
+	 */
+	protected function prepareIterationInformation($collectionCount) {
+		$iteration = array(
+			'index' => $this->numberOfRenderedNodes,
+			'cycle' => ($this->numberOfRenderedNodes + 1),
+			'isFirst' => FALSE,
+			'isLast' => FALSE
+		);
+		if ($this->numberOfRenderedNodes === 0) {
+			$iteration['isFirst'] = TRUE;
+		}
+		if (($this->numberOfRenderedNodes + 1) === $collectionCount) {
+			$iteration['isLast'] = TRUE;
+		}
+
+		return $iteration;
+	}
+}
+?>
--- TYPO3.TypoScript/Classes/TYPO3/TypoScript/TypoScriptObjects/CollectionImplementation.php
+++ TYPO3.TypoScript/Classes/TYPO3/TypoScript/TypoScriptObjects/CollectionImplementation.php
@@ -19,57 +19,7 @@ use TYPO3\Flow\Annotations as Flow;
  * //tsPath collection *Collection
  * //tsPath itemRenderer the TS object which is triggered for each element in the node collection
  */
-class CollectionImplementation extends AbstractTypoScriptObject {
-
-	/**
-	 * Render the array collection by triggering the itemRenderer for every element
-	 *
-	 * @var array or array-like
-	 */
-	protected $collection;
-
-	/**
-	 * The number of rendered nodes, filled only after evaluate() was called.
-	 *
-	 * @var integer
-	 */
-	protected $numberOfRenderedNodes;
-
-	/**
-	 * @var string
-	 */
-	protected $itemName;
-
-	/**
-	 * If set iteration data (index, cycle, isFirst, isLast) is available in context with the name given.
-	 *
-	 * @var string
-	 */
-	protected $iterationName;
-
-	/**
-	 * @param array $collection
-	 * @return void
-	 */
-	public function setCollection($collection) {
-		$this->collection = $collection;
-	}
-
-	/**
-	 * @param string $itemName
-	 * @return void
-	 */
-	public function setItemName($itemName) {
-		$this->itemName = $itemName;
-	}
-
-	/**
-	 * @param string $iterationName
-	 * @return void
-	 */
-	public function setIterationName($iterationName) {
-		$this->iterationName = $iterationName;
-	}
+class CollectionImplementation extends AbstractCollectionImplementation {
 
 	/**
 	 * Evaluate the collection nodes
@@ -78,53 +28,6 @@ class CollectionImplementation extends AbstractTypoScriptObject {
 	 * @throws \TYPO3\TypoScript\Exception
 	 */
 	public function evaluate() {
-		$collection = $this->tsValue('collection');
-
-		$output = '';
-		if ($collection === NULL) {
-			return '';
-		}
-		$this->numberOfRenderedNodes = 0;
-		$itemName = $this->tsValue('itemName');
-		if ($itemName === NULL) {
-			throw new \TYPO3\TypoScript\Exception('The Collection needs an itemName to be set.', 1344325771);
-		}
-		$iterationName = $this->tsValue('iterationName');
-		$collectionTotalCount = count($collection);
-		foreach ($collection as $collectionElement) {
-			$context = $this->tsRuntime->getCurrentContext();
-			$context[$itemName] = $collectionElement;
-			if ($iterationName !== NULL) {
-				$context[$iterationName] = $this->prepareIterationInformation($collectionTotalCount);
-			}
-
-			$this->tsRuntime->pushContextArray($context);
-			$output .= $this->tsRuntime->render($this->path . '/itemRenderer');
-			$this->tsRuntime->popContext();
-			$this->numberOfRenderedNodes++;
-		}
-
-		return $output;
-	}
-
-	/**
-	 * @param integer $collectionCount
-	 * @return array
-	 */
-	protected function prepareIterationInformation($collectionCount) {
-		$iteration = array (
-			'index' => $this->numberOfRenderedNodes,
-			'cycle' => ($this->numberOfRenderedNodes + 1),
-			'isFirst' => FALSE,
-			'isLast' => FALSE
-		);
-		if ($this->numberOfRenderedNodes === 0) {
-			$iteration['isFirst'] = TRUE;
-		}
-		if (($this->numberOfRenderedNodes + 1) === $collectionCount) {
-			$iteration['isLast'] = TRUE;
-		}
-
-		return $iteration;
+		return parent::evaluate();
 	}
 }
