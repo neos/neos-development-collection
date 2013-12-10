403c5653ef4973a1a0e127cb8760df71ae3192be
Bastian Waidelich <bastian@typo3.org>
2013-12-10 14:48:32 +0100

    [BUGFIX] Fix PluginViewsEditor path in PluginNodeTypePostprocessor
    Renames the ``editor`` setting to be in sync with the renamed editors
    in I91542f4412ab4e1d91863c77c8058f6d84461829.
    
    Change-Id: I7e1bec6a4c7735a80c36557ae8937c17eb8f19a6
    Releases: 1.0, master
    Reviewed-on: https://review.typo3.org/26269
    Tested-by: Bastian Waidelich
    Reviewed-by: Rens Admiraal
    Reviewed-by: Bastian Waidelich
    (cherry picked from commit f615c60382c88d0f62715ad4000215e98c5b17b6)
    Reviewed-on: https://review.typo3.org/26270
    Original-Commit-Hash: 99fec78110762e930e0fc23eea556ff2adae0a0b

--- TYPO3.Neos/Classes/TYPO3/Neos/NodeTypePostprocessor/PluginNodeTypePostprocessor.php
+++ TYPO3.Neos/Classes/TYPO3/Neos/NodeTypePostprocessor/PluginNodeTypePostprocessor.php
@@ -58,7 +58,7 @@ class PluginNodeTypePostprocessor implements NodeTypePostprocessorInterface {
 				'inspector' => array(
 					'group' => 'pluginViews',
 					'position' => '20',
-					'editor' => 'TYPO3.Neos/PluginViewsEditor'
+					'editor' => 'TYPO3.Neos/Inspector/Editors/PluginViewsEditor'
 				)
 			)
 		);
