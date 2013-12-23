f36e33b626bb77971822f7933f74d69630b3d5ac
Rens Admiraal <rens.admiraal@typo3.org>
2013-12-23 23:44:10 +0100

    [TASK] Only display images in media browser
    This change adds a separate controller for the media browser iframe.
    This controller uses the ImageRepository for fetching assets so only
    images are displayed.
    
    Change-Id: Idfc9e6826fe45d91e314077c7355398aa549a3dd
    Resolves: #54591
    Releases: master
    Reviewed-on: https://review.typo3.org/26555
    Reviewed-by: Aske Ertmann
    Tested-by: Aske Ertmann
    Reviewed-by: Dominique Feyer
    Reviewed-by: Karsten Dambekalns
    Reviewed-by: Robert Lemke
    Tested-by: Robert Lemke
    Original-Commit-Hash: 1f36d8960b4255ec77d89fb17a353cfd54cc7327

---
+++ TYPO3.Neos/Classes/TYPO3/Neos/Controller/Backend/MediaBrowserController.php
@@ -0,0 +1,47 @@
+<?php
+namespace TYPO3\Neos\Controller\Backend;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Neos".            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Configuration\ConfigurationManager;
+
+/**
+ * Controller for asset handling
+ */
+class MediaBrowserController extends \TYPO3\Media\Controller\AssetController {
+
+	/**
+	 * @var array
+	 */
+	protected $settings;
+
+	/**
+	 * @var ConfigurationManager
+	 * @Flow\Inject
+	 */
+	protected $configurationManager;
+
+	/**
+	 * @var \TYPO3\Media\Domain\Repository\ImageRepository
+	 * @Flow\Inject
+	 */
+	protected $assetRepository;
+
+	/**
+	 * @return void
+	 */
+	public function initializeAction() {
+		parent::initializeAction();
+		$this->settings = $this->configurationManager->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_SETTINGS, 'TYPO3.Media');
+	}
+
+}
