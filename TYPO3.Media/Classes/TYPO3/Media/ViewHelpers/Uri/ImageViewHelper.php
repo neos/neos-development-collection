03698bbe38702bce90445f587c56c5e8f4380eba
Rens Admiraal <rens.admiraal@typo3.org>
2013-12-23 12:53:39 +0100

    [BUGFIX] Upload of non-image results in Catchable Fatal Error
    Uploading a non-image asset into the media manager results in a fatal
    error as the ImageViewHelper only processes assets implementing the
    ImageInterface.
    
    This change adds default icons for common file extensions and uses
    a default fallback icon if no matching icon is found.
    
    Change-Id: I8769987d34ac54a3d36e6d3374040438edf30612
    Fixes: #54399
    Fixes: #54584
    Releases: master, 1.0
    Reviewed-on: https://review.typo3.org/27467
    Tested-by: Karsten Dambekalns
    Reviewed-by: Carsten Bleicker
    Tested-by: Carsten Bleicker
    Reviewed-by: Karsten Dambekalns
    Original-Commit-Hash: 31b1d7eefa440b16ee4bd6eb120c894f29029f31

--- TYPO3.Media/Classes/TYPO3/Media/ViewHelpers/Uri/ImageViewHelper.php
+++ TYPO3.Media/Classes/TYPO3/Media/ViewHelpers/Uri/ImageViewHelper.php
@@ -12,27 +12,29 @@ namespace TYPO3\Media\ViewHelpers\Uri;
  *                                                                        */
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Fluid\Core\ViewHelper\Exception;
+use TYPO3\Media\Domain\Model\AssetInterface;
 use TYPO3\Media\Domain\Model\ImageInterface;
 
 /**
- * Renders the src path of a thumbnail image of a given TYPO3.Media Image instance
+ * Renders the src path of a thumbnail image of a given TYPO3.Media asset instance
  *
  * = Examples =
  *
- * <code title="Rendering an image path as-is">
- * {m:uri.image(image: imageObject)}
+ * <code title="Rendering an asset path as-is">
+ * {m:uri.image(asset: assetObject)}
  * </code>
  * <output>
- * (depending on the image)
+ * (depending on the asset)
  * _Resources/Persistent/b29[...]95d.jpeg
  * </output>
  *
  *
- * <code title="Rendering an image path with scaling at a given width only">
- * {m:uri.image(image: imageObject, maximumWidth: 80)}
+ * <code title="Rendering an asset path with scaling at a given width only">
+ * {m:uri.image(asset: assetObject, maximumWidth: 80)}
  * </code>
  * <output>
- * (depending on the image; has scaled keeping the aspect ratio)
+ * (depending on the asset; has scaled keeping the aspect ratio)
  * _Resources/Persistent/b29[...]95d.jpeg
  * </output>
  *
@@ -47,19 +49,38 @@ class ImageViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 	protected $resourcePublisher;
 
 	/**
-	 * Renders the path to a thumbnail image, created from a given image.
+	 * @return void
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		// @deprecated since 1.1.0 image argument replaced with asset argument
+		$this->registerArgument('image', 'ImageInterface', 'The image to be rendered', FALSE);
+	}
+
+	/**
+	 * Renders the path to a thumbnail image, created from a given asset.
 	 *
-	 * @param \TYPO3\Media\Domain\Model\ImageInterface $image If specified, this resource object is used instead of the path and package information
+	 * @param AssetInterface $image The asset to be rendered as an image
 	 * @param integer $maximumWidth Desired maximum height of the image
 	 * @param integer $maximumHeight Desired maximum width of the image
 	 * @param boolean $allowCropping Whether the image should be cropped if the given sizes would hurt the aspect ratio
 	 * @param boolean $allowUpScaling Whether the resulting image size might exceed the size of the original image
 	 * @return string the relative image path, to be used as src attribute for <img /> tags
+	 * @throws Exception
 	 */
-	public function render(ImageInterface $image = NULL, $maximumWidth = NULL, $maximumHeight = NULL, $allowCropping = FALSE, $allowUpScaling = FALSE) {
-		$thumbnailImage = $this->getThumbnailImage($image, $maximumWidth, $maximumHeight, $allowCropping, $allowUpScaling);
-
-		return $this->resourcePublisher->getPersistentResourceWebUri($thumbnailImage->getResource());
+	public function render(AssetInterface $asset = NULL, $maximumWidth = NULL, $maximumHeight = NULL, $allowCropping = FALSE, $allowUpScaling = FALSE) {
+		// Fallback for deprecated image argument
+		$asset = $asset === NULL && $this->hasArgument('image') ? $this->arguments['image'] : $asset;
+		if (!$asset instanceof AssetInterface) {
+			throw new Exception('No asset given for rendering.');
+		}
+		if ($asset instanceof ImageInterface) {
+			$thumbnailImage = $this->getImageThumbnailImage($asset, $maximumWidth, $maximumHeight, $allowCropping, $allowUpScaling);
+			return $this->resourcePublisher->getPersistentResourceWebUri($thumbnailImage->getResource());
+		} else {
+			$thumbnailImage = $this->getAssetThumbnailImage($asset, $maximumWidth, $maximumHeight);
+			return $this->resourcePublisher->getStaticResourcesWebBaseUri() . 'Packages/' . $thumbnailImage['src'];
+		}
 	}
 
 	/**
@@ -73,9 +94,9 @@ class ImageViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 	 * @param boolean $allowUpScaling
 	 * @return \TYPO3\Media\Domain\Model\ImageInterface
 	 *
-	 * TODO move code to trait in order to avoid duplication with tag based ImageViewHelper
+	 * @TODO move code to trait in order to avoid duplication with image ViewHelper
 	 */
-	protected function getThumbnailImage(ImageInterface $image, $maximumWidth, $maximumHeight, $allowCropping, $allowUpScaling) {
+	protected function getImageThumbnailImage(ImageInterface $image, $maximumWidth, $maximumHeight, $allowCropping, $allowUpScaling) {
 		$ratioMode = ($allowCropping ? ImageInterface::RATIOMODE_OUTBOUND : ImageInterface::RATIOMODE_INSET);
 		if ($maximumWidth === NULL || ($allowUpScaling !== TRUE && $maximumWidth > $image->getWidth())) {
 			$maximumWidth = $image->getWidth();
@@ -87,6 +108,46 @@ class ImageViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 			return $image;
 		}
 		return $image->getThumbnail($maximumWidth, $maximumHeight, $ratioMode);
+	}
+
+	/**
+	 * @param AssetInterface $asset
+	 * @param integer $maximumWidth
+	 * @param integer $maximumHeight
+	 * @return array
+	 */
+	protected function getAssetThumbnailImage(AssetInterface $asset, $maximumWidth, $maximumHeight) {
+		$iconSize = $this->getDocumentIconSize($maximumWidth, $maximumHeight);
+
+		if (is_file('resource://TYPO3.Media/Public/Icons/16px/' . $asset->getResource()->getFileExtension() . '.png')) {
+			$icon = sprintf('TYPO3.Media/Icons/%spx/' . $asset->getResource()->getFileExtension() . '.png', $iconSize);
+		} else {
+			$icon =  sprintf('TYPO3.Media/Icons/%spx/_blank.png', $iconSize);
+		}
+
+		return array(
+			'width' => $iconSize,
+			'height' => $iconSize,
+			'src' => $icon
+		);
+	}
+
+	/**
+	 * @param integer $maximumWidth
+	 * @param integer $maximumHeight
+	 * @return integer
+	 */
+	protected function getDocumentIconSize($maximumWidth, $maximumHeight) {
+		$size = max($maximumWidth, $maximumHeight);
+		if ($size <= 16) {
+			return 16;
+		} elseif ($size <= 32) {
+			return 32;
+		} elseif ($size <= 48) {
+			return 48;
+		} else {
+			return 512;
+		}
 	}
 
 }
