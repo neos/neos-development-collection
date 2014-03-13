e22d651f9efe0f426212b714761ed8a2f5ec6c1a
Pankaj Lele <pankaj@lelesys.com>
2014-03-13 23:46:51 +0530

    [BUGFIX] PostgreSQL migration error: "dimensionshash" contains null
    This fixes an error while doctrine:migrate on a PostgreSQL database when
    adding a NOT NULL column to table typo3_typo3cr_domain_model_nodedata
    already having rows in it.
    
    Change-Id: I387626a0b011f324e5bf5ed773abb9b22ccf2566
    Releases: 1.1, master
    Reviewed-on: https://review.typo3.org/28356
    Reviewed-by: Karsten Dambekalns
    Reviewed-by: Christopher Hlubek
    Tested-by: Christopher Hlubek
    Original-Commit-Hash: 231f11946f663bcaab814618a95877b3131af18f

--- TYPO3.TYPO3CR/Migrations/Postgresql/Version20140206122814.php
+++ TYPO3.TYPO3CR/Migrations/Postgresql/Version20140206122814.php
@@ -20,11 +20,13 @@ class Version20140206122814 extends AbstractMigration {
 		$this->addSql("CREATE INDEX IDX_6C144D3693BDC8E2 ON typo3_typo3cr_domain_model_nodedimension (nodedata)");
 		$this->addSql("CREATE UNIQUE INDEX UNIQ_6C144D3693BDC8E25E237E061D775834 ON typo3_typo3cr_domain_model_nodedimension (nodedata, name, value)");
 		$this->addSql("ALTER TABLE typo3_typo3cr_domain_model_nodedimension ADD CONSTRAINT FK_6C144D3693BDC8E2 FOREIGN KEY (nodedata) REFERENCES typo3_typo3cr_domain_model_nodedata (persistence_object_identifier) NOT DEFERRABLE INITIALLY IMMEDIATE");
-		$this->addSql("ALTER TABLE typo3_typo3cr_domain_model_nodedata ADD dimensionshash VARCHAR(32) NOT NULL");
-		$this->addSql("ALTER TABLE typo3_typo3cr_domain_model_nodedata ADD dimensionvalues BYTEA NOT NULL");
+		$this->addSql("ALTER TABLE typo3_typo3cr_domain_model_nodedata ADD dimensionshash VARCHAR(32) DEFAULT NULL");
+		$this->addSql("ALTER TABLE typo3_typo3cr_domain_model_nodedata ADD dimensionvalues BYTEA DEFAULT NULL");
 		$this->addSql("COMMENT ON COLUMN typo3_typo3cr_domain_model_nodedata.dimensionvalues IS '(DC2Type:objectarray)'");
 		$this->addSql("UPDATE typo3_typo3cr_domain_model_nodedata SET dimensionvalues = 'a:0:{}'");
+		$this->addSql("ALTER TABLE typo3_typo3cr_domain_model_nodedata ALTER dimensionvalues SET NOT NULL");
 		$this->addSql("UPDATE typo3_typo3cr_domain_model_nodedata SET dimensionshash = 'd751713988987e9331980363e24189ce'");
+		$this->addSql("ALTER TABLE typo3_typo3cr_domain_model_nodedata ALTER dimensionshash SET NOT NULL");
 		$this->addSql("DROP INDEX UNIQ_60a956b92dbec7578d940019");
 		$this->addSql("CREATE UNIQUE INDEX UNIQ_60A956B92DBEC7578D94001992F8FB01 ON typo3_typo3cr_domain_model_nodedata (pathhash, workspace, dimensionshash)");
 	}
