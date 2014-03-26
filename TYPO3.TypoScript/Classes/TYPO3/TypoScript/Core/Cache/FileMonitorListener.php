ec436a2ae98ed9bc6b1e4b6798c0ebf4870718de
Christian Müller <christian@kitsunet.de>
2014-03-26 22:22:33 +0100

    [FEATURE] Add FileMonitor for TypoScript and Cache clearing
    Custom FileMonitor to watch changes in TypoScript files.
    Adds flushing of the content cache when either NodeTypes,
    TypoScript or Template files are changed.
    
    Needs: Id4aebb951f27b7befe1c1be81301b12679440bfc in TYPO3.Flow
    
    Change-Id: Ifa2d130d62d412d9ad5e6bdbf3b29db7ca9ebe83
    Releases: master, 1.1
    Reviewed-on: https://review.typo3.org/28890
    Reviewed-by: Christopher Hlubek
    Tested-by: Christopher Hlubek
    Reviewed-by: Aske Ertmann
    Reviewed-by: Rens Admiraal
    Original-Commit-Hash: f77928e7097b5828e27d45ef474fcd8072b0efec

---
+++ TYPO3.TypoScript/Classes/TYPO3/TypoScript/Core/Cache/FileMonitorListener.php
@@ -0,0 +1,53 @@
+<?php
+namespace TYPO3\TypoScript\Core\Cache;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.TypoScript".      *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Listener to clear TypoScript caches if important files have changed
+ *
+ * It's used in the Package bootstrap as an early instance, so no full dependency injection is available.
+ *
+ * @Flow\Proxy(false)
+ */
+class FileMonitorListener {
+
+	/**
+	 * @var \TYPO3\Flow\Cache\CacheManager
+	 */
+	protected $flowCacheManager;
+
+	/**
+	 * @param \TYPO3\Flow\Cache\CacheManager $flowCacheManager
+	 */
+	public function __construct(\TYPO3\Flow\Cache\CacheManager $flowCacheManager) {
+		$this->flowCacheManager = $flowCacheManager;
+	}
+
+	/**
+	 * @param $fileMonitorIdentifier
+	 * @param array $changedFiles
+	 * @return void
+	 */
+	public function flushContentCacheOnFileChanges($fileMonitorIdentifier, array $changedFiles) {
+		$fileMonitorsThatTriggerContentCacheFlush = array(
+			'TYPO3CR_NodeTypesConfiguration',
+			'TypoScript_Files',
+			'Fluid_TemplateFiles'
+		);
+
+		if (in_array($fileMonitorIdentifier, $fileMonitorsThatTriggerContentCacheFlush)) {
+			$this->flowCacheManager->getCache('TYPO3_TypoScript_Content')->flush();
+		}
+	}
+}
