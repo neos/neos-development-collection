9ddb537357d0c521343fa6050b7c10b12926cc25
Christian Müller <christian@kitsunet.de>
2014-03-26 09:25:49 +0100

    [BUGFIX] Behat Feature works with PHP 5.3
    Array Access to an array return from a function is not allowed
    in PHP 5.3. It was replaced by a backwards compatible access
    to the value.
    
    Change-Id: Ia9e94166079e1041c0d14af9115b8da5f6c44e9a
    Releases: master, 1.1
    Reviewed-on: https://review.typo3.org/28806
    Reviewed-by: Christian Mueller
    Tested-by: Christian Mueller
    Reviewed-by: Christopher Hlubek
    Original-Commit-Hash: ed9e80b5399a1b0c018d02fe0187bd64fe77b791

--- TYPO3.TYPO3CR/Tests/Behavior/Features/Bootstrap/FeatureContext.php
+++ TYPO3.TYPO3CR/Tests/Behavior/Features/Bootstrap/FeatureContext.php
@@ -66,7 +66,7 @@ class FeatureContext extends Behat\Behat\Context\BehatContext {
 		$rows = $table->getHash();
 		foreach ($rows as $row) {
 			$path = $row['Path'];
-			$name = array_slice(explode('/', $path), -1)[0];
+			$name = reset(array_slice(explode('/', $path), -1));
 			$parentPath = implode('/', array_slice(explode('/', $path), 0, -1)) ? : '/';
 
 			$context = $this->getContextForProperties($row, TRUE);
