fcd533c9dbddccff2f697a75cfffe38152517f63
Aske Ertmann <aertmann@gmail.com>
2014-04-07 16:08:22 +0200

    [BUGFIX] Notifications not shown after publishing/discarding
    Change-Id: I1c7368ee288faa1ed3d7794d02288a86f6b13edb
    Releases: master, 1.1
    Fixes: #NEOS-106
    Reviewed-on: https://review.typo3.org/29357
    Reviewed-by: Christian Mueller
    Tested-by: Christian Mueller
    Original-Commit-Hash: a3d2d6f899cf17633a104fa8765f8a8703ab9e50

--- TYPO3.Neos/Resources/Public/JavaScript/Content/Model/PublishableNodes.js
+++ TYPO3.Neos/Resources/Public/JavaScript/Content/Model/PublishableNodes.js
@@ -115,8 +115,9 @@ define(
 						that._removeNodeFromPublishableEntitySubjects(subject, 'live');
 					});
 					if (autoPublish !== true) {
-						var nodeType,
-							title = $('#neos-page-metainformation').attr('data-neos-title'),
+						var pageMetaInformation = $('#neos-page-metainformation'),
+							nodeType = pageMetaInformation.data('neos-_node-type'),
+							title = pageMetaInformation.attr('data-neos-title'),
 							nodeTypeDefiniton = NodeTypeService.getNodeTypeDefinition(nodeType);
 						Notification.ok('Published changes for ' + nodeTypeDefiniton.ui.label + ' "' + title + '"');
 					}
@@ -175,8 +176,9 @@ define(
 							});
 						}
 					);
-					var nodeType,
-						title = $('#neos-page-metainformation').attr('data-neos-title'),
+					var pageMetaInformation = $('#neos-page-metainformation'),
+						nodeType = pageMetaInformation.data('neos-_node-type'),
+						title = pageMetaInformation.attr('data-neos-title'),
 						nodeTypeDefiniton = NodeTypeService.getNodeTypeDefinition(nodeType);
 					Notification.ok('Discarded changes for ' + nodeTypeDefiniton.ui.label + ' "' + title + '"');
 				}
