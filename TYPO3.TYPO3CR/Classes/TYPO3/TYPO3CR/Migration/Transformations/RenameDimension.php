f86c7315e0f5cac9d65373ed245dd677ce02f2e3
Robert Lemke <robert@typo3.org>
2014-05-14 15:36:03 +0200

    [TASK] Remove support for "locale" context property
    In a previous change, some backwards compatible handling of the "locale"
    context property was introduced which set the "locales" content dimension
    value accordingly if a "locale" context property was found.
    
    This change removes support for this old context property and silently
    ignores it if present.
    
    Background: although the "locale" context property was allowed previously
    it never had any effect in terms of Neos' or Flow's behaviour. Supporting
    the "locale" context property like we did before this change would
    additionally bind TYPO3CR to the Neos content dimension for languages.
    
    Also provides a Node migration to move from locales to languages to keep
    existing translations from previous betas:
    
       ./flow node:migrate 20140516221523
    
    Change-Id: Icf0df4605c64dfe6bfc769278076e638db49a98f
    Related: NEOS-116
    Releases: 1.1, master
    Reviewed-on: https://review.typo3.org/30111
    Tested-by: Robert Lemke
    Reviewed-by: Robert Lemke
    Original-Commit-Hash: 53c4a731a9ea45fe201e6b70d358c348d3bed536

---
+++ TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Migration/Transformations/RenameDimension.php
@@ -0,0 +1,91 @@
+<?php
+namespace TYPO3\TYPO3CR\Migration\Transformations;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.TYPO3CR".         *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TYPO3CR\Domain\Model\NodeData;
+use TYPO3\TYPO3CR\Domain\Model\NodeDimension;
+use TYPO3\TYPO3CR\Domain\Repository\ContentDimensionRepository;
+
+/**
+ * Rename a dimension.
+ */
+class RenameDimension extends AbstractTransformation {
+
+	/**
+	 * @Flow\Inject
+	 * @var ContentDimensionRepository
+	 */
+	protected $contentDimensionRepository;
+
+	/**
+	 * The old name for the dimension.
+	 *
+	 * @var string
+	 */
+	protected $oldDimensionName;
+
+	/**
+	 * The new name for the dimension.
+	 *
+	 * @var string
+	 */
+	protected $newDimensionName;
+
+	/**
+	 * @param string $newDimensionName
+	 */
+	public function setNewDimensionName($newDimensionName) {
+		$this->newDimensionName = $newDimensionName;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getNewDimensionName() {
+		return $this->newDimensionName;
+	}
+
+	/**
+	 * @param string $oldDimensionName
+	 */
+	public function setOldDimensionName($oldDimensionName) {
+		$this->oldDimensionName = $oldDimensionName;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getOldDimensionName() {
+		return $this->oldDimensionName;
+	}
+
+	/**
+	 * Change the property on the given node.
+	 *
+	 * @param \TYPO3\TYPO3CR\Domain\Model\NodeData $nodeData
+	 * @return void
+	 */
+	public function execute(NodeData $nodeData) {
+		$dimensions = $nodeData->getDimensions();
+		if ($dimensions !== array()) {
+			foreach ($dimensions as $dimension) {
+				\TYPO3\Flow\var_dump($dimension->getName());
+				/** @var NodeDimension $dimension */
+				if ($dimension->getName() === $this->oldDimensionName) {
+					$dimension->setName($this->newDimensionName);
+				}
+			}
+			$nodeData->setDimensions($dimensions);
+		}
+	}
+}
