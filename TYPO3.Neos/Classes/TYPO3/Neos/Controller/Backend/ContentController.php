bb34d07ab58c5c544e529090ae2ecf03ed7f439a
Sascha Nowak <sascha.nowak@netlogix.de>
2014-09-29 19:13:59 +0200

    [BUGFIX] Use the original image in the image inspector editor
    Change-Id: Ib50ee386d40449e5ebf587435f25578e756eb8c7
    Releases: master, 1.1, 1.0
    Fixes: NEOS-416
    Original-Commit-Hash: aa8e6659c46dde944bf3c97d0195a28c349b8c35

--- TYPO3.Neos/Classes/TYPO3/Neos/Controller/Backend/ContentController.php
+++ TYPO3.Neos/Classes/TYPO3/Neos/Controller/Backend/ContentController.php
@@ -71,6 +71,7 @@ class ContentController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 
 		return json_encode(array(
 			'imageUuid' => $this->persistenceManager->getIdentifierByObject($image),
+			'originalImageResourceUri' => $this->resourcePublisher->getPersistentResourceWebUri($image->getResource()),
 			'previewImageResourceUri' => $this->resourcePublisher->getPersistentResourceWebUri($thumbnail->getResource()),
 			'originalSize' => array('w' => $image->getWidth(), 'h' => $image->getHeight()),
 			'previewSize' => array('w' => $thumbnail->getWidth(), 'h' => $thumbnail->getHeight())
