b1929bbe2114da1ae95b5df0e0b46484ed04ded4
Andreas Förthner <andreas.foerthner@netlogix.de>
2014-11-04 12:00:30 +0100

    [TASK] Adjust behat feature context to Flow changes
    This adjusts the behat feature context to the changes
    made in I7ba61a8e49d108b685394a70be8657550c4033aa.
    
    Change-Id: I39d53f10667002050d9ee58e4344fbfdc58f29ba
    Related: FLOW-102
    Releases: master
    Original-Commit-Hash: 2ca1c8b69654b5f7eef7a05a8a98534a14bb2904

--- TYPO3.TYPO3CR/Tests/Behavior/Features/Bootstrap/FeatureContext.php
+++ TYPO3.TYPO3CR/Tests/Behavior/Features/Bootstrap/FeatureContext.php
@@ -1,14 +1,22 @@
 <?php
 
 require_once(__DIR__ . '/../../../../../Flowpack.Behat/Tests/Behat/FlowContext.php');
-require_once(__DIR__ . '/StepDefinitionsTrait.php');
+require_once(__DIR__ . '/NodeOperationsTrait.php');
+require_once(__DIR__ . '/../../../../../../Framework/TYPO3.Flow/Tests/Behavior/Features/Bootstrap/IsolatedBehatStepsTrait.php');
+require_once(__DIR__ . '/../../../../../../Framework/TYPO3.Flow/Tests/Behavior/Features/Bootstrap/SecurityOperationsTrait.php');
+
+use Flowpack\Behat\Tests\Behat\FlowContext;
 
 /**
  * Features context
  */
 class FeatureContext extends Behat\Behat\Context\BehatContext {
 
-	use StepDefinitionsTrait;
+	use NodeOperationsTrait;
+
+	use SecurityOperationsTrait;
+
+	use IsolatedBehatStepsTrait;
 
 	/**
 	 * Initializes the context
@@ -16,15 +24,13 @@ class FeatureContext extends Behat\Behat\Context\BehatContext {
 	 * @param array $parameters Context parameters (configured through behat.yml)
 	 */
 	public function __construct(array $parameters) {
-		$this->useContext('flow', new \Flowpack\Behat\Tests\Behat\FlowContext($parameters));
+		$this->useContext('flow', new FlowContext($parameters));
+		$flowContext = $this->getSubcontext('flow');
+		$this->objectManager = $flowContext->getObjectManager();
+		$this->environment = $this->objectManager->get('TYPO3\Flow\Utility\Environment');
 	}
 
-	/**
-	 * @BeforeScenario @fixtures
-	 * @return void
-	 */
-	public function beforeScenarioDispatcher() {
-		$this->resetNodeInstances();
-		$this->resetContentDimensions();
+	protected function getObjectManager() {
+		return $this->objectManager;
 	}
 }
