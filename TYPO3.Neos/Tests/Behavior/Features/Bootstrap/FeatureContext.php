2b9590e88c7b27cb988f685f9724298a04bb3e1d
Rens Admiraal <rens.admiraal@typo3.org>
2014-11-07 14:14:18 +0100

    [BUGFIX] Missing getObjectManager method and trait usage
    This change implements getObjectManager() on the FeatureContext
    and includes the IsolatedBehatStepsTrait which is required for
    running the Behat tests.
    
    Change-Id: Idb8cf756eb059459f3287c4243e2d9c3fa077134
    Releases: master
    Original-Commit-Hash: 9f623d7d314faf54bc19718d80b3db8b874f7933

--- TYPO3.Neos/Tests/Behavior/Features/Bootstrap/FeatureContext.php
+++ TYPO3.Neos/Tests/Behavior/Features/Bootstrap/FeatureContext.php
@@ -7,12 +7,15 @@ use TYPO3\Flow\Utility\Arrays;
 use PHPUnit_Framework_Assert as Assert;
 
 require_once(__DIR__ . '/../../../../../Flowpack.Behat/Tests/Behat/FlowContext.php');
+require_once(__DIR__ . '/../../../../../../Framework/TYPO3.Flow/Tests/Behavior/Features/Bootstrap/IsolatedBehatStepsTrait.php');
 require_once(__DIR__ . '/../../../../../TYPO3.TYPO3CR/Tests/Behavior/Features/Bootstrap/NodeOperationsTrait.php');
 
 /**
  * Features context
  */
 class FeatureContext extends MinkContext {
+
+	use IsolatedBehatStepsTrait;
 
 	use NodeOperationsTrait;
 
@@ -39,6 +42,13 @@ class FeatureContext extends MinkContext {
 	public function __construct(array $parameters) {
 		$this->useContext('flow', new \Flowpack\Behat\Tests\Behat\FlowContext($parameters));
 		$this->objectManager = $this->getSubcontext('flow')->getObjectManager();
+	}
+
+	/**
+	 * @return \TYPO3\Flow\Object\ObjectManagerInterface
+	 */
+	protected function getObjectManager() {
+		return $this->objectManager;
 	}
 
 	/**
