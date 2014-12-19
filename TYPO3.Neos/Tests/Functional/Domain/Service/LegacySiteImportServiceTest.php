e19ff2567714ab7df70d44d95bd3bb09016e9c80
Bastian Waidelich <bastian@typo3.org>
2014-12-19 15:41:59 +0100

    [BUGFIX] Throw exception when trying to import an invalid Sites.xml file
    With this change a more meaningful exception is thrown if a site could
    not be imported for unknown reasons.
    Besides this slightly improves the ``site:import`` command to give
    feedback about the imported site on success.
    
    This also adds some cosmetic and non-functional cleanups in order to
    increase readabilty and IDE support:
    
    * Import FQN where applicable
    * Reduce nesting in ``SiteImportService::importFromFile()``
    * Remove unused import statements
    * Whitespace fixes
    
    Change-Id: I4a9fd0731354c3d33f3adb48fcfcd0ed96bbabc8
    Releases: master, 1.2
    Related: NEOS-885
    Original-Commit-Hash: e2de993dc9cce192e6064ac36dbb87b81ea3ffb2

--- TYPO3.Neos/Tests/Functional/Domain/Service/LegacySiteImportServiceTest.php
+++ TYPO3.Neos/Tests/Functional/Domain/Service/LegacySiteImportServiceTest.php
@@ -1,15 +1,16 @@
 <?php
 namespace TYPO3\Neos\Tests\Functional\Domain\Service;
 
-	/*                                                                        *
-	 * This script belongs to the TYPO3 Flow package "TYPO3.Neos".            *
-	 *                                                                        *
-	 * It is free software; you can redistribute it and/or modify it under    *
-	 * the terms of the GNU General Public License, either version 3 of the   *
-	 * License, or (at your option) any later version.                        *
-	 *                                                                        *
-	 * The TYPO3 project - inspiring people to share!                         *
-	 *                                                                        */
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Neos".            *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
 use TYPO3\Neos\Tests\Functional\AbstractNodeTest;
 
 
