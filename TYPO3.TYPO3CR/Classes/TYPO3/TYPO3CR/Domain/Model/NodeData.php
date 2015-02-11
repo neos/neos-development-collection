86b8ed477ef0acbcee4c43da0268c22ea608734a
Aske Ertmann <aertmann@gmail.com>
2015-02-11 18:30:42 +0100

    [!!!][BUGFIX] Node data identifier not unique for workspace/dimensions
    Run ``./flow doctrine migrate`` to adjust the database.
    
    This prevents a node duplicate identifier in the same workspace with
    the same dimensions, by adding a constraint across identifier,
    workspace and dimensions. Without it unexpected behavior can occur
    when finding nodes by their identifier, e.g. used for references and
    links.
    
    Change-Id: Iae9019a9280b7ce1edfdba30c849e9ca05cfed56
    Releases: master, 1.2, 1.1
    Fixes: NEOS-1002
    Original-Commit-Hash: 4d26407eb3569ace235bf9ded13038c9a52774da

--- TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Domain/Model/NodeData.php
+++ TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Domain/Model/NodeData.php
@@ -28,7 +28,10 @@ use TYPO3\TYPO3CR\Exception\NodeExistsException;
  *
  * @Flow\Entity
  * @ORM\Table(
- * 	uniqueConstraints={@ORM\UniqueConstraint(name="path_workspace_dimensions",columns={"pathhash", "workspace", "dimensionshash"})},
+ * 	uniqueConstraints={
+ * 		@ORM\UniqueConstraint(name="path_workspace_dimensions",columns={"pathhash", "workspace", "dimensionshash"}),
+ * 		@ORM\UniqueConstraint(name="identifier_workspace_dimensions",columns={"identifier", "workspace", "dimensionshash"})
+ * 	},
  * 	indexes={
  * 		@ORM\Index(name="parentpath_sortingindex",columns={"parentpathhash", "sortingindex"}),
  * 		@ORM\Index(name="identifierindex",columns={"identifier"}),
