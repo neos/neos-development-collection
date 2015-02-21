ce0e17ad4f97e0b63993c0951be0b9a85f21f5cc
Aske Ertmann <aertmann@gmail.com>
2015-02-21 21:02:30 +0100

    [FEATURE] Support dropdowns, tooltips, popovers in modules
    Adds support for using dropdowns, tooltips & popovers in backend
    modules. Use the following attribute to enable the
    functionality::
    
      data-neos-toggle="popover"
      data-neos-toggle="tooltip"
      data-neos-toggle="dropdown"
    
    The components are based on Boostrap 2.3, but all CSS classes are
    prefixed with "neos-*" to avoid conflicts with normal bootstrap usage.
    
    Change-Id: Ic013e0e6980886ad1c73a56eaae63b9ca771dbc7
    Releases: master
    Original-Commit-Hash: fd194082b0f91826ba4c2230e00b1484a6b27697

--- TYPO3.Neos/Resources/Public/Library/twitter-bootstrap/js/bootstrap-dropdown.js
+++ TYPO3.Neos/Resources/Public/Library/twitter-bootstrap/js/bootstrap-dropdown.js
@@ -1,8 +1,8 @@
 /* ============================================================
- * bootstrap-dropdown.js v2.3.1
- * http://twitter.github.com/bootstrap/javascript.html#dropdowns
+ * bootstrap-dropdown.js v2.3.2
+ * http://getbootstrap.com/2.3.2/javascript.html#dropdowns
  * ============================================================
- * Copyright 2012 Twitter, Inc.
+ * Copyright 2013 Twitter, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,146 +20,150 @@
 
 !function ($) {
 
-	"use strict"; // jshint ;_;
+  "use strict"; // jshint ;_;
 
 
-	/* DROPDOWN CLASS DEFINITION
-	 * ========================= */
+  /* DROPDOWN CLASS DEFINITION
+   * ========================= */
 
-	var toggle = '[data-toggle=dropdown]'
-		, Dropdown = function (element) {
-			var $el = $(element).on('click.dropdown.data-api', this.toggle)
-			$('html').on('click.dropdown.data-api', function () {
-				$el.parent().removeClass('open')
-			})
-		}
+  var toggle = '[data-toggle=dropdown]'
+    , Dropdown = function (element) {
+      var $el = $(element).on('click.dropdown.data-api', this.toggle)
+      $('html').on('click.dropdown.data-api', function () {
+        $el.parent().removeClass('open')
+      })
+    }
 
-	Dropdown.prototype = {
+  Dropdown.prototype = {
 
-		constructor: Dropdown
+    constructor: Dropdown
 
-		, toggle: function (e) {
-			var $this = $(this)
-				, $parent
-				, isActive
+    , toggle: function (e) {
+      var $this = $(this)
+        , $parent
+        , isActive
 
-			if ($this.is('.disabled, :disabled')) return
+      if ($this.is('.disabled, :disabled')) return
 
-			$parent = getParent($this)
+      $parent = getParent($this)
 
-			isActive = $parent.hasClass('open')
+      isActive = $parent.hasClass('open')
 
-			clearMenus()
+      clearMenus()
 
-			if (!isActive) {
-				$parent.toggleClass('open')
-			}
+      if (!isActive) {
+        if ('ontouchstart' in document.documentElement) {
+          // if mobile we we use a backdrop because click events don't delegate
+          $('<div class="dropdown-backdrop"/>').insertBefore($(this)).on('click', clearMenus)
+        }
+        $parent.toggleClass('open')
+      }
 
-			$this.focus()
+      $this.focus()
 
-			return false
-		}
+      return false
+    }
 
-		, keydown: function (e) {
-			var $this
-				, $items
-				, $active
-				, $parent
-				, isActive
-				, index
+    , keydown: function (e) {
+      var $this
+        , $items
+        , $active
+        , $parent
+        , isActive
+        , index
 
-			if (!/(38|40|27)/.test(e.keyCode)) return
+      if (!/(38|40|27)/.test(e.keyCode)) return
 
-			$this = $(this)
+      $this = $(this)
 
-			e.preventDefault()
-			e.stopPropagation()
+      e.preventDefault()
+      e.stopPropagation()
 
-			if ($this.is('.disabled, :disabled')) return
+      if ($this.is('.disabled, :disabled')) return
 
-			$parent = getParent($this)
+      $parent = getParent($this)
 
-			isActive = $parent.hasClass('open')
+      isActive = $parent.hasClass('open')
 
-			if (!isActive || (isActive && e.keyCode == 27)) {
-				if (e.which == 27) $parent.find(toggle).focus()
-				return $this.click()
-			}
+      if (!isActive || (isActive && e.keyCode == 27)) {
+        if (e.which == 27) $parent.find(toggle).focus()
+        return $this.click()
+      }
 
-			$items = $('[role=menu] li:not(.divider):visible a', $parent)
+      $items = $('[role=menu] li:not(.divider):visible a', $parent)
 
-			if (!$items.length) return
+      if (!$items.length) return
 
-			index = $items.index($items.filter(':focus'))
+      index = $items.index($items.filter(':focus'))
 
-			if (e.keyCode == 38 && index > 0) index--                                        // up
-			if (e.keyCode == 40 && index < $items.length - 1) index++                        // down
-			if (!~index) index = 0
+      if (e.keyCode == 38 && index > 0) index--                                        // up
+      if (e.keyCode == 40 && index < $items.length - 1) index++                        // down
+      if (!~index) index = 0
 
-			$items
-				.eq(index)
-				.focus()
-		}
+      $items
+        .eq(index)
+        .focus()
+    }
 
-	}
+  }
 
-	function clearMenus() {
-		$(toggle).each(function () {
-			getParent($(this)).removeClass('open')
-		})
-	}
+  function clearMenus() {
+    $('.dropdown-backdrop').remove()
+    $(toggle).each(function () {
+      getParent($(this)).removeClass('open')
+    })
+  }
 
-	function getParent($this) {
-		var selector = $this.attr('data-target')
-			, $parent
+  function getParent($this) {
+    var selector = $this.attr('data-target')
+      , $parent
 
-		if (!selector) {
-			selector = $this.attr('href')
-			selector = selector && /#/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
-		}
+    if (!selector) {
+      selector = $this.attr('href')
+      selector = selector && /#/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
+    }
 
-		$parent = selector && $(selector)
+    $parent = selector && $(selector)
 
-		if (!$parent || !$parent.length) $parent = $this.parent()
+    if (!$parent || !$parent.length) $parent = $this.parent()
 
-		return $parent
-	}
-
-
-	/* DROPDOWN PLUGIN DEFINITION
-	 * ========================== */
-
-	var old = $.fn.dropdown
-
-	$.fn.dropdown = function (option) {
-		return this.each(function () {
-			var $this = $(this)
-				, data = $this.data('dropdown')
-			if (!data) $this.data('dropdown', (data = new Dropdown(this)))
-			if (typeof option == 'string') data[option].call($this)
-		})
-	}
-
-	$.fn.dropdown.Constructor = Dropdown
+    return $parent
+  }
 
 
-	/* DROPDOWN NO CONFLICT
-	 * ==================== */
+  /* DROPDOWN PLUGIN DEFINITION
+   * ========================== */
 
-	$.fn.dropdown.noConflict = function () {
-		$.fn.dropdown = old
-		return this
-	}
+  var old = $.fn.dropdown
+
+  $.fn.dropdown = function (option) {
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('dropdown')
+      if (!data) $this.data('dropdown', (data = new Dropdown(this)))
+      if (typeof option == 'string') data[option].call($this)
+    })
+  }
+
+  $.fn.dropdown.Constructor = Dropdown
 
 
-	/* APPLY TO STANDARD DROPDOWN ELEMENTS
-	 * =================================== */
+  /* DROPDOWN NO CONFLICT
+   * ==================== */
 
-	$(document)
-		.on('click.dropdown.data-api', clearMenus)
-		.on('click.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
-		.on('click.dropdown-menu', function (e) { e.stopPropagation() })
-		.on('click.dropdown.data-api'  , toggle, Dropdown.prototype.toggle)
-		.on('keydown.dropdown.data-api', toggle + ', [role=menu]' , Dropdown.prototype.keydown)
+  $.fn.dropdown.noConflict = function () {
+    $.fn.dropdown = old
+    return this
+  }
+
+
+  /* APPLY TO STANDARD DROPDOWN ELEMENTS
+   * =================================== */
+
+  $(document)
+    .on('click.dropdown.data-api', clearMenus)
+    .on('click.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
+    .on('click.dropdown.data-api'  , toggle, Dropdown.prototype.toggle)
+    .on('keydown.dropdown.data-api', toggle + ', [role=menu]' , Dropdown.prototype.keydown)
 
 }(window.jQuery);
