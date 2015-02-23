7de9276e7acd77a1d0c0bbcc82a599ff9becce5a
Aske Ertmann <aertmann@gmail.com>
2015-02-23 22:26:22 +0100

    [FEATURE] Display file size and last modified information in list view
    Adds two new columns in the list view with file size and last modified
    information. The last modified date is shown relative to the current
    date, for easier distinguishment.
    
    Additionally makes it possible to change the sorting by clicking the
    name or the last modified table header.
    
    Change-Id: I41ee9b90b4ae3a1d290d22c07d1732d556a9ce10
    Releases: master
    Resolves: NEOS-1022
    Reviewed-on: http://review.typo3.org/37144
    Reviewed-by: Aske Ertmann <aertmann@gmail.com>
    Tested-by: Aske Ertmann <aertmann@gmail.com>
    Reviewed-by: Karsten Dambekalns <karsten@typo3.org>
    Tested-by: Karsten Dambekalns <karsten@typo3.org>
    Original-Commit-Hash: 6eb9e5550296c7f9d734106ab88409dd92a5ad4e

---
+++ TYPO3.Media/Classes/TYPO3/Media/ViewHelpers/Format/RelativeDateViewHelper.php
@@ -0,0 +1,48 @@
+<?php
+namespace TYPO3\Media\ViewHelpers\Format;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Media".           *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Renders a DateTime formatted relative to the current date
+ */
+class RelativeDateViewHelper extends AbstractViewHelper {
+
+	/**
+	 * Renders a DateTime formatted relative to the current date.
+	 * Shows the time if the date is the current date.
+	 * Shows the month and date if the date is the current year.
+	 * Shows the year/month/date if the date is not the current year.
+	 *
+	 * @param \DateTime $date
+	 * @return string an <img...> html tag
+	 * @throws \InvalidArgumentException
+	 */
+	public function render(\DateTime $date = NULL) {
+		if ($date === NULL) {
+			$date = $this->renderChildren();
+		}
+		if (!$date instanceof \DateTime) {
+			throw new \InvalidArgumentException('No valid date given,', 1424647058);
+		}
+		$now = new \TYPO3\Flow\Utility\Now();
+		if ($date->format('Y') < $now->format('Y')) {
+			return $date->format('Y M n');
+		}
+		if ($date->format('z') === $now->format('z')) {
+			return $date->format('H:i');
+		}
+		return $date->format('M n');
+	}
+}
