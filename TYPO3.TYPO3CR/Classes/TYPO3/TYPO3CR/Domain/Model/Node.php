0c5ad912427283074aa10f1cc38d339e419cc2a3
Rens Admiraal <rens.admiraal@typo3.org>
2015-03-10 18:48:07 +0100

    [BUGFIX] Prevent issues with mixed case node paths
    The node name match pattern in NodeInterface is case insensitive,
    but the utility to render a valid node name enforces them to be
    case insensitive. This is change removes the strtolower call
    to make the utility consistent with the pattern.
    
    Besides that the change checks if auto created child nodes
    already exist before creating them to prevent constraint
    errors on node copy.
    
    Releases: master, 1.2
    Resolves: NEOS-1081
    Change-Id: I4649436ae6dfa2f386d9c5b656680ba87be25002
    Original-Commit-Hash: acddbd944642f44ebbd230fbd02617763b6bf63f

--- TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Domain/Model/Node.php
+++ TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Domain/Model/Node.php
@@ -887,7 +887,10 @@ class Node implements NodeInterface, CacheAwareInterface {
 
 			foreach ($nodeType->getAutoCreatedChildNodes() as $childNodeName => $childNodeType) {
 				$childNodeIdentifier = $this->buildAutoCreatedChildNodeIdentifier($childNodeName, $newNode->getIdentifier());
-				$newNode->createNode($childNodeName, $childNodeType, $childNodeIdentifier);
+				$alreadyPresentChildNode = $newNode->getNode($childNodeName);
+				if ($alreadyPresentChildNode === NULL) {
+					$newNode->createNode($childNodeName, $childNodeType, $childNodeIdentifier);
+				}
 			}
 		}
 
