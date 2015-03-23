c26200ac0eab200cf8bff0a0f76f6b3c3e8e3fe6
Sebastian Kurfuerst <sebastian@typo3.org>
2015-03-23 16:30:39 +0100

    [FEATURE] detect file-permission issues when publishing resources
    This check during the setup finds two issues:
    
    - users who upgraded from Flow 2.x (Neos 1.x) need a different web server configuration which does not rewrite Web/_Resources
    - permission issues, where PHP is able to write a file into Web/_Resources, but the web server is not able to read them
    
    Change-Id: I484c7f19e479ede3d89f1461cec4da5b961332a1
    Resolves: NEOS-1111
    Releases: master
    Original-Commit-Hash: f8632fcffcf6c44b62bbb03914ab455bf454643a

--- TYPO3.Neos/Classes/TYPO3/Neos/Setup/Step/NeosSpecificRequirementsStep.php
+++ TYPO3.Neos/Classes/TYPO3/Neos/Setup/Step/NeosSpecificRequirementsStep.php
@@ -13,6 +13,9 @@ namespace TYPO3\Neos\Setup\Step;
 
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Configuration\ConfigurationManager;
+use TYPO3\Flow\Package\PackageManagerInterface;
+use TYPO3\Flow\Persistence\PersistenceManagerInterface;
+use TYPO3\Flow\Resource\ResourceManager;
 use TYPO3\Flow\Utility\Arrays;
 
 /**
@@ -21,10 +24,28 @@ use TYPO3\Flow\Utility\Arrays;
 class NeosSpecificRequirementsStep extends \TYPO3\Setup\Step\AbstractStep {
 
 	/**
-	 * @var \TYPO3\Flow\Configuration\Source\YamlSource
 	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Configuration\Source\YamlSource
 	 */
 	protected $configurationSource;
+
+	/**
+	 * @Flow\Inject
+	 * @var ResourceManager
+	 */
+	protected $resourceManager;
+
+	/**
+	 * @Flow\Inject
+	 * @var PackageManagerInterface
+	 */
+	protected $packageManager;
+
+	/**
+	 * @Flow\Inject
+	 * @var PersistenceManagerInterface
+	 */
+	protected $persistenceManager;
 
 	/**
 	 * {@inheritdoc}
@@ -61,6 +82,33 @@ class NeosSpecificRequirementsStep extends \TYPO3\Setup\Step\AbstractStep {
 			$hiddenField = $imageSection->createElement('imagineDriver', 'TYPO3.Form:HiddenField');
 			$hiddenField->setDefaultValue(ucfirst($foundImageHandler));
 		}
+
+		$webServerSection = $page1->createElement('resourceSection', 'TYPO3.Form:Section');
+		$webServerSection->setLabel('Web Server Configuration');
+		if ($this->canResourceBeUploadedAndFetchedAgain()) {
+			$formElement = $webServerSection->createElement('resources', 'TYPO3.Form:StaticText');
+			$formElement->setProperty('text', 'File Uploads are configured as expected.');
+			$formElement->setProperty('elementClassAttribute', 'alert alert-success');
+		} else {
+			$formElement = $webServerSection->createElement('resources', 'TYPO3.Form:StaticText');
+			$formElement->setProperty('text', "File Uploads not configured correctly. This can have the following reasons:\n\n(1) The web server does not have permissions to read files in Web/_Resources. This might occur if a parent directory is not readable.\n(2) If you upgraded from Flow 2.x or Neos 1.x, you need to adjust your Web Server Configuration and remove the rewrite rule for _Resources/Persistent. See the release notes for details.");
+			$formElement->setProperty('elementClassAttribute', 'alert alert-warning');
+		}
+	}
+
+	/**
+	 * Try to check whether resource publishing works, and the server is configured correctly or not.
+	 *
+	 * @return boolean TRUE if resource publishing worked; FALSE otherwise.
+	 * @throws \TYPO3\Flow\Resource\Exception
+	 */
+	protected function canResourceBeUploadedAndFetchedAgain() {
+		$resource = $this->resourceManager->importResourceFromContent('example-file', 'example-upload-test.txt');
+		$this->persistenceManager->whitelistObject($resource);
+		$publicResourceUri = $this->resourceManager->getPublicPersistentResourceUri($resource);
+		$result = @file_get_contents($publicResourceUri);
+
+		return $result === 'example-file';
 	}
 
 	/**
