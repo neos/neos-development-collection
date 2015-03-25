7f569d043a2444c2ba338411ac61c2dba4c30623
Bastian Waidelich <bastian@typo3.org>
2015-03-25 17:10:48 +0100

    [TASK] Reintroduce "image" argument to ImageViewHelpers
    With the new Media Management
    (Ieaaf6fa166d13c7cd0177ec381b4e74e2923c2ce) the deprecated "image"
    argument has been removed from the ``image`` and ``uri.image``
    ViewHelpers in favor of a new argument "asset".
    
    This change reintroduces the "image" argument and deprecates the
    "asset" argument.
    
    Background:
    The argument was called "image" originally, but it was renamed to
    "asset" with I8769987d34ac54a3d36e6d3374040438edf30612.
    
    Change-Id: I659963d82f1cd88178e152864eae3a99829c05df
    Related: FLOW-108
    Releases: master
    Reviewed-on: http://review.typo3.org/38171
    Reviewed-by: Bastian Waidelich <bastian@typo3.org>
    Tested-by: Bastian Waidelich <bastian@typo3.org>
    Reviewed-by: Robert Lemke <robert@typo3.org>
    Tested-by: Robert Lemke <robert@typo3.org>
    Original-Commit-Hash: 6667321e46098f1c11bf5efcac24ee2260c31d7d

--- TYPO3.Media/Classes/TYPO3/Media/ViewHelpers/ImageViewHelper.php
+++ TYPO3.Media/Classes/TYPO3/Media/ViewHelpers/ImageViewHelper.php
@@ -13,7 +13,6 @@ namespace TYPO3\Media\ViewHelpers;
 
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
-use TYPO3\Media\Domain\Model\AssetInterface;
 use TYPO3\Media\Domain\Model\ImageInterface;
 use TYPO3\Media\Domain\Model\ThumbnailSupportInterface;
 
@@ -22,46 +21,46 @@ use TYPO3\Media\Domain\Model\ThumbnailSupportInterface;
  *
  * = Examples =
  *
- * <code title="Rendering an asset as-is">
- * <typo3.media:image asset="{assetObject}" alt="a sample image without scaling" />
+ * <code title="Rendering an image as-is">
+ * <typo3.media:image image="{imageObject}" alt="a sample image without scaling" />
  * </code>
  * <output>
- * (depending on the asset, no scaling applied)
+ * (depending on the image, no scaling applied)
  * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="120" height="180" alt="a sample image without scaling" />
  * </output>
  *
  *
  * <code title="Rendering an image with scaling at a given width only">
- * <typo3.media:image asset="{assetObject}" maximumWidth="80" alt="sample" />
+ * <typo3.media:image image="{imageObject}" maximumWidth="80" alt="sample" />
  * </code>
  * <output>
- * (depending on the asset; scaled down to a maximum width of 80 pixels, keeping the aspect ratio)
+ * (depending on the image; scaled down to a maximum width of 80 pixels, keeping the aspect ratio)
  * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="80" height="120" alt="sample" />
  * </output>
  *
  *
  * <code title="Rendering an image with scaling at given width and height, keeping aspect ratio">
- * <typo3.media:image asset="{assetObject}" maximumWidth="80" maximumHeight="80" alt="sample" />
+ * <typo3.media:image image="{imageObject}" maximumWidth="80" maximumHeight="80" alt="sample" />
  * </code>
  * <output>
- * (depending on the asset; scaled down to a maximum width and height of 80 pixels, keeping the aspect ratio)
+ * (depending on the image; scaled down to a maximum width and height of 80 pixels, keeping the aspect ratio)
  * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="53" height="80" alt="sample" />
  * </output>
  *
  *
  * <code title="Rendering an image with crop-scaling at given width and height">
- * <typo3.media:image asset="{assetObject}" maximumWidth="80" maximumHeight="80" allowCropping="true" alt="sample" />
+ * <typo3.media:image image="{imageObject}" maximumWidth="80" maximumHeight="80" allowCropping="true" alt="sample" />
  * </code>
  * <output>
- * (depending on the asset; scaled down to a width and height of 80 pixels, possibly changing aspect ratio)
+ * (depending on the image; scaled down to a width and height of 80 pixels, possibly changing aspect ratio)
  * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="80" height="80" alt="sample" />
  * </output>
  *
  * <code title="Rendering an image with allowed up-scaling at given width and height">
- * <typo3.media:image asset="{assetObject}" maximumWidth="5000" allowUpScaling="true" alt="sample" />
+ * <typo3.media:image image="{imageObject}" maximumWidth="5000" allowUpScaling="true" alt="sample" />
  * </code>
  * <output>
- * (depending on the asset; scaled up or down to a width 5000 pixels, keeping aspect ratio)
+ * (depending on the image; scaled up or down to a width 5000 pixels, keeping aspect ratio)
  * <img src="_Resources/Persistent/b29[...]95d.jpeg" width="80" height="80" alt="sample" />
  * </output>
  *
@@ -102,12 +101,14 @@ class ImageViewHelper extends AbstractTagBasedViewHelper {
 		$this->registerTagAttribute('alt', 'string', 'Specifies an alternate text for an image', TRUE);
 		$this->registerTagAttribute('ismap', 'string', 'Specifies an image as a server-side image-map. Rarely used. Look at usemap instead', FALSE);
 		$this->registerTagAttribute('usemap', 'string', 'Specifies an image as a client-side image-map', FALSE);
+		// @deprecated since 2.0 use the "image" argument instead
+		$this->registerArgument('asset', 'TYPO3\Media\Domain\Model\AssetInterface', 'The image to be rendered - DEPRECATED, use "image" argument instead', FALSE);
 	}
 
 	/**
 	 * Renders an HTML img tag with a thumbnail image, created from a given asset.
 	 *
-	 * @param AssetInterface $asset The asset to be rendered as an image
+	 * @param ImageInterface $image The image to be rendered as an image
 	 * @param integer $maximumWidth Desired maximum height of the image
 	 * @param integer $maximumHeight Desired maximum width of the image
 	 * @param boolean $allowCropping Whether the image should be cropped if the given sizes would hurt the aspect ratio
@@ -115,8 +116,11 @@ class ImageViewHelper extends AbstractTagBasedViewHelper {
 	 *
 	 * @return string an <img...> html tag
 	 */
-	public function render(AssetInterface $asset = NULL, $maximumWidth = NULL, $maximumHeight = NULL, $allowCropping = FALSE, $allowUpScaling = FALSE) {
-		$thumbnailData = $this->assetService->getThumbnailUriAndSizeForAsset($asset, $maximumWidth, $maximumHeight, $allowCropping, $allowUpScaling);
+	public function render(ImageInterface $image = NULL, $maximumWidth = NULL, $maximumHeight = NULL, $allowCropping = FALSE, $allowUpScaling = FALSE) {
+		if ($image === NULL && $this->hasArgument('asset')) {
+			$image = $this->arguments['asset'];
+		}
+		$thumbnailData = $this->assetService->getThumbnailUriAndSizeForAsset($image, $maximumWidth, $maximumHeight, $allowCropping, $allowUpScaling);
 
 		$this->tag->addAttributes(array(
 			'width' => $thumbnailData['width'],
--- TYPO3.Media/Classes/TYPO3/Media/ViewHelpers/Uri/ImageViewHelper.php
+++ TYPO3.Media/Classes/TYPO3/Media/ViewHelpers/Uri/ImageViewHelper.php
@@ -12,7 +12,6 @@ namespace TYPO3\Media\ViewHelpers\Uri;
  *                                                                        */
 
 use TYPO3\Flow\Annotations as Flow;
-use TYPO3\Media\Domain\Model\AssetInterface;
 use TYPO3\Media\Domain\Model\ImageInterface;
 
 /**
@@ -20,20 +19,20 @@ use TYPO3\Media\Domain\Model\ImageInterface;
  *
  * = Examples =
  *
- * <code title="Rendering an asset path as-is">
- * {typo3.media:uri.image(asset: assetObject)}
+ * <code title="Rendering an image path as-is">
+ * {typo3.media:uri.image(image: imageObject)}
  * </code>
  * <output>
- * (depending on the asset)
+ * (depending on the image)
  * _Resources/Persistent/b29[...]95d.jpeg
  * </output>
  *
  *
- * <code title="Rendering an asset path with scaling at a given width only">
- * {typo3.media:uri.image(asset: assetObject, maximumWidth: 80)}
+ * <code title="Rendering an image path with scaling at a given width only">
+ * {typo3.media:uri.image(image: assetObject, maximumWidth: 80)}
  * </code>
  * <output>
- * (depending on the asset; has scaled keeping the aspect ratio)
+ * (depending on the image; has scaled keeping the aspect ratio)
  * _Resources/Persistent/b29[...]95d.jpeg
  * </output>
  *
@@ -64,19 +63,24 @@ class ImageViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 	 */
 	public function initializeArguments() {
 		parent::initializeArguments();
+		// @deprecated since 2.0 use the "image" argument instead
+		$this->registerArgument('asset', 'TYPO3\Media\Domain\Model\AssetInterface', 'The image to be rendered - DEPRECATED, use "image" argument instead', FALSE);
 	}
 
 	/**
 	 * Renders the path to a thumbnail image, created from a given asset.
 	 *
-	 * @param AssetInterface $asset
+	 * @param ImageInterface $image
 	 * @param integer $maximumWidth Desired maximum height of the image
 	 * @param integer $maximumHeight Desired maximum width of the image
 	 * @param boolean $allowCropping Whether the image should be cropped if the given sizes would hurt the aspect ratio
 	 * @param boolean $allowUpScaling Whether the resulting image size might exceed the size of the original image
 	 * @return string the relative image path, to be used as src attribute for <img /> tags
 	 */
-	public function render(AssetInterface $asset = NULL, $maximumWidth = NULL, $maximumHeight = NULL, $allowCropping = FALSE, $allowUpScaling = FALSE) {
-		return $this->assetService->getThumbnailUriAndSizeForAsset($asset, $maximumWidth, $maximumHeight, $allowCropping, $allowUpScaling)['src'];
+	public function render(ImageInterface $image = NULL, $maximumWidth = NULL, $maximumHeight = NULL, $allowCropping = FALSE, $allowUpScaling = FALSE) {
+		if ($image === NULL && $this->hasArgument('asset')) {
+			$image = $this->arguments['asset'];
+		}
+		return $this->assetService->getThumbnailUriAndSizeForAsset($image, $maximumWidth, $maximumHeight, $allowCropping, $allowUpScaling)['src'];
 	}
 }
