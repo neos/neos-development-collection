374c19f262787be97cd989270efd16beab2e8d25
Robert Lemke <robert@typo3.org>
2015-04-16 13:54:21 +0200

    [FEATURE] Introduce show action for Content Dimensions REST service
    This change introduces a show action for the content dimensions service.
    The new action allows for retrieval of presets of a single dimension or,
    if a filter is specified, returns only those presets which are allowed
    in combination with the given presets from other dimensions according
    to possibly defined content dimension constraints.
    
    Examples:
    
    Retrieving only presets from the "language" content dimensions:
    
      http://localhost/neos/service/content-dimensions/language
    
    Retrieving only those presets from the "language" dimensions which are
    allowed in combination with the given "country" preset:
    
      http://localhost/neos/service/content-dimensions/language?chosenDimensionPresets[country]=de
    
    Change-Id: I75c7d815e6957d6312c70cff8c159f0385aea0ad
    Releases: master
    Related: NEOS-1205
    Original-Commit-Hash: 5e2df66eb478d4dec819140dd27a5ae911e059d2

--- TYPO3.Neos/Classes/TYPO3/Neos/Controller/Service/ContentDimensionsController.php
+++ TYPO3.Neos/Classes/TYPO3/Neos/Controller/Service/ContentDimensionsController.php
@@ -44,9 +44,8 @@ class ContentDimensionsController extends ActionController {
 	protected $contentDimensionPresetSource;
 
 	/**
-	 * Returns the full content dimension presets as JSON object; see
-	 * ContentDimensionPresetSourceInterface::getAllPresets() for a format
-	 * description.
+	 * Returns the full content dimensions presets as JSON object; see
+	 * ContentDimensionPresetSourceInterface::getAllPresets() for a format description.
 	 *
 	 * @return void
 	 */
@@ -57,4 +56,40 @@ class ContentDimensionsController extends ActionController {
 			$this->view->assign('contentDimensionsPresets', $this->contentDimensionPresetSource->getAllPresets());
 		}
 	}
+
+	/**
+	 * Returns only presets of the dimension specified by $dimensionName. But even though only one dimension is returned,
+	 * the output follows the structure as described in ContentDimensionPresetSourceInterface::getAllPresets().
+	 *
+	 * It is possible to pass a selection of presets as a filter. In that case, $chosenDimensionPresets must be an array
+	 * of one or more dimension names (key) and preset names (value). The returned list will then only contain dimension
+	 * presets which are allowed in combination with the given presets.
+	 *
+	 * Example: Given that $chosenDimensionPresets = array('country' => 'US') and that a second dimension "language"
+	 * exists and $dimensionName is "language. This method will now display a list of dimension presets for the dimension
+	 * "language" which are allowed in combination with the country "US".
+	 *
+	 * @param string $dimensionName Name of the dimension to return presets for
+	 * @param array $chosenDimensionPresets An optional array of dimension names and a single preset per dimension
+	 * @return void
+	 */
+	public function showAction($dimensionName, $chosenDimensionPresets = array()) {
+		if ($chosenDimensionPresets === array()) {
+			$contentDimensionsAndPresets = $this->contentDimensionPresetSource->getAllPresets();
+			if (!isset($contentDimensionsAndPresets[$dimensionName])) {
+				$this->throwStatus(404, sprintf('The dimension %s does not exist.', $dimensionName));
+			}
+			$contentDimensionsAndPresets = array($dimensionName => $contentDimensionsAndPresets[$dimensionName]);
+		} else {
+			$contentDimensionsAndPresets = $this->contentDimensionPresetSource->getAllowedDimensionPresetsAccordingToPreselection($dimensionName, $chosenDimensionPresets);
+		}
+
+		if ($this->view instanceof JsonView) {
+			$this->view->assign('value', $contentDimensionsAndPresets);
+		} else {
+			$this->view->assign('dimensionName', $dimensionName);
+			$this->view->assign('contentDimensionsPresets', $contentDimensionsAndPresets);
+		}
+	}
+
 }
