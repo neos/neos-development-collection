7b2dbc029d3f87bd157170b2e85ef8c53ec8404d
Christopher Hlubek <hlubek@networkteam.com>
2015-07-08 18:31:38 +0200

    [BUGFIX] find() operation should support multiple filters
    This change adds support for additional attribute filters after
    instanceof filters. These were ignored before which was not intuitive
    and consistent in the way find() works in jQuery.
    
    Change-Id: I67b3bb7a8fdf84d0e3f147f3ed015ac3f95e8d1d
    Fixes: NEOS-1366
    Releases: master, 2.0
    Depends: I49a9391e2d576f21c377bf3344202f9c3de2767c
    Original-Commit-Hash: 6fd13e6e372cd22354b94b609f3a408ba4ffaf66

--- TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Eel/FlowQueryOperations/ChildrenOperation.php
+++ TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Eel/FlowQueryOperations/ChildrenOperation.php
@@ -108,15 +108,16 @@ class ChildrenOperation extends AbstractOperation {
 			}
 
 			// Only apply optimization if there's a property name filter or a instanceof filter or another filter already did optimization
-			if (isset($filter['PropertyNameFilter']) || count($instanceOfFilters) > 0 || $optimized === TRUE) {
+			if ((isset($filter['PropertyNameFilter']) || isset($filter['PathFilter'])) || count($instanceOfFilters) > 0 || $optimized === TRUE) {
 				$optimized = TRUE;
 				$filteredOutput = array();
 				$filteredOutputNodePaths = array();
 				// Optimize property name filter if present
-				if (isset($filter['PropertyNameFilter'])) {
+				if (isset($filter['PropertyNameFilter']) || isset($filter['PathFilter'])) {
+					$nodePath = isset($filter['PropertyNameFilter']) ? $filter['PropertyNameFilter'] : $filter['PathFilter'];
 					/** @var NodeInterface $contextNode */
 					foreach ($flowQuery->getContext() as $contextNode) {
-						$childNode = $contextNode->getNode($filter['PropertyNameFilter']);
+						$childNode = $contextNode->getNode($nodePath);
 						if ($childNode !== NULL && !isset($filteredOutputNodePaths[$childNode->getPath()])) {
 							$filteredOutput[] = $childNode;
 							$filteredOutputNodePaths[$childNode->getPath()] = TRUE;
--- TYPO3.TYPO3CR/Tests/Functional/Eel/FlowQueryOperations/ChildrenOperationTest.php
+++ TYPO3.TYPO3CR/Tests/Functional/Eel/FlowQueryOperations/ChildrenOperationTest.php
@@ -57,6 +57,15 @@ class ChildrenOperationTest extends AbstractNodeTest {
 	/**
 	 * @test
 	 */
+	public function pathFiltersIsSupported() {
+		$q = new FlowQuery(array($this->node));
+		$foundNodes = $q->children('teaser/dummy42, sidebar')->get();
+		$this->assertEquals(2, count($foundNodes));
+	}
+
+	/**
+	 * @test
+	 */
 	public function attributeFilterIsSupported() {
 		$q = new FlowQuery(array($this->node));
 		$foundNodes = $q->children('[title]')->get();
