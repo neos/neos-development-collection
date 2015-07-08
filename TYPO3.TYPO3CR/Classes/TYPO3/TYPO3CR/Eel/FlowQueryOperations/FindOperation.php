dfc2ceb39d7f7cb115f55103d7c8ab29494f2831
Christopher Hlubek <hlubek@networkteam.com>
2015-07-08 18:31:38 +0200

    [BUGFIX] find() operation should support multiple filters
    This change adds support for additional attribute filters after
    instanceof filters. These were ignored before which was not intuitive
    and consistent in the way find() works in jQuery.
    
    Change-Id: I67b3bb7a8fdf84d0e3f147f3ed015ac3f95e8d1d
    Fixes: NEOS-1366
    Releases: master, 2.0
    Depends: I49a9391e2d576f21c377bf3344202f9c3de2767c
    Original-Commit-Hash: 555e00e1a1ce88494d97fc003af40cbe11c010d3

--- TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Eel/FlowQueryOperations/FindOperation.php
+++ TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Eel/FlowQueryOperations/FindOperation.php
@@ -19,8 +19,36 @@ use TYPO3\TYPO3CR\Domain\Repository\NodeDataRepository;
 
 /**
  * "find" operation working on TYPO3CR nodes. This operation allows for retrieval
- * of nodes specified by a path. The current context node is also used as a context
- * for evaluating relative paths.
+ * of nodes specified by a path, identifier or node type (recursive).
+ *
+ * Example (node name):
+ *
+ * 	q(node).find('main')
+ *
+ * Example (relative path):
+ *
+ * 	q(node).find('main/text1')
+ *
+ * Example (absolute path):
+ *
+ * 	q(node).find('/sites/my-site/home')
+ *
+ * Example (identifier):
+ *
+ * 	q(node).find('#30e893c1-caef-0ca5-b53d-e5699bb8e506')
+ *
+ * Example (node type):
+ *
+ * 	q(node).find('[instanceof TYPO3.Neos.NodeTypes:Text]')
+ *
+ * Example (multiple node types):
+ *
+ * 	q(node).find('[instanceof TYPO3.Neos.NodeTypes:Text],[instanceof TYPO3.Neos.NodeTypes:Image]')
+ *
+ * Example (node type with filter):
+ *
+ * 	q(node).find('[instanceof TYPO3.Neos.NodeTypes:Text][text*="Neos"]')
+ *
  */
 class FindOperation extends AbstractOperation {
 
@@ -79,39 +107,86 @@ class FindOperation extends AbstractOperation {
 		$result = array();
 		$selectorAndFilter = $arguments[0];
 
-		try {
-			$parsedFilter = \TYPO3\Eel\FlowQuery\FizzleParser::parseFilterGroup($selectorAndFilter);
-		} catch (\Exception $e) {}
-
-		if ($selectorAndFilter[0] === '#') {
-			if (!preg_match(\TYPO3\Flow\Validation\Validator\UuidValidator::PATTERN_MATCH_UUID, substr($selectorAndFilter, 1))) {
-				throw new \TYPO3\Eel\FlowQuery\FlowQueryException('find() requires a valid identifier', 1332492263);
-			}
-			/** @var \TYPO3\TYPO3CR\Domain\Model\NodeInterface $contextNode */
-			foreach ($context as $contextNode) {
-				array_push($result, $contextNode->getContext()->getNodeByIdentifier(substr($selectorAndFilter, 1)));
-			}
-		} elseif (isset($parsedFilter['Filters'][0]['AttributeFilters']) && $parsedFilter['Filters'][0]['AttributeFilters'][0]['Operator'] === 'instanceof') {
+		$parsedFilter = NULL;
+		$parsedFilter = \TYPO3\Eel\FlowQuery\FizzleParser::parseFilterGroup($selectorAndFilter);
+		if (isset($parsedFilter['Filters']) && $this->hasOnlyInstanceOfFilters($parsedFilter['Filters'])) {
 			$nodeTypes = array();
 			foreach ($parsedFilter['Filters'] as $filter) {
-				if (isset($filter['AttributeFilters']) && $filter['AttributeFilters'][0]['Operator'] === 'instanceof') {
-					$nodeTypes[] = $filter['AttributeFilters'][0]['Operand'];
-				}
+				$nodeTypes[] = $filter['AttributeFilters'][0]['Operand'];
 			}
 			/** @var \TYPO3\TYPO3CR\Domain\Model\NodeInterface $contextNode */
 			foreach ($context as $contextNode) {
 				$result = array_merge($result, $this->nodeDataRepository->findByParentAndNodeTypeInContext($contextNode->getPath(), implode(',', $nodeTypes), $contextNode->getContext(), TRUE));
 			}
 		} else {
-			/** @var \TYPO3\TYPO3CR\Domain\Model\NodeInterface $contextNode */
-			foreach ($context as $contextNode) {
-				$node = $contextNode->getNode($selectorAndFilter);
-				if ($node !== NULL) {
-					array_push($result, $node);
+			foreach ($parsedFilter['Filters'] as $filter) {
+				$filterResults = array();
+				$generatedNodes = FALSE;
+				if (isset($filter['IdentifierFilter'])) {
+					if (!preg_match(\TYPO3\Flow\Validation\Validator\UuidValidator::PATTERN_MATCH_UUID, $filter['IdentifierFilter'])) {
+						throw new \TYPO3\Eel\FlowQuery\FlowQueryException('find() requires a valid identifier', 1332492263);
+					}
+					/** @var \TYPO3\TYPO3CR\Domain\Model\NodeInterface $contextNode */
+					foreach ($context as $contextNode) {
+						$filterResults = array($contextNode->getContext()->getNodeByIdentifier($filter['IdentifierFilter']));
+					}
+					$generatedNodes = TRUE;
+				} elseif (isset($filter['IdentifierFilter'])) {
+					if (!preg_match(\TYPO3\Flow\Validation\Validator\UuidValidator::PATTERN_MATCH_UUID, $filter['IdentifierFilter'])) {
+						throw new \TYPO3\Eel\FlowQuery\FlowQueryException('find() requires a valid identifier', 1332492263);
+					}
+					/** @var \TYPO3\TYPO3CR\Domain\Model\NodeInterface $contextNode */
+					foreach ($context as $contextNode) {
+						$filterResults = array($contextNode->getContext()->getNodeByIdentifier($filter['IdentifierFilter']));
+					}
+					$generatedNodes = TRUE;
+				} elseif (isset($filter['PropertyNameFilter']) || isset($filter['PathFilter'])) {
+					$nodePath = isset($filter['PropertyNameFilter']) ? $filter['PropertyNameFilter'] : $filter['PathFilter'];
+					foreach ($context as $contextNode) {
+						$node = $contextNode->getNode($nodePath);
+						if ($node !== NULL) {
+							array_push($filterResults, $node);
+						}
+					}
+					$generatedNodes = TRUE;
 				}
+
+				if (isset($filter['AttributeFilters']) && $filter['AttributeFilters'][0]['Operator'] === 'instanceof') {
+					foreach ($context as $contextNode) {
+						$filterResults = array_merge($filterResults, $this->nodeDataRepository->findByParentAndNodeTypeInContext($contextNode->getPath(), $filter['AttributeFilters'][0]['Operand'], $contextNode->getContext(), TRUE));
+					}
+					unset($filter['AttributeFilters'][0]);
+					$generatedNodes = TRUE;
+				}
+				if (isset($filter['AttributeFilters']) && count($filter['AttributeFilters']) > 0) {
+					if (!$generatedNodes) {
+						throw new \TYPO3\Eel\FlowQuery\FlowQueryException('find() needs an identifier, path or instanceof filter for the first filter part', 1436884196);
+					}
+					$filterQuery = new FlowQuery($filterResults);
+					foreach ($filter['AttributeFilters'] as $attributeFilter) {
+						$filterQuery->pushOperation('filter', array($attributeFilter['text']));
+					}
+					$filterResults = $filterQuery->get();
+				}
+				$result = array_merge($result, $filterResults);
 			}
 		}
 
 		$flowQuery->setContext(array_unique($result));
+	}
+
+	/**
+	 * Check if the parsed filters only contain instanceof filters (e.g. "[instanceof Foo],[instanceof Bar]")
+	 *
+	 * @param array $filters
+	 * @return boolean
+	 */
+	protected function hasOnlyInstanceOfFilters(array $filters) {
+		foreach ($filters as $filter) {
+			if (!isset($filter['AttributeFilters']) || count($filter['AttributeFilters']) !== 1 || $filter['AttributeFilters'][0]['Operator'] !== 'instanceof') {
+				return FALSE;
+			}
+		}
+		return TRUE;
 	}
 }
