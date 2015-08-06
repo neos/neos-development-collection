f7fc7b83c3a400152c98cf0c4254baab52af88f5
Aske Ertmann <aertmann@gmail.com>
2015-08-06 18:51:58 +0200

    [BUGFIX] Missing node type configuration causes JS load error
    If the node type configuration cannot be found for the current document
    node a fatal error is thrown in the navigate component. This change makes
    sure to properly test if the configuration is available before using it,
    to prevent such errors.
    
    Change-Id: Ic0e1d630d05c0087209ab3205eb94466783930e8
    Releases: master, 2.0
    Fixes: NEOS-1478
    (cherry picked from commit 4417789175c4476538d4055fc3929f2992288155)
    Original-Commit-Hash: 8ef445fc052bd200d8d12948ee89a1f64ef14e20

--- TYPO3.Neos/Resources/Public/JavaScript/Content/Navigate/ContextStructureTree.js
+++ TYPO3.Neos/Resources/Public/JavaScript/Content/Navigate/ContextStructureTree.js
@@ -130,8 +130,8 @@ define(
 			var page = InstanceWrapper.entities.get(InstanceWrapper.service('rdfa').getElementSubject(documentMetadata)),
 				namespace = Configuration.get('TYPO3_NAMESPACE'),
 				pageTitle = typeof page !== 'undefined' && typeof page.get(namespace + 'title') !== 'undefined' ? page.get(namespace + 'title') : this.pageNodePath,
-				documentNodeType = (page ? page.get('typo3:_nodeType'): 'TYPO3.Neos.NodeTypes:Page'), // TODO: This fallback to TYPO3.Neos.NodeTypes:Page should go away, but currently in some rare cases "page" is not yet initialized. In order to fix this loading order issue, we need to re-structure the tree, though.
-				documentNodeTypeConfiguration = NodeTypeService.getNodeTypeDefinition(documentNodeType);
+				nodeType = documentMetadata.attr('typeof').substr(6),
+				nodeTypeConfiguration = NodeTypeService.getNodeTypeDefinition(nodeType);
 
 			this.set('treeConfiguration', $.extend(true, this.get('treeConfiguration'), {
 				parent: this,
@@ -145,8 +145,8 @@ define(
 						select: false,
 						active: false,
 						unselectable: true,
-						nodeType: documentNodeType,
-						nodeTypeLabel: documentNodeTypeConfiguration.label,
+						nodeType: nodeType,
+						nodeTypeLabel: nodeTypeConfiguration ? nodeTypeConfiguration.label : '',
 						addClass: 'typo3-neos-page',
 						iconClass: 'icon-sitemap'
 					}
@@ -168,11 +168,12 @@ define(
 						$(nodeSpan).addClass('neos-dynatree-dirty');
 					}
 				},
+
 				onCustomRender: function(node) {
 					var nodeTypeLabel = I18n.translate(node.data.nodeTypeLabel),
 						tooltip = node.data.title;
 
-					if (nodeTypeLabel === '' || tooltip.indexOf(nodeTypeLabel) === -1) {
+					if (nodeTypeLabel !== '' && tooltip.indexOf(nodeTypeLabel) === -1) {
 						tooltip += ' (' + nodeTypeLabel + ')';
 					}
 					node.data.tooltip = tooltip;
