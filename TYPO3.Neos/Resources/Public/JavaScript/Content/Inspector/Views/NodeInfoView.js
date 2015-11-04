fa7527749d123591eeb56a21454001c0c4e93a2f
Aske Ertmann <aertmann@gmail.com>
2015-11-04 17:07:09 +0100

    [TASK] Adjust formatDate template helper

--- TYPO3.Neos/Resources/Public/JavaScript/Content/Inspector/Views/NodeInfoView.js
+++ TYPO3.Neos/Resources/Public/JavaScript/Content/Inspector/Views/NodeInfoView.js
@@ -8,10 +8,10 @@ function(
 	Ember,
 	template
 ) {
-	Ember.Handlebars.registerBoundHelper('formatDate', function(value) {
+	Ember.HTMLBars.helpers.registerHelper('formatDate', function(value) {
 		// Fallback if the value is undefined,
 		// since the Date Object requires a string/Datetime-Object as the first parameter.
-		if(value) {
+		if (value) {
 			return new Date(value).toISOString().slice(0, 16).replace('T', ' ');
 		} else {
 			return '';
