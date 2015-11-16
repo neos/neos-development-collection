39274aabb18bc33735c8da09bb199fe014ad28d7
Aske Ertmann <aertmann@gmail.com>
2015-11-16 09:30:50 +0100

    [FEATURE] Thumbnail controller for generating uninitialized thumbnails
    Controller when given a thumbnail object generates the thumbnail and redirects
    to the resource. If the thumbnail is already generated, it skips the generation
    and just redirects to the resource.
    
    Related: NEOS-1668

---
+++ TYPO3.Media/Classes/TYPO3/Media/Controller/ThumbnailController.php
@@ -0,0 +1,60 @@
+<?php
+namespace TYPO3\Media\Controller;
+
+/*
+ * This file is part of the TYPO3.Media package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Persistence\PersistenceManagerInterface;
+use TYPO3\Flow\Resource\ResourceManager;
+use TYPO3\Media\Domain\Model\Thumbnail;
+use TYPO3\Media\Domain\Repository\ThumbnailRepository;
+
+/**
+ * Controller for asynchronous thumbnail handling
+ *
+ * @Flow\Scope("singleton")
+ */
+class ThumbnailController extends \TYPO3\Flow\Mvc\Controller\ActionController
+{
+    /**
+     * @Flow\Inject
+     * @var ResourceManager
+     */
+    protected $resourceManager;
+
+    /**
+     * @Flow\Inject
+     * @var PersistenceManagerInterface
+     */
+    protected $persistenceManager;
+
+    /**
+     * @Flow\Inject
+     * @var ThumbnailRepository
+     */
+    protected $thumbnailRepository;
+
+    /**
+     * Generate thumbnail and redirect to resource URI
+     *
+     * @param Thumbnail $thumbnail
+     * @return void
+     */
+    public function thumbnailAction(Thumbnail $thumbnail)
+    {
+        if ($thumbnail->getResource() === NULL) {
+            $thumbnail->refresh();
+            $this->persistenceManager->whiteListObject($thumbnail);
+            $this->thumbnailRepository->update($thumbnail);
+        }
+        $this->redirectToUri($this->resourceManager->getPublicPersistentResourceUri($thumbnail->getResource()), 0, 302);
+    }
+}
