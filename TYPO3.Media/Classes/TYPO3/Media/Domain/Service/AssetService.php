6f97277e2d3b1fea6b582bfbfd8bedf4f31de652
Aske Ertmann <aertmann@gmail.com>
2015-11-16 09:06:50 +0100

    [FEATURE] Support asynchronous thumbnail generation in asset service
    Adds a ``async`` argument to ``getThumbnailUriAndSizeForAsset`` to
    enable returning a URI to the thumbnail controller for asynchronously
    generation of the thumbnail when requested.
    
    Related: NEOS-1668

--- TYPO3.Media/Classes/TYPO3/Media/Domain/Service/AssetService.php
+++ TYPO3.Media/Classes/TYPO3/Media/Domain/Service/AssetService.php
@@ -12,10 +12,13 @@ namespace TYPO3\Media\Domain\Service;
  */
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Mvc\ActionRequest;
+use TYPO3\Flow\Mvc\Routing\UriBuilder;
 use TYPO3\Flow\Resource\ResourceManager;
 use TYPO3\Media\Domain\Model\AssetInterface;
 use \TYPO3\Media\Domain\Model\ImageInterface;
 use TYPO3\Media\Domain\Model\ThumbnailConfiguration;
+use TYPO3\Media\Exception\AssetServiceException;
 
 class AssetService
 {
@@ -32,21 +35,42 @@ class AssetService
     protected $thumbnailService;
 
     /**
+     * @Flow\Inject
+     * @var UriBuilder
+     */
+    protected $uriBuilder;
+
+    /**
      * Calculates the dimensions of the thumbnail to be generated and returns the thumbnail URI.
      * In case of Images this is a thumbnail of the image, in case of other assets an icon representation.
      *
      * @param AssetInterface $asset
      * @param ThumbnailConfiguration $configuration
+     * @param boolean $async Return asynchronous image URI in case the requested image does not exist already
+     * @param ActionRequest $request Request argument must be provided for asynchronous thumbnails
      * @return array with keys "width", "height" and "src"
+     * @throws AssetServiceException
      */
-    public function getThumbnailUriAndSizeForAsset(AssetInterface $asset, ThumbnailConfiguration $configuration)
+    public function getThumbnailUriAndSizeForAsset(AssetInterface $asset, ThumbnailConfiguration $configuration, $async = false, ActionRequest $request = null)
     {
         if ($asset instanceof ImageInterface) {
-            $thumbnailImage = $this->thumbnailService->getThumbnail($asset, $configuration);
+            $thumbnailImage = $this->thumbnailService->getThumbnail($asset, $configuration, $async);
+            if ($async === true && $thumbnailImage->getResource() === null) {
+                if ($request === null) {
+                    throw new AssetServiceException('Request argument must be provided for async thumbnails.', 1447660835);
+                }
+                $this->uriBuilder->setRequest($request->getMainRequest());
+                $uri = $this->uriBuilder
+                    ->reset()
+                    ->setCreateAbsoluteUri(true)
+                    ->uriFor('thumbnail', array('thumbnail' => $thumbnailImage), 'Thumbnail', 'TYPO3.Media');
+            } else {
+                $uri = $this->resourceManager->getPublicPersistentResourceUri($thumbnailImage->getResource());
+            }
             $thumbnailData = array(
                 'width' => $thumbnailImage->getWidth(),
                 'height' => $thumbnailImage->getHeight(),
-                'src' => $this->resourceManager->getPublicPersistentResourceUri($thumbnailImage->getResource())
+                'src' => $uri
             );
         } else {
             $thumbnailData = $this->getAssetThumbnailImage($asset, $configuration->getWidth() ?: $configuration->getMaximumWidth(), $configuration->getHeight() ?: $configuration->getMaximumHeight());
