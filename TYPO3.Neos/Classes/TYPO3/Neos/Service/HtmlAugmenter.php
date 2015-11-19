d3968af6a5840a1a49a365844467fb5c8e8439f8
Christian Müller <christian@neos.io>
2015-11-19 22:22:50 +0100

    [SECURITY] Use double encoding in HtmlAugmenter
    When entering HTML tags into text elements (using Aloha), the tags were
    correctly escaped by Neos. Still the tags would be in the DOM as "real"
    tags when switching to a preview mode in the backend.
    
    This is caused by VIE and is resolved by double-encoding when using
    htmlspecialchars() on the attribute values used to pass data to VIE.
    
    Fixes: NEOS-1655

--- TYPO3.Neos/Classes/TYPO3/Neos/Service/HtmlAugmenter.php
+++ TYPO3.Neos/Classes/TYPO3/Neos/Service/HtmlAugmenter.php
@@ -108,7 +108,7 @@ class HtmlAugmenter
     {
         $renderedAttributes = '';
         foreach ($attributes as $attributeName => $attributeValue) {
-            $encodedAttributeName = htmlspecialchars($attributeName, ENT_COMPAT, 'UTF-8', false);
+            $encodedAttributeName = htmlspecialchars($attributeName, ENT_COMPAT, 'UTF-8');
             if ($attributeValue === null) {
                 $renderedAttributes .= ' ' . $encodedAttributeName;
             } else {
@@ -116,7 +116,7 @@ class HtmlAugmenter
                     throw new Exception(sprintf('Only attributes with string values can be rendered, attribute %s is of type %s', $attributeName, gettype($attributeValue)));
                 }
 
-                $encodedAttributeValue = htmlspecialchars((string)$attributeValue, ENT_COMPAT, 'UTF-8', false);
+                $encodedAttributeValue = htmlspecialchars((string)$attributeValue, ENT_COMPAT, 'UTF-8');
                 $renderedAttributes .= ' ' . $encodedAttributeName . '="' . $encodedAttributeValue . '"';
             }
         }
