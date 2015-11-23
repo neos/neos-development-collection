neos/neos-development-collection

commit 8c87918e41cd0d445869f5acc77ece01b0a0764a

[BUGFIX] Translate node type label in PluginView master plugin selection
Fixes: NEOS-1543

TYPO3.Neos/Classes/TYPO3/Neos/Controller/Backend/ContentController.php
@@ -12,6 +12,7 @@ namespace TYPO3\Neos\Controller\Backend;
  */
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\I18n\EelHelper\TranslationHelper;
 use TYPO3\Flow\Property\PropertyMappingConfiguration;
 use TYPO3\Flow\Property\TypeConverter\PersistentObjectConverter;
 use TYPO3\Media\Domain\Model\Asset;
@@ -400,7 +401,14 @@ class ContentController extends ActionController
                 if ($page === null) {
                     continue;
                 }
-                $masterPlugins[$pluginNode->getPath()] = sprintf('"%s" on page "%s"', $pluginNode->getNodeType()->getLabel(), $page->getLabel());
+                $translationHelper = new TranslationHelper();
+                $masterPlugins[$pluginNode->getIdentifier()] = $translationHelper->translate(
+                    'masterPlugins.nodeTypeOnPageLabel',
+                    null,
+                    ['nodeTypeName' => $translationHelper->translate($pluginNode->getNodeType()->getLabel()), 'pageLabel' => $page->getLabel()],
+                    'Main',
+                    'TYPO3.Neos'
+               );
             }
         }
         return json_encode((object) $masterPlugins);

TYPO3.Neos/Resources/Private/Translations/en/Main.xlf
@@ -457,6 +457,11 @@
 			<trans-unit id="error.invalidNodeType.description" xml:space="preserve">
 				<source>The configuration of the NodeType that is supposed to be rendered here is not available. Probably you renamed the NodeType and are missing a migration or you simply misspelled it.</source>
 			</trans-unit>
+
+			<!-- Miscellaneous -->
+			<trans-unit id="masterPlugins.nodeTypeOnPageLabel" xml:space="preserve">
+				<source>"{nodeTypeName}" on page "{pageLabel}"</source>
+			</trans-unit>
 		</body>
 	</file>
 </xliff>

commit c44872da659d017cc36244ca061af5ea6008ec45

[BUGFIX] Fix selecting master view option in plugin view
Regression introduced in 8c87918e41cd0d445869f5acc77ece01b0a0764a
causing the selection of master view to cause a fatal error in plugin views.

TYPO3.Neos/Classes/TYPO3/Neos/Controller/Backend/ContentController.php
@@ -400,7 +400,7 @@ class ContentController extends ActionController
                     continue;
                 }
                 $translationHelper = new TranslationHelper();
-                $masterPlugins[$pluginNode->getIdentifier()] = $translationHelper->translate(
+                $masterPlugins[$pluginNode->getPath()] = $translationHelper->translate(
                     'masterPlugins.nodeTypeOnPageLabel',
                     null,
                     ['nodeTypeName' => $translationHelper->translate($pluginNode->getNodeType()->getLabel()), 'pageLabel' => $page->getLabel()],