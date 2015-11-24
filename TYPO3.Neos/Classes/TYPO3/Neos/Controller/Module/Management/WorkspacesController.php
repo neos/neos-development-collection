9724c1246841c30f1096bc4db98c46a5b342ed63
Robert Lemke <robert@flownative.com>
2015-11-24 17:01:03 +0100

    [BUGFIX] Broken workspace managment backend module
    This fixes a couple of issues with the new workspace management features
    and improves the user interface in the workspace review screen.
    
    Editing the owner of a workspace resulted in a fatal error due to
    a wrong method signature. Ownership can now be transferred as intended.
    
    The rebase and redirect action (which can convenientely be triggered
    through an edit button in the review screen) complained about a wrong
    "$this" in a fatal error. Removed $this and now that works.
    
    At the bottom of the review workspace changes screen a small help
    message explained what would happen when a user presses one of the
    buttons. Removed that fineprint and now let the buttons speak for
    themselves.

--- TYPO3.Neos/Classes/TYPO3/Neos/Controller/Module/Management/WorkspacesController.php
+++ TYPO3.Neos/Classes/TYPO3/Neos/Controller/Module/Management/WorkspacesController.php
@@ -224,12 +224,16 @@ class WorkspacesController extends AbstractModuleController
      * Update a workspace
      *
      * @param Workspace $workspace A workspace to update
+     * @param User $newOwner New owner for the workspace, if any
      * @return void
      */
-    public function updateAction(Workspace $workspace)
+    public function updateAction(Workspace $workspace, $newOwner = null)
     {
         if ($workspace->getTitle() === '') {
             $workspace->setTitle($workspace->getName());
+        }
+        if ($newOwner !== null) {
+            $workspace->setOwner($newOwner);
         }
 
         $this->workspaceRepository->update($workspace);
@@ -292,7 +296,7 @@ class WorkspacesController extends AbstractModuleController
     public function rebaseAndRedirectAction(NodeInterface $targetNode, Workspace $targetWorkspace)
     {
         $currentAccount = $this->securityContext->getAccount();
-        $personalWorkspace = $this->workspaceRepository->$this->findOneByName('user-' . $currentAccount->getAccountIdentifier());
+        $personalWorkspace = $this->workspaceRepository->findOneByName('user-' . $currentAccount->getAccountIdentifier());
         /** @var Workspace $personalWorkspace */
 
         if ($this->publishingService->getUnpublishedNodesCount($personalWorkspace) > 0) {
@@ -500,7 +504,7 @@ class WorkspacesController extends AbstractModuleController
         $baseWorkspaceOptions = [];
         foreach ($this->workspaceRepository->findAll() as $workspace) {
             /** @var Workspace $workspace */
-            if (!$workspace->isPersonalWorkspace() && $workspace !== $excludedWorkspace) {
+            if (!$workspace->isPersonalWorkspace() && $workspace !== $excludedWorkspace && ($workspace->isPublicWorkspace() || $workspace->isInternalWorkspace() || $this->userService->currentUserCanManageWorkspace($workspace))) {
                 $baseWorkspaceOptions[$workspace->getName()] = $workspace->getTitle();
             }
         }
