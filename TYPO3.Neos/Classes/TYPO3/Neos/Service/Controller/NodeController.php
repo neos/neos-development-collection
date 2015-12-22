d227108002310ff7bc19a325459bed989d73c129
Aske Ertmann <aertmann@gmail.com>
2015-12-22 10:41:05 +0100

    BUGFIX: Show node after copying and pasting
    Redirect to copied node instead of target node
    
    NEOS-1506 #close

--- TYPO3.Neos/Classes/TYPO3/Neos/Service/Controller/NodeController.php
+++ TYPO3.Neos/Classes/TYPO3/Neos/Service/Controller/NodeController.php
@@ -302,8 +302,8 @@ class NodeController extends AbstractServiceController
      */
     public function copyAndRenderAction(Node $node, Node $targetNode, $position, $typoScriptPath, $nodeName = null)
     {
-        $this->nodeOperations->copy($node, $targetNode, $position, $nodeName);
-        $this->redirectToRenderNode($targetNode, $typoScriptPath);
+        $copiedNode = $this->nodeOperations->copy($node, $targetNode, $position, $nodeName);
+        $this->redirectToRenderNode($copiedNode, $typoScriptPath);
     }
 
     /**
