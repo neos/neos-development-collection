0bc192d5be7fc94b4a3baad5c44fbd62f26b6bc5
Dmitri Pisarev <dimaip@gmail.com>
2015-12-28 18:28:09 +0300

    TASK: Update to RSVP promise format in NodeActions
    `HttpClient.getResource` returns RSVP.Promise and not jQuery promise.

--- TYPO3.Neos/Resources/Public/JavaScript/Content/Model/NodeActions.js
+++ TYPO3.Neos/Resources/Public/JavaScript/Content/Model/NodeActions.js
@@ -177,8 +177,7 @@ define(
 					}
 
 					that._insertNode(result, localXhr, nodeType, collection, position, referenceNodeEntity, clipboard.type === 'cut');
-				}
-			).fail(
+				},
 				function(error) {
 					Notification.error('Failed to perform node action');
 					console.error('Failed to perform node action', error);
@@ -262,8 +261,7 @@ define(
 			).then(
 				function(result) {
 					that._insertNode(result, localXhr, nodeType, collection, position, referenceNodeEntity, false);
-				}
-			).fail(
+				},
 				function(error) {
 					Notification.error('Failed to perform node action');
 					console.error('Failed to perform node action', error);
