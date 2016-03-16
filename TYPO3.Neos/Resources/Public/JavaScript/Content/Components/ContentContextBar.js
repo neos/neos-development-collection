df7cd0735dd797504ca5acd05089bc9a2ff15afe
Karsten Dambekalns <karsten@dambekalns.de>
2016-03-16 12:18:03 +0100

    TASK: Update previewUri correctly on initial load
    Using the pageLoaded event does not work on initial page load, just like
    calling the update function in init().
    
    So we add observers to act on the properties the computed property was
    bound to earlier.

--- TYPO3.Neos/Resources/Public/JavaScript/Content/Components/ContentContextBar.js
+++ TYPO3.Neos/Resources/Public/JavaScript/Content/Components/ContentContextBar.js
@@ -51,6 +51,6 @@ define(
         } else {
           this.set('previewUri', location.href.replace(/@[A-Za-z0-9;&,\-_=]+/g, '@' + targetWorkspaceName));
         }
-      }
-    });
+      }.observes('targetWorkspaceController.targetWorkspace', 'contentDimensionController.selectedDimensions')
+    })
   });
