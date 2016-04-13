neos/neos-development-collection

commit c2620da5ea121f7dcd39886717332caaf515edfa

TASK: Use bootstrap tooltip for Aloha buttons

TYPO3.Neos/Resources/Public/JavaScript/InlineEditing/Editors/Aloha/UiPlugin/button.js
@@ -1,25 +1,0 @@
-define([
-	'originalButton',
-	'ui/utils'
-], function (
-	OriginalButton,
-	Utils
-) {
-	'use strict';
-
-	// We patch this class to remove jQuery UI tooltip, replacing it by native browser titles.
-	return OriginalButton.extend({
-		init: function () {
-			this.createButtonElement();
-			Utils.makeButton(this.buttonElement, this)
-				.uibutton('widget')
-				.click(Aloha.jQuery.proxy(function () {
-					this._onClick();
-				}, this));
-		},
-
-		closeTooltip: function() {},
-
-		adoptParent: function () {}
-	});
-});

TYPO3.Neos/Resources/Public/JavaScript/aloha.js
@@ -189,8 +189,7 @@ function(
 					},
 					'*': {
 						'ui/ui-plugin': '../../JavaScript/InlineEditing/Editors/Aloha/UiPlugin/ui-plugin',
-						'ui/multiSplit': '../../JavaScript/InlineEditing/Editors/Aloha/UiPlugin/multiSplit',
-						'ui/button': '../../JavaScript/InlineEditing/Editors/Aloha/UiPlugin/button'
+						'ui/multiSplit': '../../JavaScript/InlineEditing/Editors/Aloha/UiPlugin/multiSplit'
 					}
 				},
 				paths: {

TYPO3.Neos/Resources/Public/Library/aloha/aloha.js
@@ -35172,7 +35172,7 @@ function (jQuery, Component, Utils) {
 			Utils.makeButton(this.buttonElement, this)
 				.uibutton('widget')
 				.tooltip({
-					tooltipClass: 'aloha aloha-ui-tooltip',
+					placement: 'bottom',
 					position: {
 						my: 'left top',
 						at: 'right bottom'
@@ -35195,7 +35195,7 @@ function (jQuery, Component, Utils) {
 
 		closeTooltip: function () {
 			// 'close', /*event*/, /*force*/
-			this.buttonElement.tooltip('close', null, true);
+			this.buttonElement.tooltip('hide');
 		},
 
 		/**
@@ -48576,7 +48576,7 @@ define('ui/menuButton',[
 			wrapper.children('[title]').removeAttr('title').end()
 				.attr('title', props.tooltip)
 				.tooltip({
-					tooltipClass: 'aloha aloha-ui-tooltip',
+					placement: 'bottom',
 					position: {
 						my: 'left top',
 						at: 'right bottom'
@@ -48585,10 +48585,10 @@ define('ui/menuButton',[
 
 			if (props.menu) {
 				wrapper.on('menushown', function() {
-					wrapper.tooltip('disable');
+					wrapper.tooltip('hide');
 				});
 				wrapper.on('menuhidden', function() {
-					wrapper.tooltip('enable');
+					wrapper.tooltip('show');
 				});
 			}
 		}

TYPO3.Neos/Scripts/Gruntfile.js
@@ -116,6 +116,12 @@ module.exports = function (grunt) {
 						src = src.replace(/var componentNameByElement = {\n/, "var componentNameByElement = { 'code': 'code'," + "\n");
 						src = src.replace("availableButtons: [ 'u',", "availableButtons: [ 'code', 'u',");
 
+						// tooltips
+						src = src.replace(/tooltipClass: 'aloha aloha-ui-tooltip',/g, "placement: 'bottom',");
+						src = src.replace(".tooltip('close', null, true);", ".tooltip('hide');");
+						src = src.replace(".tooltip('disable');", ".tooltip('hide');");
+						src = src.replace(".tooltip('enable');", ".tooltip('show');");
+
 						return src;
 					}
 				}

commit 5c9d787c3f6e470b70303c68d046e23bdaa0262b

BUGFIX: Prevent incorrect toggling of table mode
Regression introduced with c2620da5ea121f7dcd39886717332caaf515edfa
causing table mode being enabled in the inspector when enabling Aloha
repeatedly.

TYPO3.Neos/Resources/Public/JavaScript/InlineEditing/Editors/Aloha/UiPlugin/button.js
@@ -1,0 +1,11 @@
+define([
+	'originalButton'
+], function (
+	OriginalButton
+) {
+	'use strict';
+	return OriginalButton.extend({
+		// Needed for the custom table buttons
+		adoptParent: function () {}
+	});
+});

TYPO3.Neos/Resources/Public/JavaScript/aloha.js
@@ -189,7 +189,8 @@ function(
 					},
 					'*': {
 						'ui/ui-plugin': '../../JavaScript/InlineEditing/Editors/Aloha/UiPlugin/ui-plugin',
-						'ui/multiSplit': '../../JavaScript/InlineEditing/Editors/Aloha/UiPlugin/multiSplit'
+						'ui/multiSplit': '../../JavaScript/InlineEditing/Editors/Aloha/UiPlugin/multiSplit',
+						'ui/button': '../../JavaScript/InlineEditing/Editors/Aloha/UiPlugin/button'
 					}
 				},
 				paths: {