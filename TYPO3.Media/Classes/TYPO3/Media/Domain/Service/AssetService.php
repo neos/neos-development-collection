36a7d7bb2c9c02a22675359be93c41a1098397fa
Rens Admiraal <rens@simplyadmire.com>
2016-04-26 23:09:47 +0200

    FEATURE: Asset usage strategies
    This change allows to register asset usage strategies that
    define if an asset is still in use and how many times.

--- TYPO3.Media/Classes/TYPO3/Media/Domain/Service/AssetService.php
+++ TYPO3.Media/Classes/TYPO3/Media/Domain/Service/AssetService.php
@@ -14,13 +14,25 @@ namespace TYPO3\Media\Domain\Service;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Mvc\ActionRequest;
 use TYPO3\Flow\Mvc\Routing\UriBuilder;
+use TYPO3\Flow\Object\ObjectManagerInterface;
+use TYPO3\Flow\Persistence\RepositoryInterface;
+use TYPO3\Flow\Reflection\ReflectionService;
 use TYPO3\Flow\Resource\ResourceManager;
+use TYPO3\Flow\Utility\Arrays;
 use TYPO3\Media\Domain\Model\AssetInterface;
 use TYPO3\Media\Domain\Model\ImageInterface;
 use TYPO3\Media\Domain\Model\Thumbnail;
 use TYPO3\Media\Domain\Model\ThumbnailConfiguration;
+use TYPO3\Media\Domain\Repository\AssetRepository;
+use TYPO3\Media\Domain\Strategy\AssetUsageStrategyInterface;
 use TYPO3\Media\Exception\AssetServiceException;
 
+/**
+ * An asset service that handles for example commands on assets, retrieves information
+ * about usage of assets and rendering thumbnails.
+ *
+ * @Flow\Scope("singleton")
+ */
 class AssetService
 {
     /**
@@ -42,6 +54,41 @@ class AssetService
     protected $uriBuilder;
 
     /**
+     * @Flow\Inject
+     * @var ObjectManagerInterface
+     */
+    protected $objectManager;
+
+    /**
+     * @Flow\Inject
+     * @var ReflectionService
+     */
+    protected $reflectionService;
+
+    /**
+     * @var array
+     */
+    protected $usageStrategies;
+
+    /**
+     * Returns the repository for an asset
+     *
+     * @param AssetInterface $asset
+     * @return RepositoryInterface
+     * @api
+     */
+    public function getRepository(AssetInterface $asset)
+    {
+        $assetRepositoryClassName = str_replace('\\Model\\', '\\Repository\\', get_class($asset)) . 'Repository';
+
+        if (class_exists($assetRepositoryClassName)) {
+            return $this->objectManager->get($assetRepositoryClassName);
+        }
+
+        return $this->objectManager->get(AssetRepository::class);
+    }
+
+    /**
      * Calculates the dimensions of the thumbnail to be generated and returns the thumbnail URI.
      * In case of Images this is a thumbnail of the image, in case of other assets an icon representation.
      *
@@ -53,7 +100,7 @@ class AssetService
      */
     public function getThumbnailUriAndSizeForAsset(AssetInterface $asset, ThumbnailConfiguration $configuration, ActionRequest $request = null)
     {
-        $thumbnailImage =  $this->thumbnailService->getThumbnail($asset, $configuration);
+        $thumbnailImage = $this->thumbnailService->getThumbnail($asset, $configuration);
         if (!$thumbnailImage instanceof ImageInterface) {
             return null;
         }
@@ -80,5 +127,91 @@ class AssetService
             'height' => $thumbnailImage->getHeight(),
             'src' => $uri
         );
+    }
+
+    /**
+     * Returns all registered asset usage strategies
+     *
+     * @return array<\TYPO3\Media\Domain\Strategy\AssetUsageStrategyInterface>
+     * @throws \TYPO3\Flow\Object\Exception\UnknownObjectException
+     */
+    protected function getUsageStrategies()
+    {
+        if (is_array($this->usageStrategies)) {
+            return $this->usageStrategies;
+        }
+
+        $assetUsageStrategieImplementations = $this->reflectionService->getAllImplementationClassNamesForInterface('TYPO3\Media\Domain\Strategy\AssetUsageStrategyInterface');
+        foreach ($assetUsageStrategieImplementations as $assetUsageStrategieImplementationClassName) {
+            $this->usageStrategies[] = $this->objectManager->get($assetUsageStrategieImplementationClassName);
+        }
+
+        return $this->usageStrategies;
+    }
+
+    /**
+     * Returns an array of asset usage references.
+     *
+     * @param AssetInterface $asset
+     * @return array<\TYPO3\Media\Domain\Model\Dto\UsageReference>
+     */
+    public function getUsageReferences(AssetInterface $asset)
+    {
+        $usages = [];
+        /** @var AssetUsageStrategyInterface $strategy */
+        foreach ($this->getUsageStrategies() as $strategy) {
+            $usages = Arrays::arrayMergeRecursiveOverrule($usages, $strategy->getUsageReferences($asset));
+        }
+
+        return $usages;
+    }
+
+    /**
+     * Returns the total count of times an asset is used.
+     *
+     * @param AssetInterface $asset
+     * @return integer
+     */
+    public function getUsageCount(AssetInterface $asset)
+    {
+        $usageCount = 0;
+        /** @var AssetUsageStrategyInterface $strategy */
+        foreach ($this->getUsageStrategies() as $strategy) {
+            $usageCount += $strategy->getUsageCount($asset);
+        }
+
+        return $usageCount;
+    }
+
+    /**
+     * Returns true if the asset is used.
+     *
+     * @param AssetInterface $asset
+     * @return boolean
+     */
+    public function isInUse(AssetInterface $asset)
+    {
+        /** @var AssetUsageStrategyInterface $strategy */
+        foreach ($this->getUsageStrategies() as $strategy) {
+            if ($strategy->isInUse($asset) === true) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Validates if the asset can be removed
+     *
+     * @param AssetInterface $asset
+     * @throws AssetServiceException Thrown if the asset can not be removed
+     * @return void
+     */
+    public function validateRemoval(AssetInterface $asset)
+    {
+        if ($this->isInUse($asset)) {
+            throw new AssetServiceException('Asset could not be deleted, because it is still in use.', 1462196420);
+        }
     }
 }
