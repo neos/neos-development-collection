6de75576a189c2c6e87b1c3f5ab36b4f3fbe04de
Aske Ertmann <aertmann@gmail.com>
2016-05-21 14:24:23 +0200

    BUGFIX: Prevent recursive loop when closing edit asset view

--- TYPO3.Neos/Resources/Public/JavaScript/Content/Inspector/SecondaryInspectorController.js
+++ TYPO3.Neos/Resources/Public/JavaScript/Content/Inspector/SecondaryInspectorController.js
@@ -62,9 +62,6 @@ function(Ember, Button) {
 		 * Always hide the given view class
 		 */
 		hide: function() {
-			if (window.Typo3MediaBrowserCallbacks && window.Typo3MediaBrowserCallbacks.close) {
-				window.Typo3MediaBrowserCallbacks.close();
-			}
 			this.set('_visible', false);
 		}
 	}).create();
