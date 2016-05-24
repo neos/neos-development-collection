a89ea402eb525e81064e44ce27f614e8e82ee4bc
Dominique Feyer <dfeyer@ttree.ch>
2016-05-24 09:22:38 +0200

    TASK: Extract logic from the controller to a service object

--- TYPO3.Neos/Classes/TYPO3/Neos/Controller/Backend/SettingsController.php
+++ TYPO3.Neos/Classes/TYPO3/Neos/Controller/Backend/SettingsController.php
@@ -13,8 +13,7 @@ namespace TYPO3\Neos\Controller\Backend;
 
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Mvc\Controller\ActionController;
-use TYPO3\Flow\Utility\Arrays;
-use TYPO3\Flow\Utility\PositionalArraySorter;
+use TYPO3\Neos\Service\PreviewCentralService;
 use TYPO3\TYPO3CR\Domain\Service\NodeTypeManager;
 
 /**
@@ -29,36 +28,18 @@ class SettingsController extends ActionController
     protected $nodeTypeManager;
 
     /**
+     * @var PreviewCentralService
+     * @Flow\Inject
+     */
+    protected $previewCentralService;
+
+    /**
      * @param string $nodeType
      * @return string
      */
     public function editPreviewAction($nodeType)
     {
-        $nodeType = $this->nodeTypeManager->getNodeType($nodeType);
-        $settings = Arrays::getValueByPath($this->settings, 'userInterface.editPreviewModes');
-
-        $allowedPreviewModes = $editPreviewModes = [];
-        $configurationPath = 'ui.editPreviewModes';
-        if ($nodeType->hasConfiguration($configurationPath) && is_array($nodeType->getConfiguration($configurationPath))) {
-            $editPreviewModes = $nodeType->getConfiguration($configurationPath);
-            $allowedPreviewModes = array_keys(array_filter($editPreviewModes, function ($mode) {
-                return isset($mode['enabled']) && $mode['enabled'] === true;
-            }));
-        }
-        if ($editPreviewModes !== []) {
-            foreach ($settings as $name => $configuration) {
-                if (!in_array($name, $allowedPreviewModes)) {
-                    unset($settings[$name]);
-                }
-                if (isset($editPreviewModes[$name]['position'])) {
-                    $settings[$name]['position'] = (integer)$editPreviewModes[$name]['position'];
-                }
-            }
-        }
-
-        $configuration = new PositionalArraySorter($settings);
-
         $this->response->setHeader('Content-Type', 'application/json');
-        return json_encode($configuration->toArray());
+        return json_encode($this->previewCentralService->findEditPreviewModesByNodeType($nodeType));
     }
 }
