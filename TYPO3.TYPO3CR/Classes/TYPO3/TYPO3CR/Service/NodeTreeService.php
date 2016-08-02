456c29221d7a86158d75b08605c1f75e37e4ce57
Bernhard Schmitt <b.schmitt@core4.de>
2016-08-02 17:40:16 +0200

    FEATURE: Provide central NodeTreeService for node tree traversal
    NEOS-1853 #comment This is a prototype depth-first approach to traverse the node tree

---
+++ TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Service/NodeTreeService.php
@@ -0,0 +1,38 @@
+<?php
+namespace TYPO3\TYPO3CR\Service;
+
+/*
+ * This file is part of the TYPO3.TYPO3CR package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+
+/**
+ * The node tree service
+ * @Flow\Scope("singleton")
+ */
+class NodeTreeService
+{
+
+    /**
+     * @param NodeInterface $node
+     * @param callable $action
+     * @return void
+     */
+    public function traverseTree(NodeInterface $node, callable $action)
+    {
+        $continue = $action($node);
+        if ($continue !== false && $node->hasChildNodes()) {
+            foreach ($node->getChildNodes() as $childNode) {
+                $this->traverseTree($childNode, $action);
+            }
+        }
+    }
+}
