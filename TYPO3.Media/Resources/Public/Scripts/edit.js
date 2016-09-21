d893cb71a1be4e0343caf8839bbf28501e3e0eb9
Aske Ertmann <aske@ertmann.co>
2016-09-21 19:29:51 +0200

    BUGFIX: Prevent navigating to media overview when viewing asset details

--- TYPO3.Media/Resources/Public/Scripts/edit.js
+++ TYPO3.Media/Resources/Public/Scripts/edit.js
@@ -1,8 +1,9 @@
 (function($) {
 	$(function() {
 		if (window.parent !== window && window.parent.Typo3MediaBrowserCallbacks) {
-			$('.neos-footer .neos-button-primary').on('click', function(e) {
-				if(window.parent.Typo3MediaBrowserCallbacks && typeof window.parent.Typo3MediaBrowserCallbacks.close === 'function') {
+			$('.neos-action-cancel, .neos-button-primary', '.neos-footer').on('click', function(e) {
+				if (window.parent.Typo3MediaBrowserCallbacks && typeof window.parent.Typo3MediaBrowserCallbacks.close === 'function') {
+					console.log('close');
 					window.parent.Typo3MediaBrowserCallbacks.close();
 				}
 			});
