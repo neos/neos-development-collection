9af93bb317e5f397193d813e16feae39bf7e9364
Aske Ertmann <aske@ertmann.co>
2016-09-28 01:29:07 +0200

    TASK: Minor nitpicks

--- TYPO3.Media/Resources/Public/Scripts/edit.js
+++ TYPO3.Media/Resources/Public/Scripts/edit.js
@@ -3,7 +3,6 @@
 		if (window.parent !== window && window.parent.Typo3MediaBrowserCallbacks) {
 			$('.neos-action-cancel, .neos-button-primary', '.neos-footer').on('click', function(e) {
 				if (window.parent.Typo3MediaBrowserCallbacks && typeof window.parent.Typo3MediaBrowserCallbacks.close === 'function') {
-					console.log('close');
 					window.parent.Typo3MediaBrowserCallbacks.close();
 				}
 			});
