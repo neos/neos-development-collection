e5f94973aab9b3712b3919130186b03fd88bf6b6
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2016-11-24 22:59:50 +0100

    TASK: Apply migration Neos.Flow-20161124204700 to package "Neos.Media.Browser"
    Adjusts code to package renaming from "TYPO3.Flow" to "Neos.Flow"

--- Neos.Media.Browser/Classes/Controller/Backend/ImageBrowserController.php
+++ Neos.Media.Browser/Classes/Controller/Backend/ImageBrowserController.php
@@ -11,7 +11,7 @@ namespace Neos\Media\Browser\Controller\Backend;
  * source code.
  */
 
-use TYPO3\Flow\Annotations as Flow;
+use Neos\Flow\Annotations as Flow;
 use TYPO3\Media\Domain\Model\Asset;
 use TYPO3\Media\Domain\Model\ImageVariant;
 use TYPO3\Media\Domain\Repository\ImageRepository;
