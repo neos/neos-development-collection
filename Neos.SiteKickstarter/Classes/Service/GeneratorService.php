3c6e7cee36b6a313284a58895e342b30c76ffcc1
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2016-11-25 01:28:01 +0100

    TASK: Apply migration Neos.Neos-20161125002322 to package "Neos.SiteKickstarter"
    Adjusts code to package renaming from "TYPO3.Neos" to "Neos.Neos"

--- Neos.SiteKickstarter/Classes/Service/GeneratorService.php
+++ Neos.SiteKickstarter/Classes/Service/GeneratorService.php
@@ -121,7 +121,7 @@ class GeneratorService extends \Neos\Kickstart\Service\GeneratorService
 
         $contextVariables = array();
         $contextVariables['siteName'] = $siteName;
-        $contextVariables['neosViewHelper'] = '{namespace neos=TYPO3\Neos\ViewHelpers}';
+        $contextVariables['neosViewHelper'] = '{namespace neos=Neos\Neos\ViewHelpers}';
         $contextVariables['typoScriptViewHelper'] = '{namespace ts=TYPO3\TypoScript\ViewHelpers}';
         $packageKeyDomainPart = substr(strrchr($packageKey, '.'), 1) ?: $packageKey;
         $contextVariables['siteNodeName'] = lcfirst($packageKeyDomainPart);
