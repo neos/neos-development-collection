b5b66e548c8e2ba4d39031c95fda928bc066cb3e
Christian Mueller <christian@flownative.com>
2016-12-13 09:31:17 +0100

    !!! TASK: Remove deprecated old node label code
    This removes the ``NodeDataLabelGeneratorInterface`` as well
    as the respective adaptor and implementation. Additionally
    the deprecated ``getFullLabel`` method is removed from the
    node interface and implementation. Finally the deprecated
    ``crop`` argument was removed as well.
    
    This is breaking if you still rely on the old interface or
    method. Make sure to migrate to the ``NodeLabelGeneratorInterface``
    it's implementations and the ``getLabel`` method.

--- Neos.ContentRepository/Classes/Domain/Model/Node.php
+++ Neos.ContentRepository/Classes/Domain/Model/Node.php
@@ -421,20 +421,7 @@ class Node implements NodeInterface, CacheAwareInterface
      */
     public function getLabel()
     {
-        // TODO: remove second argument after deprecation phase ends for cropping.
-        return $this->getNodeType()->getNodeLabelGenerator()->getLabel($this, false);
-    }
-
-    /**
-     * Returns a full length plain text description of this node
-     *
-     * @return string
-     * @deprecated since 1.2
-     */
-    public function getFullLabel()
-    {
-        // TODO: remove second argument after deprecation phase ends for cropping.
-        return $this->getNodeType()->getNodeLabelGenerator()->getLabel($this, false);
+        return $this->getNodeType()->getNodeLabelGenerator()->getLabel($this);
     }
 
     /**
