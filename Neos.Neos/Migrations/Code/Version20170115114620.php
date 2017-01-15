8733b174a0ed5e8c8f44d0d5fc3a953d0d8447eb
Gerhard Boden <gerhard.boden@gmail.com>
2017-01-15 14:32:59 +0100

    TASK: Provide code and database migrations for authentication provider name change

---
+++ Neos.Neos/Migrations/Code/Version20170115114620.php
@@ -0,0 +1,35 @@
+<?php
+namespace Neos\Flow\Core\Migrations;
+
+/*
+ * This file is part of the Neos.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+/**
+ * Migrate usages of the Settings path Neos.Flow.security.authentication.providers.Typo3BackendProvider to Neos.Flow.security.authentication.providers.NeosDefaultProvider
+ */
+class Version20170115114620 extends AbstractMigration
+{
+    /**
+     * @return string
+     */
+    public function getIdentifier()
+    {
+        return 'Neos.Neos-20170115114620';
+    }
+
+    /**
+     * @return void
+     */
+    public function up()
+    {
+        $this->moveSettingsPaths('Neos.Flow.security.authentication.providers.Typo3BackendProvider', 'Neos.Flow.security.authentication.providers.NeosDefaultProvider');
+        $this->searchAndReplace('Typo3BackendProvider', 'NeosDefaultProvider');
+    }
+}
