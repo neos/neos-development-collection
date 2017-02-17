neos/neos-development-collection

commit 6bb10253aa7b4ac58aef5ce02eba1efff3a30582

BUGFIX: Initialize usageStrategies as empty array
Currently, the Asset models of the Media package cannot be used without having an AssetUsageStrategy implemented. If no such strategy is implemented (e.g. by using Media without Neos and no need for usage information), using an Asset model will lead to a fatal error because the AssetService is trying to loop $usageStrategies which is not an empty array, but null. Therefore, $usageStrategies must be initialized as an empty array.

TYPO3.Media/Classes/TYPO3/Media/Domain/Service/AssetService.php
@@ -71,7 +71,7 @@ class AssetService
     /**
      * @var array
      */
-    protected $usageStrategies;
+    protected $usageStrategies = [];
 
     /**
      * @Flow\Inject

commit 0173567e25c35122e2f3039f94fa6e2eaf5c00f5

BUGFIX: Initialization of asset usage strategies
Fixes a regression introduced in 6bb10253aa7b4ac58aef5ce02eba1efff3a30582,
causing the asset usage strategies to never be initialized.

TYPO3.Media/Classes/TYPO3/Media/Domain/Service/AssetService.php
@@ -71,7 +71,7 @@ class AssetService
     /**
      * @var array
      */
-    protected $usageStrategies = [];
+    protected $usageStrategies;
 
     /**
      * @Flow\Inject
@@ -150,6 +150,7 @@ class AssetService
             return $this->usageStrategies;
         }
 
+        $this->usageStrategies = [];
         $assetUsageStrategieImplementations = $this->reflectionService->getAllImplementationClassNamesForInterface('TYPO3\Media\Domain\Strategy\AssetUsageStrategyInterface');
         foreach ($assetUsageStrategieImplementations as $assetUsageStrategieImplementationClassName) {
             $this->usageStrategies[] = $this->objectManager->get($assetUsageStrategieImplementationClassName);