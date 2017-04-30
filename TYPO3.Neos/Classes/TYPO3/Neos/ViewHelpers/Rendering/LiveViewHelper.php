60e1218bb89bcb9a1dbaef9960cdc0634210da60
Christian Mueller <christian@flownative.com>
2017-04-30 10:47:56 +0200

    BUGFIX: Clarified doc block for LiveViewHelper
    If the ``LiveViewHelper`` doesn't get a node as argument and neither
    there is a node in the template variables it will always return true.
    The adjusted doc block clarifies that you need either.
    
    Fixes: #1416

--- TYPO3.Neos/Classes/TYPO3/Neos/ViewHelpers/Rendering/LiveViewHelper.php
+++ TYPO3.Neos/Classes/TYPO3/Neos/ViewHelpers/Rendering/LiveViewHelper.php
@@ -16,6 +16,8 @@ use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 
 /**
  * ViewHelper to find out if Neos is rendering the live website.
+ * Make sure you either give a node from the current context to
+ * the ViewHelper or have "node" set as template variable at least.
  *
  * = Examples =
  *
