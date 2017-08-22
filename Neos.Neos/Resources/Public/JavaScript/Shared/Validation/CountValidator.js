090b193a71a16ea3c92ac04fd4f6d1833a357a8b
Florian Lammel <mail@florianlammel.com>
2017-08-22 23:44:02 +0200

    FEATURE: Accept JSON string for validation

--- Neos.Neos/Resources/Public/JavaScript/Shared/Validation/CountValidator.js
+++ Neos.Neos/Resources/Public/JavaScript/Shared/Validation/CountValidator.js
@@ -5,9 +5,10 @@ define(
 	[
 		'./AbstractValidator',
 		'Library/underscore',
-		'Shared/I18n'
+		'Shared/I18n',
+		'Shared/Utility'
 	],
-	function(AbstractValidator, _, I18n) {
+	function(AbstractValidator, _, I18n, Utility) {
 		return AbstractValidator.extend({
 			/**
 			 * @var {object}
@@ -19,11 +20,15 @@ define(
 
 			/**
 			 * The given value is valid if it is an array or object that contains the specified amount of elements.
+			 * If the value is a valid JSON string then the parsed object is checked.
 			 *
 			 * @param {mixed} value The value that should be validated
 			 * @return {void}
 			 */
 			isValid: function(value) {
+				if (Utility.isValidJsonString(value)) {
+					value = JSON.parse(value);
+				}
 				if (typeof value !== 'array' || typeof value !== 'object') {
 					this.addError(I18n.translate('content.inspector.validators.countValidator.notCountable'));
 					return;
