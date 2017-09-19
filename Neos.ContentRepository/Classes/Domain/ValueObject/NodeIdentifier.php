f72fb792b325da0d710613827e4fff89d179e88f
Bernhard Schmitt <b.schmitt@core4.de>
2017-09-19 17:48:16 +0200

    Feature: Make node identifier cache-aware and validate it against a custom pattern

--- Neos.ContentRepository/Classes/Domain/ValueObject/NodeIdentifier.php
+++ Neos.ContentRepository/Classes/Domain/ValueObject/NodeIdentifier.php
@@ -11,14 +11,26 @@ namespace Neos\ContentRepository\Domain\ValueObject;
  * information, please view the LICENSE file which was distributed with this
  * source code.
  */
-
 use Neos\ContentRepository\Utility;
 
 /**
  * NodeIdentifier
  */
-final class NodeIdentifier extends AbstractIdentifier
+use Neos\Cache\CacheAwareInterface;
+use Neos\Flow\Utility\Algorithms;
+
+final class NodeIdentifier implements \JsonSerializable, CacheAwareInterface
 {
+    /**
+     * A preg pattern to match against node identifiers
+     */
+    const PATTERN = '/^([a-z0-9\-]{1,255})$/';
+
+    /**
+     * @var string
+     */
+    protected $identifier;
+
     /**
      * @param NodeName $childNodeName
      * @param NodeIdentifier $nodeIdentifier
@@ -27,5 +39,28 @@ final class NodeIdentifier extends AbstractIdentifier
     public static function forAutoCreatedChildNode(NodeName $childNodeName, NodeIdentifier $nodeIdentifier): NodeIdentifier
     {
         return new NodeIdentifier(Utility::buildAutoCreatedChildNodeIdentifier((string)$childNodeName, (string)$nodeIdentifier));
+    }
+
+    public function __construct(string $identifier)
+    {
+        if (!preg_match(self::PATTERN, $identifier)) {
+            throw new \InvalidArgumentException('Invalid node identifier "' . $identifier . '" (a node identifier must only contain lowercase characters, numbers and the "-" sign).', 1505805774);
+        }
+        $this->identifier = $identifier;
+    }
+
+    function jsonSerialize()
+    {
+        return $this->identifier;
+    }
+
+    public function __toString()
+    {
+        return $this->identifier;
+    }
+
+    public function getCacheEntryIdentifier(): string
+    {
+        return $this->identifier;
     }
 }
