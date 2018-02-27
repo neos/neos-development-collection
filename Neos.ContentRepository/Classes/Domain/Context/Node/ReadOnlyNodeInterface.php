9f3767b33807aa7a8e406fc11cdb530c97c2c469
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2018-02-27 14:39:26 +0100

    BUGFIX: really add the ReadOnlyNodeInterface

---
+++ Neos.ContentRepository/Classes/Domain/Context/Node/ReadOnlyNodeInterface.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Neos\ContentRepository\Domain\Context\Node;
+
+use Neos\ContentRepository\Domain\Context\Node\Event\CopyableAcrossContentStreamsInterface;
+use Neos\ContentRepository\Domain\Projection\Content\PropertyCollection;
+use Neos\ContentRepository\Domain\ValueObject\ContentStreamIdentifier;
+use Neos\ContentRepository\Domain\ValueObject\DimensionSpacePoint;
+use Neos\ContentRepository\Domain\ValueObject\NodeAggregateIdentifier;
+use Neos\ContentRepository\Domain\ValueObject\NodeIdentifier;
+use Neos\ContentRepository\Domain\ValueObject\NodeName;
+use Neos\ContentRepository\Domain\ValueObject\NodeTypeName;
+use Neos\EventSourcing\Event\Decorator\EventWithMetadata;
+use Neos\EventSourcing\Event\EventInterface;
+use Neos\EventSourcing\Event\EventPublisher;
+use Neos\EventSourcing\EventStore\ExpectedVersion;
+use Neos\EventSourcing\TypeConverter\EventToArrayConverter;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Property\PropertyMapper;
+use Neos\Flow\Property\PropertyMappingConfiguration;
+
+/**
+ * The "new" Event-Sourced NodeInterface. Supersedes the old Neos\ContentRepository\Domain\Model\NodeInterface.
+ */
+interface ReadOnlyNodeInterface
+{
+
+    public function getContentStreamIdentifier(): ContentStreamIdentifier;
+
+    public function getNodeIdentifier(): NodeIdentifier;
+
+    public function getNodeAggregateIdentifier(): NodeAggregateIdentifier;
+
+    public function getNodeTypeName(): NodeTypeName;
+
+    public function getNodeName(): NodeName;
+
+    public function getDimensionSpacePoint(): DimensionSpacePoint;
+
+    /**
+     * Returns all properties of this node.
+     *
+     * If the node has a content object attached, the properties will be fetched
+     * there.
+     *
+     * @return PropertyCollection Property values, indexed by their name
+     * @api
+     */
+    public function getProperties(): PropertyCollection;
+
+
+    /**
+     * Returns the specified property.
+     *
+     * If the node has a content object attached, the property will be fetched
+     * there if it is gettable.
+     *
+     * @param string $propertyName Name of the property
+     * @return mixed value of the property
+     * @throws NodeException if the node does not contain the specified property
+     * @api
+     */
+    public function getProperty($propertyName);
+
+
+}
