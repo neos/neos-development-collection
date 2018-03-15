206f9aae5bf4e101406a2ae495245965f23c8996
Michael Gerdemann <michael@gerdemann.me>
2018-03-15 16:39:14 +0100

    BUGFIX: ImageMagick PDF to Image sometimes results in black background

--- Neos.Media/Classes/Domain/Model/ThumbnailGenerator/DocumentThumbnailGenerator.php
+++ Neos.Media/Classes/Domain/Model/ThumbnailGenerator/DocumentThumbnailGenerator.php
@@ -66,6 +66,9 @@ class DocumentThumbnailGenerator extends AbstractThumbnailGenerator
             $im->setImageFormat('png');
             $im->setImageBackgroundColor('white');
             $im->setImageCompose(\Imagick::COMPOSITE_OVER);
+            if (defined('\Imagick::ALPHACHANNEL_FLATTEN')) {
+                $im->setImageAlphaChannel(\Imagick::ALPHACHANNEL_FLATTEN);
+            }
             if (defined('\Imagick::ALPHACHANNEL_OFF')) {
                 // ImageMagick >= 7.0, Imagick >= 3.4.3RC1
                 // @see https://pecl.php.net/package/imagick/3.4.3RC1
