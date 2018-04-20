fe8e146effcfedb7acfca3892745c2dd419be987
Robert Lemke <robert@flownative.com>
2018-04-20 10:22:22 +0200

    Fix code style issues in Neos.Media.Browser

--- Neos.Media.Browser/Classes/Controller/ImageController.php
+++ Neos.Media.Browser/Classes/Controller/ImageController.php
@@ -5,7 +5,7 @@ namespace Neos\Media\Browser\Controller;
  * This file is part of the Neos.Media.Browser package.
  *
  * (c) Contributors of the Neos Project - www.neos.io
-  *
+ *
  * This package is Open Source Software. For the full copyright and license
  * information, please view the LICENSE file which was distributed with this
  * source code.
@@ -38,7 +38,7 @@ class ImageController extends AssetController
      * @param string $assetSourceIdentifier
      * @param string $assetProxyIdentifier
      * @param Asset $asset
-     * @return void|string
+     * @return void
      * @throws \Neos\Flow\Mvc\Exception\StopActionException
      * @throws \Neos\Flow\Mvc\Exception\UnsupportedRequestTypeException
      */
--- Neos.Media.Browser/Classes/ViewHelpers/Controller/PaginateController.php
+++ Neos.Media.Browser/Classes/ViewHelpers/Controller/PaginateController.php
@@ -1,16 +1,15 @@
 <?php
-
 namespace Neos\Media\Browser\ViewHelpers\Controller;
 
 /*
-* This file is part of the Neos.Media.Browser package.
-*
-* (c) Contributors of the Neos Project - www.neos.io
-*
-* This package is Open Source Software. For the full copyright and license
-* information, please view the LICENSE file which was distributed with this
-* source code.
-*/
+ * This file is part of the Neos.Media.Browser package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
 
 use Neos\Media\Domain\Model\AssetSource\AssetProxyQueryResultInterface;
 use Neos\FluidAdaptor\Core\Widget\AbstractWidgetController;
