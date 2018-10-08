8f5127dc51fdc92b19da4942e9787b4fa81d7977
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2018-10-08 19:20:45 +0200

    TASK: fix NodeUri and DimensionsMenu

---
+++ Neos.EventSourcedNeosAdjustments/Classes/Fusion/AbstractMenuImplementation.php
@@ -0,0 +1,152 @@
+<?php
+namespace Neos\EventSourcedNeosAdjustments\Fusion;
+
+/*
+ * This file is part of the Neos.Neos package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\ContentRepository\Domain\Projection\Content\NodeInterface;
+use Neos\Fusion\Exception as FusionException;
+use Neos\Fusion\FusionObjects\TemplateImplementation;
+
+/**
+ * Base class for Menu and DimensionsMenu
+ *
+ * Main Options:
+ *  - renderHiddenInIndex: if TRUE, hidden-in-index nodes will be shown in the menu. FALSE by default.
+ */
+abstract class AbstractMenuImplementation extends TemplateImplementation
+{
+    const STATE_NORMAL = 'normal';
+    const STATE_CURRENT = 'current';
+    const STATE_ACTIVE = 'active';
+    const STATE_ABSENT = 'absent';
+
+    /**
+     * An internal cache for the built menu items array.
+     *
+     * @var array
+     */
+    protected $items;
+
+    /**
+     * @var NodeInterface
+     */
+    protected $currentNode;
+
+    /**
+     * Internal cache for the currentLevel tsValue.
+     *
+     * @var integer
+     */
+    protected $currentLevel;
+
+    /**
+     * Internal cache for the renderHiddenInIndex property.
+     *
+     * @var boolean
+     */
+    protected $renderHiddenInIndex;
+
+    /**
+     * Rootline of all nodes from the current node to the site root node, keys are depth of nodes.
+     *
+     * @var array<NodeInterface>
+     */
+    protected $currentNodeRootline;
+
+    /**
+     * Should nodes that have "hiddenInIndex" set still be visible in this menu.
+     *
+     * @return boolean
+     */
+    public function getRenderHiddenInIndex()
+    {
+        if ($this->renderHiddenInIndex === null) {
+            $this->renderHiddenInIndex = (boolean)$this->fusionValue('renderHiddenInIndex');
+        }
+
+        return $this->renderHiddenInIndex;
+    }
+
+    /**
+     * Main API method which sends the to-be-rendered data to Fluid
+     *
+     * @return array
+     */
+    public function getItems()
+    {
+        if ($this->items === null) {
+            $fusionContext = $this->runtime->getCurrentContext();
+            $this->currentNode = isset($fusionContext['activeNode']) ? $fusionContext['activeNode'] : $fusionContext['documentNode'];
+            $this->currentLevel = 1;
+            $this->items = $this->buildItems();
+        }
+
+        return $this->items;
+    }
+
+    /**
+     * Builds the array of menu items containing those items which match the
+     * configuration set for this Menu object.
+     *
+     * Must be overridden in subclasses.
+     *
+     * @throws FusionException
+     * @return array An array of menu items and further information
+     */
+    abstract protected function buildItems();
+
+    /**
+     * Return TRUE/FALSE if the node is currently hidden or not in the menu; taking the "renderHiddenInIndex" configuration
+     * of the Menu Fusion object into account.
+     *
+     * This method needs to be called inside buildItems() in the subclasses.
+     *
+     * @param NodeInterface $node
+     * @return boolean
+     */
+    protected function isNodeHidden(NodeInterface $node)
+    {
+        return ($node->isVisible() === false || ($this->getRenderHiddenInIndex() === false && $node->isHiddenInIndex() === true) || $node->isAccessible() === false);
+    }
+
+    /**
+     * Get the rootline from the current node up to the site node.
+     *
+     * @return array
+     */
+    protected function getCurrentNodeRootline()
+    {
+        if ($this->currentNodeRootline === null) {
+            /** @todo replace this */
+            $nodeRootline = $this->currentNode->getContext()->getNodesOnPath($this->runtime->getCurrentContext()['site']->getPath(), $this->currentNode->getPath());
+            $this->currentNodeRootline = array();
+
+            foreach ($nodeRootline as $rootlineElement) {
+                $this->currentNodeRootline[$this->getNodeLevelInSite($rootlineElement)] = $rootlineElement;
+            }
+        }
+
+        return $this->currentNodeRootline;
+    }
+
+    /**
+     * Node Level relative to site root node.
+     * 0 = Site root node
+     *
+     * @param NodeInterface $node
+     * @return integer
+     */
+    protected function getNodeLevelInSite(NodeInterface $node)
+    {
+        $siteNode = $this->currentNode->getContext()->getCurrentSiteNode();
+        return $node->getDepth() - $siteNode->getDepth();
+    }
+}
---
+++ Neos.EventSourcedNeosAdjustments/Classes/Fusion/NodeUriImplementation.php
@@ -0,0 +1,170 @@
+<?php
+namespace Neos\EventSourcedNeosAdjustments\Fusion;
+
+/*
+ * This file is part of the Neos.Neos package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\ContentRepository\Domain\Projection\Content\NodeInterface;
+use Neos\ContentRepository\Domain\ValueObject\NodeAggregateIdentifier;
+use Neos\EventSourcedContentRepository\Domain\Projection\Content\ContentSubgraphInterface;
+use Neos\EventSourcedNeosAdjustments\Domain\Context\Content\NodeAddressFactory;
+use Neos\EventSourcedNeosAdjustments\Domain\Context\Content\NodeSiteResolvingService;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\Routing\UriBuilder;
+use Neos\Fusion\FusionObjects\AbstractFusionObject;
+
+/**
+ * Create a link to a node
+ */
+class NodeUriImplementation extends AbstractFusionObject
+{
+    /**
+     * @Flow\Inject
+     * @var NodeAddressFactory
+     */
+    protected $nodeAddressFactory;
+
+    /**
+     * @Flow\Inject
+     * @var NodeSiteResolvingService
+     */
+    protected $nodeSiteResolvingService;
+
+    /**
+     * A node object or a string node path or NULL to resolve the current document node
+     *
+     * @return mixed
+     */
+    public function getNode()
+    {
+        return $this->fusionValue('node');
+    }
+
+    /**
+     * The requested format, for example "html"
+     *
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->fusionValue('format');
+    }
+
+    /**
+     * The anchor to be appended to the URL
+     *
+     * @return string
+     */
+    public function getSection()
+    {
+        return (string)$this->fusionValue('section');
+    }
+
+    /**
+     * Additional query parameters that won't be prefixed like $arguments (overrule $arguments)
+     *
+     * @return array
+     */
+    public function getAdditionalParams()
+    {
+        return $this->fusionValue('additionalParams');
+    }
+
+    /**
+     * Arguments to be removed from the URI. Only active if addQueryString = TRUE
+     *
+     * @return array
+     */
+    public function getArgumentsToBeExcludedFromQueryString()
+    {
+        return $this->fusionValue('argumentsToBeExcludedFromQueryString');
+    }
+
+    /**
+     * If TRUE, the current query parameters will be kept in the URI
+     *
+     * @return boolean
+     */
+    public function getAddQueryString()
+    {
+        return (boolean)$this->fusionValue('addQueryString');
+    }
+
+    /**
+     * If TRUE, an absolute URI is rendered
+     *
+     * @return boolean
+     */
+    public function isAbsolute()
+    {
+        return (boolean)$this->fusionValue('absolute');
+    }
+
+    /**
+     * The name of the base node inside the Fusion context to use for resolving relative paths.
+     *
+     * @return string
+     */
+    public function getBaseNodeName()
+    {
+        return $this->fusionValue('baseNodeName');
+    }
+
+    /**
+     * @return ContentSubgraphInterface
+     */
+    public function getSubgraph(): ContentSubgraphInterface
+    {
+        return $this->fusionValue('subgraph');
+    }
+
+    /**
+     * Render the Uri.
+     *
+     * @return string The rendered URI or NULL if no URI could be resolved for the given node
+     * @throws \Neos\Flow\Mvc\Routing\Exception\MissingActionNameException
+     */
+    public function evaluate()
+    {
+        $node = $this->getNode();
+        $nodeAddress = $this->nodeAddressFactory->createFromNode($node);
+        if ($node instanceof NodeInterface) {
+            $nodeAddress = $this->nodeAddressFactory->adjustWithNodeAggregateIdentifier($nodeAddress, $node->getNodeAggregateIdentifier());
+        } elseif ($node === '~') {
+            $nodeAddress = $this->nodeAddressFactory->adjustWithNodeAggregateIdentifier($nodeAddress, $this->nodeSiteResolvingService->findSiteNodeForNodeAddress($nodeAddress)->getNodeAggregateIdentifier());
+        } elseif (is_string($node) && substr($node, 0, 7) === 'node://') {
+            $nodeAddress = $this->nodeAddressFactory->adjustWithNodeAggregateIdentifier($nodeAddress, new NodeAggregateIdentifier(\mb_substr($node, 7)));
+        } else {
+            return '';
+        }
+        if ($this->getSubgraph()) {
+            $nodeAddress = $this->nodeAddressFactory->adjustWithDimensionSpacePoint($nodeAddress, $this->getSubgraph()->getDimensionSpacePoint());
+        }
+
+        $uriBuilder = new UriBuilder();
+        $uriBuilder->setRequest($this->runtime->getControllerContext()->getRequest());
+        $uriBuilder
+            ->setAddQueryString($this->getAddQueryString())
+            ->setArguments($this->getAdditionalParams())
+            ->setArgumentsToBeExcludedFromQueryString($this->getArgumentsToBeExcludedFromQueryString())
+            ->setCreateAbsoluteUri($this->isAbsolute())
+            ->setFormat($this->getFormat())
+            ->setSection($this->getSection());
+
+        return $uriBuilder->uriFor(
+            'show',
+            [
+                'node' => $nodeAddress
+            ],
+            'Frontend\\Node',
+            'Neos.Neos'
+        );
+    }
+}
