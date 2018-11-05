c8145716152ead644171df9f18e7cdf7930b7df9
Bernhard Schmitt <schmitt@sitegeist.de>
2018-11-05 13:47:10 +0100

    Make property collection iterable

--- Neos.ContentRepository/Classes/Domain/ValueObject/PropertyCollectionInterface.php
+++ Neos.ContentRepository/Classes/Domain/ValueObject/PropertyCollectionInterface.php
@@ -12,6 +12,6 @@ namespace Neos\ContentRepository\Domain\ValueObject;
  */
 
 
-interface PropertyCollectionInterface extends \ArrayAccess
+interface PropertyCollectionInterface extends \ArrayAccess, \Iterator
 {
 }
