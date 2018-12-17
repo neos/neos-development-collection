neos/neos-development-collection

commit 5b2c885f51366bb84c0f5d100eb0a12d56ec053d

Add "strict_types" declaration to all PHP files
According to https://discuss.neos.io/t/usage-of-php-strict-typing-by-default/3861

Neos.ContentGraph.DoctrineDbalAdapter/Classes/Domain/Repository/ContentSubgraph.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Neos\ContentGraph\DoctrineDbalAdapter\Domain\Repository;
 

Neos.EventSourcedContentRepository/Classes/Domain/Context/ContentStream/ContentStreamCommandHandler.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace Neos\EventSourcedContentRepository\Domain\Context\ContentStream;
 
 /*

Neos.EventSourcedContentRepository/Classes/Domain/Context/ContentStream/ContentStreamRepository.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace Neos\EventSourcedContentRepository\Domain\Context\ContentStream;
 
 /*

Neos.EventSourcedContentRepository/Classes/Domain/Context/Node/NodeCommandHandler.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace Neos\EventSourcedContentRepository\Domain\Context\Node;
 
 /*

Neos.EventSourcedContentRepository/Classes/Domain/Context/NodeAggregate/NodeAggregate.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace Neos\EventSourcedContentRepository\Domain\Context\NodeAggregate;
 
 /*

Neos.EventSourcedContentRepository/Classes/Domain/Context/Workspace/WorkspaceCommandHandler.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace Neos\EventSourcedContentRepository\Domain\Context\Workspace;
 
 /*

Neos.EventSourcedContentRepository/Classes/Domain/Projection/Workspace/WorkspaceProjector.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace Neos\EventSourcedContentRepository\Domain\Projection\Workspace;
 
 /*

Neos.EventSourcedContentRepository/Tests/Behavior/Features/Bootstrap/EventSourcedTrait.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 /*
  * This file is part of the Neos.ContentRepository package.

commit 67cc2950e8b0f8e611e0ca62995cee905727e3ef

Fix invalid types that break tests
Due to the `strict_types` declarations introduced with 5b2c885f51366bb84c0f5d100eb0a12d56ec053d

Neos.ContentGraph.DoctrineDbalAdapter/Classes/Domain/Repository/ContentSubgraph.php
@@ -329,7 +329,7 @@ SELECT n.*, h.name, h.contentstreamidentifier, h.dimensionspacepoint FROM neos_c
         }
 
         $res = $query->execute($this->getDatabaseConnection())->fetchColumn(0);
-        return $res;
+        return (int)$res;
     }
 
     /**

Neos.EventSourcedContentRepository/Classes/Domain/Context/ContentStream/ContentStreamCommandHandler.php
@@ -47,7 +47,7 @@ final class ContentStreamCommandHandler
         $this->requireContentStreamToNotExistYet($command->getContentStreamIdentifier());
 
         $this->eventPublisher->publish(
-            ContentStreamEventStreamName::fromContentStreamIdentifier($command->getContentStreamIdentifier()),
+            ContentStreamEventStreamName::fromContentStreamIdentifier($command->getContentStreamIdentifier())->getEventStreamName(),
             new Event\ContentStreamWasCreated(
                 $command->getContentStreamIdentifier(),
                 $command->getInitiatingUserIdentifier()
@@ -68,7 +68,7 @@ final class ContentStreamCommandHandler
         $sourceContentStream = $this->contentStreamRepository->findContentStream($command->getSourceContentStreamIdentifier());
 
         $this->eventPublisher->publish(
-            ContentStreamEventStreamName::fromContentStreamIdentifier($command->getContentStreamIdentifier()),
+            ContentStreamEventStreamName::fromContentStreamIdentifier($command->getContentStreamIdentifier())->getEventStreamName(),
             new Event\ContentStreamWasForked(
                 $command->getContentStreamIdentifier(),
                 $command->getSourceContentStreamIdentifier(),

Neos.EventSourcedContentRepository/Classes/Domain/Context/ContentStream/ContentStreamRepository.php
@@ -56,9 +56,9 @@ final class ContentStreamRepository
     {
         if (!isset($this->contentStreams[(string)$contentStreamIdentifier])) {
             $eventStreamName = ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier);
-            $eventStore = $this->eventStoreManager->getEventStoreForStreamName($eventStreamName);
+            $eventStore = $this->eventStoreManager->getEventStoreForStreamName($eventStreamName->getEventStreamName());
             try {
-                $eventStream = $eventStore->get(new StreamNameFilter($eventStreamName));
+                $eventStream = $eventStore->get(new StreamNameFilter($eventStreamName->getEventStreamName()));
                 $eventStream->rewind();
                 if (!$eventStream->current()) {
                     // a content stream without events in its event stream does not exist yet

Neos.EventSourcedContentRepository/Classes/Domain/Context/Node/NodeCommandHandler.php
@@ -13,27 +13,37 @@ namespace Neos\EventSourcedContentRepository\Domain\Context\Node;
  */
 
 use Neos\ContentRepository\DimensionSpace\DimensionSpace\ContentDimensionZookeeper;
+use Neos\ContentRepository\DimensionSpace\DimensionSpace\DimensionSpacePoint;
+use Neos\ContentRepository\DimensionSpace\DimensionSpace\DimensionSpacePointSet;
+use Neos\ContentRepository\DimensionSpace\DimensionSpace\InterDimensionalVariationGraph;
 use Neos\ContentRepository\Domain\Model\NodeType;
+use Neos\ContentRepository\Domain\Projection\Content\NodeInterface;
+use Neos\ContentRepository\Domain\Service\NodeTypeManager;
+use Neos\ContentRepository\Domain\ValueObject\ContentStreamIdentifier;
+use Neos\ContentRepository\Domain\ValueObject\NodeAggregateIdentifier;
+use Neos\ContentRepository\Domain\ValueObject\NodeIdentifier;
+use Neos\ContentRepository\Domain\ValueObject\NodeName;
+use Neos\ContentRepository\Domain\ValueObject\NodeTypeName;
 use Neos\ContentRepository\Domain\ValueObject\RootNodeIdentifiers;
 use Neos\ContentRepository\Exception\NodeConstraintException;
-use Neos\ContentRepository\NodeException;
+use Neos\ContentRepository\Exception\NodeException;
 use Neos\ContentRepository\Exception\NodeExistsException;
 use Neos\EventSourcedContentRepository\Domain\Context\ContentStream\ContentStreamEventStreamName;
-use Neos\ContentRepository\DimensionSpace\DimensionSpace\InterDimensionalVariationGraph;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\AddNodeToAggregate;
-use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\HideNode;
-use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\RemoveNodeAggregate;
-use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\RemoveNodesFromAggregate;
-use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\ShowNode;
-use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\SetNodeReferences;
-use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\TranslateNodeInAggregate;
+use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\ChangeNodeName;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\CreateNodeAggregateWithNode;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\CreateRootNode;
+use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\HideNode;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\MoveNode;
-use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\ChangeNodeName;
+use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\RemoveNodeAggregate;
+use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\RemoveNodesFromAggregate;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\SetNodeProperty;
+use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\SetNodeReferences;
+use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\ShowNode;
+use Neos\EventSourcedContentRepository\Domain\Context\Node\Command\TranslateNodeInAggregate;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodeAggregateWasRemoved;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodeAggregateWithNodeWasCreated;
+use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodeInAggregateWasTranslated;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodeMoveMapping;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodeNameWasChanged;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodePropertyWasSet;
@@ -42,18 +52,8 @@ use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodesWereMoved;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodesWereRemovedFromAggregate;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodeWasAddedToAggregate;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodeWasHidden;
-use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodeInAggregateWasTranslated;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\NodeWasShown;
 use Neos\EventSourcedContentRepository\Domain\Context\Node\Event\RootNodeWasCreated;
-use Neos\ContentRepository\Domain\Projection\Content\NodeInterface;
-use Neos\ContentRepository\Domain\Service\NodeTypeManager;
-use Neos\ContentRepository\Domain\ValueObject\ContentStreamIdentifier;
-use Neos\ContentRepository\DimensionSpace\DimensionSpace\DimensionSpacePoint;
-use Neos\ContentRepository\DimensionSpace\DimensionSpace\DimensionSpacePointSet;
-use Neos\ContentRepository\Domain\ValueObject\NodeAggregateIdentifier;
-use Neos\ContentRepository\Domain\ValueObject\NodeIdentifier;
-use Neos\ContentRepository\Domain\ValueObject\NodeName;
-use Neos\ContentRepository\Domain\ValueObject\NodeTypeName;
 use Neos\EventSourcedContentRepository\Domain\Context\Parameters\VisibilityConstraints;
 use Neos\EventSourcedContentRepository\Domain\ValueObject\PropertyValue;
 use Neos\EventSourcedContentRepository\Exception;
@@ -325,7 +325,7 @@ final class NodeCommandHandler
             );
 
             $this->nodeEventPublisher->publish(
-                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier),
+                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier)->getEventStreamName(),
                 $event
             );
         });
@@ -351,7 +351,7 @@ final class NodeCommandHandler
             );
 
             $this->nodeEventPublisher->publish(
-                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier),
+                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier)->getEventStreamName(),
                 $event
             );
         });
@@ -384,7 +384,7 @@ final class NodeCommandHandler
             );
 
             $this->nodeEventPublisher->publishMany(
-                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier),
+                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier)->getEventStreamName(),
                 $events
             );
         });
@@ -411,7 +411,7 @@ final class NodeCommandHandler
             );
 
             $this->nodeEventPublisher->publish(
-                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier),
+                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier)->getEventStreamName(),
                 $event
             );
         });
@@ -437,7 +437,7 @@ final class NodeCommandHandler
             );
 
             $this->nodeEventPublisher->publish(
-                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier),
+                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier)->getEventStreamName(),
                 $event
             );
         });
@@ -532,7 +532,7 @@ final class NodeCommandHandler
             );
 
             $this->nodeEventPublisher->publish(
-                ContentStreamEventStreamName::fromContentStreamIdentifier($command->getContentStreamIdentifier()),
+                ContentStreamEventStreamName::fromContentStreamIdentifier($command->getContentStreamIdentifier())->getEventStreamName(),
                 $nodesWereMoved
             );
         });
@@ -609,7 +609,7 @@ final class NodeCommandHandler
             );
 
             $this->nodeEventPublisher->publish(
-                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier),
+                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier)->getEventStreamName(),
                 $event
             );
         });
@@ -635,7 +635,7 @@ final class NodeCommandHandler
             );
 
             $this->nodeEventPublisher->publish(
-                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier),
+                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier)->getEventStreamName(),
                 $event
             );
         });
@@ -671,7 +671,7 @@ final class NodeCommandHandler
             );
 
             $this->nodeEventPublisher->publish(
-                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier),
+                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier)->getEventStreamName(),
                 $event
             );
         });
@@ -679,7 +679,6 @@ final class NodeCommandHandler
 
     /**
      * @param TranslateNodeInAggregate $command
-     * @throws NodeException
      */
     public function handleTranslateNodeInAggregate(TranslateNodeInAggregate $command): void
     {
@@ -688,7 +687,7 @@ final class NodeCommandHandler
 
             $events = $this->nodeInAggregateWasTranslatedFromCommand($command);
             $this->nodeEventPublisher->publishMany(
-                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier),
+                ContentStreamEventStreamName::fromContentStreamIdentifier($contentStreamIdentifier)->getEventStreamName(),
                 $events
             );
         });
@@ -706,7 +705,7 @@ final class NodeCommandHandler
         // TODO Check that command->dimensionSpacePoint is not a generalization or specialization of sourceNode->dimensionSpacePoint!!! (translation)
 
         $sourceContentSubgraph = $this->contentGraph->getSubgraphByIdentifier($contentStreamIdentifier, $sourceNode->getDimensionSpacePoint(), VisibilityConstraints::withoutRestrictions());
-        /** @var Node $sourceParentNode */
+        /** @var NodeInterface $sourceParentNode */
         $sourceParentNode = $sourceContentSubgraph->findParentNode($sourceNode->getNodeAggregateIdentifier());
         if ($sourceParentNode === null) {
             throw new NodeException(sprintf('Parent node for %s in %s not found',

Neos.EventSourcedContentRepository/Classes/Domain/Context/NodeAggregate/NodeAggregate.php
@@ -161,15 +161,15 @@ final class NodeAggregate
                 switch (get_class($event)) {
                     case NodeAggregateWithNodeWasCreated::class:
                         /** @var NodeAggregateWithNodeWasCreated $event */
-                        $dimensionSpacePointOccupied |= $event->getDimensionSpacePoint()->equals($dimensionSpacePoint);
+                        $dimensionSpacePointOccupied = $dimensionSpacePointOccupied || $event->getDimensionSpacePoint()->equals($dimensionSpacePoint);
                         break;
                     case Event\NodeSpecializationWasCreated::class:
                         /** @var Event\NodeSpecializationWasCreated $event */
-                        $dimensionSpacePointOccupied |= $event->getSpecializationLocation()->equals($dimensionSpacePoint);
+                        $dimensionSpacePointOccupied = $dimensionSpacePointOccupied || $event->getSpecializationLocation()->equals($dimensionSpacePoint);
                         break;
                     case Event\NodeGeneralizationWasCreated::class:
                         /** @var Event\NodeGeneralizationWasCreated $event */
-                        $dimensionSpacePointOccupied |= $event->getGeneralizationLocation()->equals($dimensionSpacePoint);
+                        $dimensionSpacePointOccupied = $dimensionSpacePointOccupied || $event->getGeneralizationLocation()->equals($dimensionSpacePoint);
                         break;
                     default:
                         continue;

Neos.EventSourcedContentRepository/Classes/Domain/Context/Workspace/WorkspaceCommandHandler.php
@@ -198,10 +198,10 @@ final class WorkspaceCommandHandler
         // - extract the initial ContentStreamWasForked event, to read the version of the source content stream when the fork occurred
         // - ensure that no other changes have been done in the meantime in the base content stream
         $workspaceContentStreamName = ContentStreamEventStreamName::fromContentStreamIdentifier($workspace->getCurrentContentStreamIdentifier());
-        $eventStore = $this->eventStoreManager->getEventStoreForStreamName($workspaceContentStreamName);
+        $eventStore = $this->eventStoreManager->getEventStoreForStreamName($workspaceContentStreamName->getEventStreamName());
 
         /* @var $workspaceContentStream EventAndRawEvent[] */
-        $workspaceContentStream = iterator_to_array($eventStore->get(new StreamNameFilter($workspaceContentStreamName)));
+        $workspaceContentStream = iterator_to_array($eventStore->get(new StreamNameFilter($workspaceContentStreamName->getEventStreamName())));
 
         $events = [];
         foreach ($workspaceContentStream as $eventAndRawEvent) {
@@ -216,7 +216,7 @@ final class WorkspaceCommandHandler
         $contentStreamWasForked = $this->extractSingleForkedContentStreamEvent($workspaceContentStream);
         try {
             $baseWorkspaceContentStreamName = ContentStreamEventStreamName::fromContentStreamIdentifier($baseWorkspace->getCurrentContentStreamIdentifier());
-            $this->eventPublisher->publishMany($baseWorkspaceContentStreamName, $events, $contentStreamWasForked->getVersionOfSourceContentStream());
+            $this->eventPublisher->publishMany($baseWorkspaceContentStreamName->getEventStreamName(), $events, $contentStreamWasForked->getVersionOfSourceContentStream());
         } catch (ConcurrencyException $e) {
             throw new BaseWorkspaceHasBeenModifiedInTheMeantime(sprintf('The base workspace has been modified in the meantime; please rebase. Expected version %d of source content stream %s', $contentStreamWasForked->getVersionOfSourceContentStream(), $baseWorkspace->getCurrentContentStreamIdentifier()));
         }
@@ -280,9 +280,9 @@ final class WorkspaceCommandHandler
         );
 
         $workspaceContentStreamName = ContentStreamEventStreamName::fromContentStreamIdentifier($workspace->getCurrentContentStreamIdentifier());
-        $eventStore = $this->eventStoreManager->getEventStoreForStreamName($workspaceContentStreamName);
+        $eventStore = $this->eventStoreManager->getEventStoreForStreamName($workspaceContentStreamName->getEventStreamName());
 
-        $workspaceContentStream = $eventStore->get(new StreamNameFilter($workspaceContentStreamName));
+        $workspaceContentStream = $eventStore->get(new StreamNameFilter($workspaceContentStreamName->getEventStreamName()));
         foreach ($workspaceContentStream as $eventAndRawEvent) {
             $metadata = $eventAndRawEvent->getRawEvent()->getMetadata();
             if (isset($metadata['commandClass'])) {

Neos.EventSourcedContentRepository/Classes/Domain/Projection/Workspace/WorkspaceProjector.php
@@ -85,7 +85,7 @@ final class WorkspaceProjector implements ProjectorInterface
         ]);
 
         // TODO: HACK to update in-memory projection(!!!!!!) nasty!!!
-        $this->workspaceFinder->findOneByName($event->getWorkspaceName())->currentContentStreamIdentifier = $event->getCurrentContentStreamIdentifier();
+        $this->workspaceFinder->findOneByName($event->getWorkspaceName())->currentContentStreamIdentifier = (string)$event->getCurrentContentStreamIdentifier();
     }
 
     public function reset(): void

Neos.EventSourcedContentRepository/Tests/Behavior/Features/Bootstrap/EventSourcedTrait.php
@@ -1195,7 +1195,7 @@ trait EventSourcedTrait
 
         $subtree = $this->contentGraphInterface
             ->getSubgraphByIdentifier($this->contentStreamIdentifier, $this->dimensionSpacePoint, $this->visibilityConstraints)
-            ->findSubtrees([$nodeAggregateIdentifier], $maximumLevels, $nodeTypeConstraints);
+            ->findSubtrees([$nodeAggregateIdentifier], (int)$maximumLevels, $nodeTypeConstraints);
 
         /** @var SubtreeInterface[] $flattenedSubtree */
         $flattenedSubtree = [];