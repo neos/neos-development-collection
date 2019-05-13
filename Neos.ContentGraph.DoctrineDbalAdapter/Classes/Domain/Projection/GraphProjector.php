8482a10d40c7fc0285067973194183208ab30c8d
Bernhard Schmitt <schmitt@sitegeist.de>
2019-05-13 14:54:48 +0200

    Only remove directly attached restriction edges when a node was enabled

--- Neos.ContentGraph.DoctrineDbalAdapter/Classes/Domain/Projection/GraphProjector.php
+++ Neos.ContentGraph.DoctrineDbalAdapter/Classes/Domain/Projection/GraphProjector.php
@@ -651,59 +651,14 @@ insert ignore into neos_contentgraph_restrictionrelation
         $this->getDatabaseConnection()->executeUpdate('
                 -- GraphProjector::removeOutgoingRestrictionRelationsOfNodeAggregateInDimensionSpacePoints
  
-                delete r.* from
-                    neos_contentgraph_restrictionedge r
-                    join 
-                     (
-                        -- we build a recursive tree
-                        with recursive tree as (
-                             -- --------------------------------
-                             -- INITIAL query: select the root nodes of the tree
-                             -- --------------------------------
-                             select
-                                n.relationanchorpoint,
-                                n.nodeaggregateidentifier,
-                                h.dimensionspacepointhash
-                             from
-                                neos_contentgraph_node n
-                             -- we need to join with the hierarchy relation, because we need the dimensionspacepointhash.
-                             inner join neos_contentgraph_hierarchyrelation h
-                                on h.childnodeanchor = n.relationanchorpoint
-                             where
-                                n.nodeaggregateidentifier = :entryNodeAggregateIdentifier
-                                and h.contentstreamidentifier = :contentStreamIdentifier
-                                and h.dimensionspacepointhash in (:dimensionSpacePointHashes)
-                        union
-                             -- --------------------------------
-                             -- RECURSIVE query: do one "child" query step
-                             -- --------------------------------
-                             select
-                                c.relationanchorpoint,
-                                c.nodeaggregateidentifier,
-                                h.dimensionspacepointhash
-                             from
-                                tree p
-                             inner join neos_contentgraph_hierarchyrelation h
-                                on h.parentnodeanchor = p.relationanchorpoint
-                             inner join neos_contentgraph_node c
-                                on h.childnodeanchor = c.relationanchorpoint
-                             where
-                                h.contentstreamidentifier = :contentStreamIdentifier
-                                and h.dimensionspacepointhash in (:dimensionSpacePointHashes)
-                        )
-                        select * from tree
-                     ) as tree
-
-                -- the "tree" CTE now contains a list of tuples (nodeAggregateIdentifier,dimensionSpacePointHash)
-                -- which are *descendants* of the starting NodeAggregateIdentifier (in the given DimensionSpacePointHashes).
-                where
-                    r.contentstreamidentifier = :contentStreamIdentifier
-                    and r.dimensionspacepointhash = tree.dimensionspacepointhash
-                    and r.affectednodeaggregateidentifier = tree.nodeaggregateidentifier
-            ',
+                DELETE r.*
+                    FROM neos_contentgraph_restrictionrelation r
+                    WHERE r.contentstreamidentifier = :contentStreamIdentifier
+                    AND r.originnodeaggregateidentifier = :originNodeAggregateIdentifier
+                    AND r.dimensionspacepointhash in (:dimensionSpacePointHashes)',
             [
-                'entryNodeAggregateIdentifier' => (string)$nodeAggregateIdentifier,
                 'contentStreamIdentifier' => (string)$contentStreamIdentifier,
+                'originNodeAggregateIdentifier' => (string)$nodeAggregateIdentifier,
                 'dimensionSpacePointHashes' => $affectedDimensionSpacePoints->getPointHashes()
             ],
             [
