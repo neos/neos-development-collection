23ce30e01877eab437c72a94a9134d5a8498779b
The Neos Team <hello@neos.io>
2019-06-15 11:08:22 +0000

    Apply fixes from StyleCI

--- Neos.Neos/Tests/Functional/Configuration/ConfigurationValidationTest.php
+++ Neos.Neos/Tests/Functional/Configuration/ConfigurationValidationTest.php
@@ -11,7 +11,6 @@ namespace Neos\Neos\Tests\Functional\Configuration;
  * source code.
  */
 
-use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Tests\Functional\Configuration\ConfigurationValidationTest as FlowConfigurationValidationTest;
 
 /**
