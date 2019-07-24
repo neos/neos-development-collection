023ee9b0c29a2e57b57cfb6a6589031b0b10ea2c
Markus Günther <info@unikka.de>
2019-07-25 01:08:39 +0200

    TASK: Remove compass config in favor of webpack

--- Neos.Neos/webpack.config.js
+++ Neos.Neos/webpack.config.js
@@ -10,7 +10,7 @@ const webpackConfig = {
     Main: [
         './Resources/Public/JavaScript/index.js',
         './Resources/Private/Styles/Neos.scss',
-    ]
+    ],
   },
   output: {
     path: __dirname + '/Resources/Public/JavaScript',
@@ -63,6 +63,67 @@ const webpackConfig = {
   }
 };
 
+const stylesConfig = {
+    context: __dirname,
+    devtool: debug ? 'inline-sourcemap' : false,
+    entry: {
+        Login: [
+            './Resources/Private/Styles/Login.scss',
+        ],
+        Error: [
+            './Resources/Private/Styles/Error.scss',
+        ],
+        RawContentMode: [
+            './Resources/Private/Styles/RawContentMode.scss',
+        ],
+        Welcome: [
+            './Resources/Private/Styles/Welcome.scss',
+        ]
+    },
+    output: {
+        path: __dirname + '/Resources/Public/Styles',
+        filename: '[name].css'
+    },
+    module: {
+        rules: [
+        {
+            test    : /\.(gif|png|jpg|svg)$/,
+            include : __dirname + '/Resources/Public/Images/',
+            loader  : 'url-loader?limit=30000&name=images/[name].[ext]'
+        },
+        {
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
+        },
+        {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+            {
+                loader: 'file-loader',
+                options: {
+                name: '[name].[ext]',
+                outputPath: '../Fonts/'
+                }
+            }
+            ]
+        }
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({filename: './[name].css'})
+    ],
+    optimization: {
+        minimizer: []
+    },
+    performance: {
+        hints: debug ? 'warning' : false
+    }
+};
+
 
 if (!debug) {
   webpackConfig.optimization.minimizer.push(
@@ -77,6 +138,20 @@ if (!debug) {
           }
       }),
   );
+
+  stylesConfig.optimization.minimizer.push(
+    new TerserPlugin({
+        terserOptions: {
+            sourceMap: true,
+            warnings: false,
+            parse: {},
+            compress: {},
+            mangle: true,
+            keep_fnames: true
+        }
+    }),
+);
 }
 
 module.exports = webpackConfig;
+module.exports = stylesConfig;
