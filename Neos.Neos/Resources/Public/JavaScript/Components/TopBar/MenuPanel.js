7c9cdedd1f17a4c6287b6a143326d885c4013151
Markus Günther <info@unikka.de>
2019-08-17 02:06:01 +0200

    FEATURE: Implement toggle handling for menu sections

--- Neos.Neos/Resources/Public/JavaScript/Components/TopBar/MenuPanel.js
+++ Neos.Neos/Resources/Public/JavaScript/Components/TopBar/MenuPanel.js
@@ -1,9 +1,24 @@
+import Expandable from './Expandable';
+
 export default class MenuPanel {
 	constructor(_root) {
 		this._root = _root;
 		this._button = this._root.querySelectorAll('.neos-menu-button');
 		this._panel = this._root.querySelectorAll('.neos-menu-panel');
 		this._setupEventListeners();
+
+		if (this._panel) {
+			this._initializeMenuSections();
+		}
+	}
+
+	_initializeMenuSections() {
+		this._panel.forEach(_panel => {
+			const menuSectionElements = _panel.querySelectorAll('.neos-menu-section');
+			menuSectionElements.forEach(menuSectionElement => {
+				new Expandable(menuSectionElement, '.neos-menu-panel-toggle');
+			});
+		});
 	}
 
 	_setupEventListeners() {
