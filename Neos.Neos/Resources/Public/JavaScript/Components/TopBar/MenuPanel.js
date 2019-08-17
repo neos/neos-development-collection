b7b7f2bdf3c453283916d72fdf3ad1b5eac2dc83
Markus Günther <info@unikka.de>
2019-08-18 00:25:47 +0200

    FEATURE: State management for menu sections

--- Neos.Neos/Resources/Public/JavaScript/Components/TopBar/MenuPanel.js
+++ Neos.Neos/Resources/Public/JavaScript/Components/TopBar/MenuPanel.js
@@ -1,10 +1,13 @@
 import Expandable from './Expandable';
+import { isNil, getItemByKeyValue } from '../../Helper';
 
+const SESSION_KEY = 'Neos.Neos.menuSectionStates';
 export default class MenuPanel {
 	constructor(_root) {
 		this._root = _root;
 		this._button = this._root.querySelectorAll('.neos-menu-button');
 		this._panel = this._root.querySelectorAll('.neos-menu-panel');
+		this._menuSectionStates = this._loadSessionData();
 		this._setupEventListeners();
 
 		if (this._panel) {
@@ -15,8 +18,17 @@ export default class MenuPanel {
 	_initializeMenuSections() {
 		this._panel.forEach(_panel => {
 			const menuSectionElements = _panel.querySelectorAll('.neos-menu-section');
+			const { sections } = this._menuSectionStates;
 			menuSectionElements.forEach(menuSectionElement => {
-				new Expandable(menuSectionElement, '.neos-menu-panel-toggle');
+				const sectionName = menuSectionElement.getAttribute('data-key');
+				const sectionState = getItemByKeyValue(sections, 'name', sectionName);
+				const initalState = !isNil(sectionState) ? sectionState.open : false;
+				new Expandable(
+					menuSectionElement,
+					'.neos-menu-panel-toggle',
+					this._onMenuSectionStateChange.bind(this),
+					initalState
+				);
 			});
 		});
 	}
@@ -25,6 +37,39 @@ export default class MenuPanel {
 		this._button.forEach(_toggleButton => {
 			_toggleButton.addEventListener('click', this._toggle.bind(this));
 		});
+	}
+
+	_loadSessionData() {
+		const sessionData = sessionStorage.getItem(SESSION_KEY);
+		if (isNil(sessionData)) {
+			const initialSessionData = { sections: [] };
+			sessionStorage.setItem(SESSION_KEY, JSON.stringify(initialSessionData));
+			return initialSessionData;
+		}
+
+		return JSON.parse(sessionData);
+	}
+
+	_onMenuSectionStateChange(sectionName, newValue) {
+		const { sections } = this._menuSectionStates;
+		const sectionState = getItemByKeyValue(sections, 'name', sectionName);
+		if (isNil(sections)) {
+			this._menuSectionStates.sections = [];
+		}
+
+		if (isNil(sectionState)) {
+			this._menuSectionStates.sections.push({
+				name: sectionName,
+				open: newValue
+			});
+		} else {
+			sectionState.open = newValue;
+		}
+
+		sessionStorage.setItem(
+			SESSION_KEY,
+			JSON.stringify(this._menuSectionStates)
+		);
 	}
 
 	_toggle(_event) {
