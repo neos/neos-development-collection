3a89a1d4460da2f92d70fdc7c488ce7c630b1af7
Markus Günther <info@unikka.de>
2019-09-02 12:29:47 +0200

    TASK: Remove getJavascriptBuiltVersion handling in configuration viewhelper

--- Neos.Neos/Classes/ViewHelpers/Backend/JavascriptConfigurationViewHelper.php
+++ Neos.Neos/Classes/ViewHelpers/Backend/JavascriptConfigurationViewHelper.php
@@ -127,9 +127,6 @@ class JavascriptConfigurationViewHelper extends AbstractViewHelper
         $neosJavaScriptBasePath = $this->getStaticResourceWebBaseUri('resource://Neos.Neos/Public/JavaScript');
 
         $configuration[] = 'window.T3Configuration.neosJavascriptBasePath = ' . json_encode($neosJavaScriptBasePath) . ';';
-        if ($this->backendAssetsUtility->shouldLoadMinifiedJavascript()) {
-            $configuration[] = 'window.T3Configuration.neosJavascriptVersion = ' . json_encode($this->backendAssetsUtility->getJavascriptBuiltVersion()) . ';';
-        }
 
         if ($this->bootstrap->getContext()->isDevelopment()) {
             $configuration[] = 'window.T3Configuration.DevelopmentMode = true;';
