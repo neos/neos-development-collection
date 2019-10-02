a36d74867f7528a8be6c9902015776e2408cc96f
Karsten Dambekalns <karsten@dambekalns.de>
2019-10-02 16:24:28 +0200

    FEATURE: Command to render missing asset variants from presets
    This adds a media:rendervariants command that will render variants
    for all assets that are configured but do not exist.

--- Neos.Media/Classes/Domain/Repository/ImageRepository.php
+++ Neos.Media/Classes/Domain/Repository/ImageRepository.php
@@ -11,7 +11,9 @@ namespace Neos\Media\Domain\Repository;
  * source code.
  */
 
+use Doctrine\ORM\QueryBuilder;
 use Neos\Flow\Annotations as Flow;
+use Neos\Media\Domain\Model\ImageVariant;
 
 /**
  * A repository for Images
@@ -20,4 +22,39 @@ use Neos\Flow\Annotations as Flow;
  */
 class ImageRepository extends AssetRepository
 {
+    /**
+     * Return raw data about existing assets and their variants
+     *
+     * @return array
+     */
+    public function findAssetIdentifiersWithVariants(): array
+    {
+        /** @var QueryBuilder $queryBuilder */
+        $assetIdentifiers = array_column($this->entityManager->createQuery(sprintf('SELECT i.Persistence_Object_Identifier  FROM %s i', $this->entityClassName))->getScalarResult(), 'Persistence_Object_Identifier');
+
+        /** @var QueryBuilder $queryBuilder */
+        $queryBuilder = $this->entityManager->createQueryBuilder();
+        $queryBuilder
+            ->select('IDENTITY(v.originalAsset) AS assetIdentifier, v.presetIdentifier, v.presetVariantName')
+            ->from(ImageVariant::class, 'v');
+
+        $rawVariantData = $queryBuilder->getQuery()->getArrayResult();
+
+        $variantData = [];
+        foreach ($rawVariantData as $item) {
+            if (!isset($variantData[$item['assetIdentifier']])) {
+                $variantData[$item['assetIdentifier']] = [];
+            }
+            if ($item['presetIdentifier']) {
+                $variantData[$item['assetIdentifier']][$item['presetIdentifier']][$item['presetVariantName']] = true;
+            }
+        }
+
+        $result = [];
+        foreach ($assetIdentifiers as $assetIdentifier) {
+            $result[$assetIdentifier] = $variantData[$assetIdentifier] ?? [];
+        }
+
+        return $result;
+    }
 }
