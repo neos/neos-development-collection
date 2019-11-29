d6321c43754407b363a16ed1c2ecc765c7c5bccd
Markus Günther <info@unikka.de>
2019-11-29 10:04:53 +0100

    FEATURE: Extract storage handling to own service

--- Neos.Neos/Resources/Public/JavaScript/Components/TopBar/MenuPanel.js
+++ Neos.Neos/Resources/Public/JavaScript/Components/TopBar/MenuPanel.js
@@ -1,13 +1,12 @@
-import Expandable from './Expandable';
-import { isNil, getCollectionValueByPath } from '../../Helper';
+import Expandable from "./Expandable";
+import { loadStorageData, saveStorageData } from "../../Services/Storage";
 
-const STORAGE_KEY = 'persistedState';
-const VALUE_PATH = 'ui.drawer.collapsedMenuGroups';
+const VALUE_PATH = "ui.drawer.collapsedMenuGroups";
 export default class MenuPanel {
 	constructor(_root) {
 		this._root = _root;
-		this._button = this._root.querySelectorAll('.neos-menu-button');
-		this._panel = this._root.querySelectorAll('.neos-menu-panel');
+		this._button = this._root.querySelectorAll(".neos-menu-button");
+		this._panel = this._root.querySelectorAll(".neos-menu-panel");
 		this._menuSectionStates = this._loadMenuSectionStates();
 		this._setupEventListeners();
 
@@ -18,14 +17,14 @@ export default class MenuPanel {
 
 	_initializeMenuSections() {
 		this._panel.forEach(_panel => {
-			const menuSectionElements = _panel.querySelectorAll('.neos-menu-section');
+			const menuSectionElements = _panel.querySelectorAll(".neos-menu-section");
 			const sections = this._menuSectionStates;
 			menuSectionElements.forEach(menuSectionElement => {
-				const sectionName = menuSectionElement.getAttribute('data-key');
+				const sectionName = menuSectionElement.getAttribute("data-key");
 				const sectionState = !sections.includes(sectionName);
 				new Expandable(
 					menuSectionElement,
-					'.neos-menu-panel-toggle',
+					".neos-menu-panel-toggle",
 					this._onMenuSectionStateChange.bind(this),
 					sectionState
 				);
@@ -35,37 +34,18 @@ export default class MenuPanel {
 
 	_setupEventListeners() {
 		this._button.forEach(_toggleButton => {
-			_toggleButton.addEventListener('click', this._toggle.bind(this));
+			_toggleButton.addEventListener("click", this._toggle.bind(this));
 		});
 	}
 
-	_loadStorageData() {
-		const storageData = localStorage.getItem(STORAGE_KEY);
-		if (isNil(storageData)) {
-			const initialStorageData = {
-				ui: {
-					drawer: {
-						collapsedMenuGroups: []
-					}
-				}
-			};
-			localStorage.setItem(STORAGE_KEY, JSON.stringify(initialStorageData));
-			return initialStorageData;
-		}
-
-		return JSON.parse(storageData);
-	}
-
 	_loadMenuSectionStates() {
-		const storageData = this._loadStorageData();
-		return getCollectionValueByPath(storageData, VALUE_PATH);
+		const storageData = loadStorageData(VALUE_PATH, []);
+		return Array.isArray(storageData) ? storageData : [];
 	}
 
 	_saveMenuSectionStates() {
-		const storageData = this._loadStorageData();
-		if (!isNil(storageData) && Array.isArray(this._menuSectionStates)) {
-			storageData.ui.drawer.collapsedMenuGroups = this._menuSectionStates;
-			localStorage.setItem(STORAGE_KEY, JSON.stringify(storageData));
+		if (Array.isArray(this._menuSectionStates)) {
+			saveStorageData(VALUE_PATH, this._menuSectionStates);
 		}
 	}
 
@@ -85,8 +65,8 @@ export default class MenuPanel {
 
 	_toggle(_event) {
 		this._button.forEach(_toggleButton => {
-			_toggleButton.classList.toggle('neos-pressed');
+			_toggleButton.classList.toggle("neos-pressed");
 		});
-		document.body.classList.toggle('neos-menu-panel-open');
+		document.body.classList.toggle("neos-menu-panel-open");
 	}
 }
---
+++ Neos.Neos/Resources/Public/JavaScript/Services/Storage.js
@@ -0,0 +1,38 @@
+import {
+	isNil,
+	getCollectionValueByPath,
+	createCollectionByPath
+} from "../Helper";
+
+const STORAGE_KEY = "persistedState";
+
+const getStorage = () => {
+	const storage = localStorage.getItem(STORAGE_KEY);
+	return JSON.parse(storage);
+};
+
+const loadStorageData = (path, defaultValue) => {
+	const storage = getStorage();
+	const storageData = getCollectionValueByPath(storage, path);
+	if (isNil(storageData)) {
+		const initialStorageData = createCollectionByPath(
+			{},
+			path,
+			isNil(defaultValue) ? null : defaultValue
+		);
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(initialStorageData));
+		return initialStorageData;
+	}
+
+	return storageData;
+};
+
+const saveStorageData = (path, value) => {
+	const storage = getStorage();
+	const updatedStorageData = createCollectionByPath(storage, path, value);
+	if (!isNil(updatedStorageData)) {
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(updatedStorageData));
+	}
+}
+
+export { loadStorageData, saveStorageData };
