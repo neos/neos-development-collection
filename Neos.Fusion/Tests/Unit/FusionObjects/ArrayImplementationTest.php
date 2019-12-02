e203ccda192a37a4726ef25e57f65e314eb04e97
bwaidelich <b.waidelich@wwwision.de>
2019-12-02 10:25:09 +0100

    Fix ArrayImplementation Unit Tests

--- Neos.Fusion/Tests/Unit/FusionObjects/ArrayImplementationTest.php
+++ Neos.Fusion/Tests/Unit/FusionObjects/ArrayImplementationTest.php
@@ -26,6 +26,7 @@ class ArrayImplementationTest extends UnitTestCase
     public function evaluateWithEmptyArrayRendersNull()
     {
         $mockRuntime = $this->getMockBuilder(Runtime::class)->disableOriginalConstructor()->getMock();
+        $mockRuntime->method('canRender')->willReturn(true);
         $path = 'array/test';
         $fusionObjectName = 'Neos.Fusion:Array';
         $renderer = new ArrayImplementation($mockRuntime, $path, $fusionObjectName);
@@ -118,6 +119,7 @@ class ArrayImplementationTest extends UnitTestCase
     public function evaluateRendersKeysSortedByPositionMetaProperty($message, $subElements, $expectedKeyOrder)
     {
         $mockRuntime = $this->getMockBuilder(Runtime::class)->disableOriginalConstructor()->getMock();
+        $mockRuntime->method('canRender')->willReturn(true);
 
         $mockRuntime->expects(self::any())->method('evaluate')->will(self::returnCallback(function ($path) use (&$renderedPaths) {
             $renderedPaths[] = $path;
