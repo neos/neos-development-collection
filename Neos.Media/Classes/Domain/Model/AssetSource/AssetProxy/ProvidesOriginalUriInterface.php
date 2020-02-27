df1942546930b6a7c8a03723e4ed1381ad5e2958
Karsten Dambekalns <karsten@dambekalns.de>
2020-02-27 11:21:43 +0100

    TASK: Indentation fix
    Co-Authored-By: Daniel Lienert <daniel@lienert.cc>

--- Neos.Media/Classes/Domain/Model/AssetSource/AssetProxy/ProvidesOriginalUriInterface.php
+++ Neos.Media/Classes/Domain/Model/AssetSource/AssetProxy/ProvidesOriginalUriInterface.php
@@ -5,7 +5,7 @@ namespace Neos\Media\Domain\Model\AssetSource\AssetProxy;
  * This file is part of the Neos.Media package.
  *
  * (c) Contributors of the Neos Project - www.neos.io
-  *
+ *
  * This package is Open Source Software. For the full copyright and license
  * information, please view the LICENSE file which was distributed with this
  * source code.
