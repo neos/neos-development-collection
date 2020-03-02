a728974e91c18ce60ef3d40d5105926506fe7803
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2020-03-02 14:19:11 +0100

    TASK: disable change projection for enabling/disabling nodes

--- Neos.EventSourcedContentRepository/Classes/Domain/Projection/Changes/ChangeProjector.php
+++ Neos.EventSourcedContentRepository/Classes/Domain/Projection/Changes/ChangeProjector.php
@@ -65,14 +65,16 @@ class ChangeProjector implements ProjectorInterface
     public function whenNodeAggregateWasDisabled(NodeAggregateWasDisabled $event)
     {
         foreach ($event->getAffectedDimensionSpacePoints()->getPoints() as $dimensionSpacePoint) {
-            $this->markAsChanged($event->getContentStreamIdentifier(), $event->getNodeAggregateIdentifier(), $dimensionSpacePoint);
+            // TODO: the following line does not work, because $dimensionSpacePoint is of type DimensionSpacePoint, but the markAsChanged accepts only an OriginDimensionSpacePoint
+            //$this->markAsChanged($event->getContentStreamIdentifier(), $event->getNodeAggregateIdentifier(), $dimensionSpacePoint);
         }
     }
 
     public function whenNodeAggregateWasEnabled(NodeAggregateWasEnabled $event)
     {
         foreach ($event->getAffectedDimensionSpacePoints()->getPoints() as $dimensionSpacePoint) {
-            $this->markAsChanged($event->getContentStreamIdentifier(), $event->getNodeAggregateIdentifier(), $dimensionSpacePoint);
+            // TODO: the following line does not work, because $dimensionSpacePoint is of type DimensionSpacePoint, but the markAsChanged accepts only an OriginDimensionSpacePoint
+            //$this->markAsChanged($event->getContentStreamIdentifier(), $event->getNodeAggregateIdentifier(), $dimensionSpacePoint);
         }
     }
 
