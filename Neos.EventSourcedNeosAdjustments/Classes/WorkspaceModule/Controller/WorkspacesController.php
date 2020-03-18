47de11c9c121f57a5a32d4d4c4b78908f266b088
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2020-03-18 08:28:05 +0100

    FEATURE: allow workspaces module to be called basically (remainder not yet working)

--- Neos.EventSourcedNeosAdjustments/Classes/WorkspaceModule/Controller/WorkspacesController.php
+++ Neos.EventSourcedNeosAdjustments/Classes/WorkspaceModule/Controller/WorkspacesController.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Neos\EventSourcedNeosAdjustments\WorkspaceModule;
+namespace Neos\EventSourcedNeosAdjustments\WorkspaceModule\Controller;
 
 /*
  * This file is part of the Neos.Neos package.
