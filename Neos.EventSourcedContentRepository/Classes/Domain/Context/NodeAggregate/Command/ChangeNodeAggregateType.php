84ab9ef5bf23aac6f07ec3446a7b49152435942f
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2020-08-20 20:30:33 +0200

    TASK: disable proxying

--- Neos.EventSourcedContentRepository/Classes/Domain/Context/NodeAggregate/Command/ChangeNodeAggregateType.php
+++ Neos.EventSourcedContentRepository/Classes/Domain/Context/NodeAggregate/Command/ChangeNodeAggregateType.php
@@ -12,6 +12,7 @@ namespace Neos\EventSourcedContentRepository\Domain\Context\NodeAggregate\Comman
  * source code.
  */
 
+use Neos\Flow\Annotations as Flow;
 use Neos\ContentRepository\Domain\ContentStream\ContentStreamIdentifier;
 use Neos\ContentRepository\Domain\NodeAggregate\NodeAggregateIdentifier;
 use Neos\ContentRepository\Domain\NodeType\NodeTypeName;
@@ -21,6 +22,9 @@ use Neos\EventSourcedContentRepository\Domain\Context\NodeAggregate\MatchableWit
 use Neos\EventSourcedContentRepository\Domain\Context\NodeAggregate\NodeAggregateIdentifiersByNodePaths;
 use Neos\EventSourcedContentRepository\Domain\Context\NodeAggregate\NodeAggregateTypeChangeChildConstraintConflictResolutionStrategy;
 
+/**
+ * @Flow\Proxy(false)
+ */
 final class ChangeNodeAggregateType implements \JsonSerializable, RebasableToOtherContentStreamsInterface, MatchableWithNodeAddressInterface
 {
     /**
