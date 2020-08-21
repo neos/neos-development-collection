9631e0175968a257fcea73e3bde90631ea3d8f17
Martin Ficzel <ficzel@sitegeist.de>
2020-08-21 17:07:51 +0200

    FEATURE: Add NodeTypePresetPostprocessor
    This allows to configure nodeType Presets via settings and apply them to nodeTypes via.
    Other than Mixins this allows to ensure consistent configurations across differently named
    properties. The presets can be applied by definining the `options.preset` for childNodes or properties.
    
    ```
    Neos:
      Neos:
        nodeTypes:
          presets:
            childNods:
              vendor:
                constraintPreset:
                  constraints:
                    'Neos.Neos:Content': true
                    'Neos.NodeTypes.BaseMixins:TitleMixin': true
    
            properties:
              vendor:
                textBlock:
                  type: string
                  ui:
                    inlineEditable
                type: string
                defaultValue: ''
                ui:
                  inlineEditable: true
                  inline:
                    editorOptions:
                      placeholder: '(( text block ))'
                      autoparagraph: true
                      formatting:
                        strong: true
                        em: true
                        u: false
                        underline: false
    ```

---
+++ Neos.Neos/Classes/NodeTypePostprocessor/NodeTypePresetPostprocessor.php
@@ -0,0 +1,75 @@
+<?php
+namespace Neos\Neos\NodeTypePostprocessor;
+
+/*
+ * This file is part of the Neos.Neos package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\ContentRepository\NodeTypePostprocessor\NodeTypePostprocessorInterface;
+use Neos\ContentRepository\Domain\Model\NodeType;
+use Neos\Utility\Arrays;
+
+/**
+ * Apply presets from configuration to nodeTypes
+ */
+class NodeTypePresetPostprocessor implements NodeTypePostprocessorInterface
+{
+
+    /**
+     * @var array
+     * @Flow\InjectConfiguration(package="Neos.Neos", path="nodeTypes.presets.properties")
+     */
+    protected $propertyPresetConfiguration;
+
+    /**
+     * @var array
+     * @Flow\InjectConfiguration(package="Neos.Neos", path="nodeTypes.presets.childNodes")
+     */
+    protected $childNodePresetConfiguration;
+
+    /**
+     * @param NodeType $nodeType (uninitialized) The node type to process
+     * @param array $configuration input configuration
+     * @param array $options The processor options
+     * @return void
+     */
+    public function process(NodeType $nodeType, array &$configuration, array $options): void
+    {
+        if ($nodeType->hasConfiguration('properties')) {
+            foreach ($nodeType->getConfiguration('properties') as $propertyName => $propertyConfiguration) {
+                if ($preset = Arrays::getValueByPath($propertyConfiguration, 'options.preset')) {
+                    $presetConfiguration = Arrays::getValueByPath($this->propertyPresetConfiguration, $preset);
+                    if ($presetConfiguration) {
+                        $mergedPropertyConfiguration = Arrays::arrayMergeRecursiveOverrule(
+                            $presetConfiguration,
+                            $propertyConfiguration
+                        );
+                        $configuration['properties'][$propertyName] = $mergedPropertyConfiguration;
+                    }
+                }
+            }
+        }
+
+        if ($nodeType->hasConfiguration('childNodes')) {
+            foreach ($nodeType->getConfiguration('childNodes') as $propertyName => $propertyConfiguration) {
+                if ($preset = Arrays::getValueByPath($propertyConfiguration, 'options.preset')) {
+                    $presetConfiguration = Arrays::getValueByPath($this->childNodePresetConfiguration, $preset);
+                    if ($presetConfiguration) {
+                        $mergedPropertyConfiguration = Arrays::arrayMergeRecursiveOverrule(
+                            $presetConfiguration,
+                            $propertyConfiguration
+                        );
+                        $configuration['childNodes'][$propertyName] = $mergedPropertyConfiguration;
+                    }
+                }
+            }
+        }
+    }
+}
