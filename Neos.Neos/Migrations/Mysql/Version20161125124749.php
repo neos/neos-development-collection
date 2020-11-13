d22d3a22cef8816ce127f5de83c4f317025cd220
Karsten Dambekalns <karsten@dambekalns.de>
2020-11-13 10:52:38 +0100

    !!! TASK: Adjust to doctrine/migrations v3
    Adjust DB migrations to Doctrine Migrations 3.0

--- Neos.Neos/Migrations/Mysql/Version20161125124749.php
+++ Neos.Neos/Migrations/Mysql/Version20161125124749.php
@@ -1,7 +1,7 @@
 <?php
 namespace Neos\Flow\Persistence\Doctrine\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
@@ -13,7 +13,7 @@ class Version20161125124749 extends AbstractMigration
      * @param Schema $schema
      * @return void
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void 
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on "mysql".');
 
@@ -30,7 +30,7 @@ class Version20161125124749 extends AbstractMigration
      * @param Schema $schema
      * @return void
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema): void 
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on "mysql".');
 
