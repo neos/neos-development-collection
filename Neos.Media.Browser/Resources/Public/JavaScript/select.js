16f1201c693ae4e4d467ca6d1bc1a4996f54719b
Markus Günther <info@unikka.de>
2021-03-17 20:42:18 +0100

    BUGFIX: Fix jQuery usage for Media.Browser

--- Neos.Media.Browser/Resources/Public/JavaScript/select.js
+++ Neos.Media.Browser/Resources/Public/JavaScript/select.js
@@ -1,30 +1,32 @@
-(function ($) {
-    if (window.parent !== window && window.parent.NeosMediaBrowserCallbacks) {
-        // we are inside iframe
-        $('.asset-list').on('click', '[data-asset-identifier]', function(e) {
-            if (
-                $(e.target).closest('a, button').not('[data-asset-identifier]').length === 0 &&
-                window.parent.NeosMediaBrowserCallbacks &&
-                typeof window.parent.NeosMediaBrowserCallbacks.assetChosen === 'function'
-            ) {
-                let localAssetIdentifier = $(this).attr('data-local-asset-identifier');
-                if (localAssetIdentifier !== '') {
-                    window.parent.NeosMediaBrowserCallbacks.assetChosen(localAssetIdentifier);
-                } else {
-                    $.post(
-                        $('link[rel="neos-media-browser-service-assetproxies-import"]').attr('href'),
-                        {
-                            assetSourceIdentifier: $(this).attr('data-asset-source-identifier'),
-                            assetIdentifier: $(this).attr('data-asset-identifier'),
-                            __csrfToken: $('body').attr('data-csrf-token')
-                        },
-                        function(data) {
-                            window.parent.NeosMediaBrowserCallbacks.assetChosen(data.localAssetIdentifier);
-                        }
-                    );
-                }
-                e.preventDefault();
-            }
-        });
-    }
-})(jQuery);
+window.addEventListener('DOMContentLoaded', (event) => {
+	jQuery(function() {
+			if (window.parent !== window && window.parent.NeosMediaBrowserCallbacks) {
+					// we are inside iframe
+					jQuery('.asset-list').on('click', '[data-asset-identifier]', function(e) {
+							if (
+								jQuery(e.target).closest('a, button').not('[data-asset-identifier]').length === 0 &&
+									window.parent.NeosMediaBrowserCallbacks &&
+									typeof window.parent.NeosMediaBrowserCallbacks.assetChosen === 'function'
+							) {
+									let localAssetIdentifier = jQuery(this).attr('data-local-asset-identifier');
+									if (localAssetIdentifier !== '') {
+											window.parent.NeosMediaBrowserCallbacks.assetChosen(localAssetIdentifier);
+									} else {
+										jQuery.post(
+													jQuery('link[rel="neos-media-browser-service-assetproxies-import"]').attr('href'),
+													{
+															assetSourceIdentifier: jQuery(this).attr('data-asset-source-identifier'),
+															assetIdentifier: jQuery(this).attr('data-asset-identifier'),
+															__csrfToken: jQuery('body').attr('data-csrf-token')
+													},
+													function(data) {
+															window.parent.NeosMediaBrowserCallbacks.assetChosen(data.localAssetIdentifier);
+													}
+											);
+									}
+									e.preventDefault();
+							}
+					});
+			}
+	});
+});
