692fcd615fa7d780873fa882082176d8c90ed3c8
Markus Günther <info@unikka.de>
2021-03-18 19:46:45 +0100

    TASK: Appply compiled assets

--- Neos.Media.Browser/Resources/Public/JavaScript/media-browser.js
+++ Neos.Media.Browser/Resources/Public/JavaScript/media-browser.js
@@ -1,6 +1,5 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 	window.jQuery(function () {
 		jQuery('[data-neos-toggle="popover"]').popover();
-		jQuery('[data-neos-toggle="tooltip"]').tooltip();
 	});
 });
