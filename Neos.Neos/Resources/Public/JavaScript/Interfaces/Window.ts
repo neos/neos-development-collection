6a9b387949962566ad4f83d423e1603a826dad1b
Markus Günther <info@unikka.de>
2021-03-23 09:51:34 +0100

    TASK: Add missing deprecation notice

--- Neos.Neos/Resources/Public/JavaScript/Interfaces/Window.ts
+++ Neos.Neos/Resources/Public/JavaScript/Interfaces/Window.ts
@@ -3,6 +3,7 @@ import NeosNotification from "./NeosNotification";
 import NeosConfiguration from "./NeosConfiguration";
 
 export default interface Window {
+  // @deprecated Can be removed with Neos 8.0
   Typo3Neos: {
     I18n: NeosI18n;
     Notification: NeosNotification;
