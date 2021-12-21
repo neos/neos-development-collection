3962e9b41bcc020a866944d78158af06009d9cc4
Denny Lubitz <lubitz@vivomedia.de>
2021-12-21 21:28:59 +0100

    Add strict types and comments

--- Neos.Media.Browser/Classes/Domain/Session/BrowserStateEntityValue.php
+++ Neos.Media.Browser/Classes/Domain/Session/BrowserStateEntityValue.php
@@ -1,10 +1,18 @@
 <?php
+declare(strict_types=1);
 
 namespace Neos\Media\Browser\Domain\Session;
 
 class BrowserStateEntityValue
 {
+    /**
+     * @var string
+     */
     protected $identifier;
+
+    /**
+     * @var string
+     */
     protected $class;
 
     public function __construct(string $identifier, string $class)
