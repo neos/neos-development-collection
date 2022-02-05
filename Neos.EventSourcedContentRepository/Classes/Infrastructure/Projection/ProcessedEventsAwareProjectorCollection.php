aef385c26e433f50413d5db2180f05aa590a5730
Bernhard Schmitt <schmitt@sitegeist.de>
2022-02-05 22:00:50 +0100

    Get rid of ImmutableArrayObject

--- Neos.EventSourcedContentRepository/Classes/Infrastructure/Projection/ProcessedEventsAwareProjectorCollection.php
+++ Neos.EventSourcedContentRepository/Classes/Infrastructure/Projection/ProcessedEventsAwareProjectorCollection.php
@@ -1,45 +1,80 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Neos\EventSourcedContentRepository\Infrastructure\Projection;
 
-use Neos\EventSourcedContentRepository\Domain\ImmutableArrayObject;
+/*
+ * This file is part of the Neos.ContentRepository package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
 
-final class ProcessedEventsAwareProjectorCollection extends ImmutableArrayObject
+use Neos\Flow\Annotations as Flow;
+
+/**
+ * @implements \IteratorAggregate<int,ProcessedEventsAwareProjectorInterface>
+ * @implements \ArrayAccess<int,ProcessedEventsAwareProjectorInterface>
+ */
+#[Flow\Proxy(false)]
+final class ProcessedEventsAwareProjectorCollection implements \IteratorAggregate, \ArrayAccess
 {
-    public function __construct(Iterable $collection)
+    /**
+     * @var array<int,ProcessedEventsAwareProjectorInterface>
+     */
+    private array $processedEventsAwareProjectors;
+
+    /**
+     * @var \ArrayIterator<int,ProcessedEventsAwareProjectorInterface>
+     */
+    private \ArrayIterator $iterator;
+
+    /**
+     * @param iterable<int,ProcessedEventsAwareProjectorInterface> $collection
+     */
+    public function __construct(iterable $collection)
     {
         $processedEventsAwareProjectors = [];
         foreach ($collection as $projector) {
             if (!$projector instanceof ProcessedEventsAwareProjectorInterface) {
-                throw new \InvalidArgumentException(get_class() . ' can only consist of ' . ProcessedEventsAwareProjectorInterface::class . ' objects.', 1616950763);
+                throw new \InvalidArgumentException('ProcessedEventsAwareProjectorCollection can only consist of ' . ProcessedEventsAwareProjectorInterface::class . ' objects.', 1616950763);
             }
             $processedEventsAwareProjectors[] = $projector;
         }
-        parent::__construct($processedEventsAwareProjectors);
+        $this->processedEventsAwareProjectors = $processedEventsAwareProjectors;
+        $this->iterator = new \ArrayIterator($processedEventsAwareProjectors);
     }
 
-    /**
-     * @param mixed $key
-     * @return ProcessedEventsAwareProjectorInterface|false
-     */
-    public function offsetGet($key)
-    {
-        return parent::offsetGet($key);
-    }
 
     /**
-     * @return array|ProcessedEventsAwareProjectorInterface[]
-     */
-    public function getArrayCopy(): array
-    {
-        return parent::getArrayCopy();
-    }
-
-    /**
-     * @return \ArrayIterator|ProcessedEventsAwareProjectorInterface[]
+     * @return \ArrayIterator<int,ProcessedEventsAwareProjectorInterface>
      */
     public function getIterator(): \ArrayIterator
     {
-        return parent::getIterator();
+        return $this->iterator;
+    }
+
+    public function offsetExists(mixed $offset): bool
+    {
+        return isset($this->processedEventsAwareProjectors[$offset]);
+    }
+
+    public function offsetGet(mixed $offset): ?ProcessedEventsAwareProjectorInterface
+    {
+        return $this->processedEventsAwareProjectors[$offset] ?? null;
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        throw new \BadMethodCallException('Cannot modify immutable object of class ContentGraphs.', 1643562663);
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+        throw new \BadMethodCallException('Cannot modify immutable object of class ContentGraphs.', 1643562663);
     }
 }
