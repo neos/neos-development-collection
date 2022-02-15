d7acd5b2c031dcbacaa5e8309a53aead8cc750d2
Bernhard Schmitt <schmitt@sitegeist.de>
2022-02-16 00:58:00 +0100

    Make ContentStreamIdentifier and NodeTypeName flyweight

--- Neos.ContentRepository/Classes/Domain/ContentStream/ContentStreamIdentifier.php
+++ Neos.ContentRepository/Classes/Domain/ContentStream/ContentStreamIdentifier.php
@@ -1,6 +1,4 @@
 <?php
-declare(strict_types=1);
-namespace Neos\ContentRepository\Domain\ContentStream;
 
 /*
  * This file is part of the Neos.ContentRepository package.
@@ -12,6 +10,10 @@ namespace Neos\ContentRepository\Domain\ContentStream;
  * source code.
  */
 
+declare(strict_types=1);
+
+namespace Neos\ContentRepository\Domain\ContentStream;
+
 use Neos\Cache\CacheAwareInterface;
 use Neos\Flow\Utility\Algorithms;
 use Neos\Flow\Annotations as Flow;
@@ -19,29 +21,36 @@ use Neos\Flow\Annotations as Flow;
 /**
  * The ContentStreamIdentifier is the identifier for a Content Stream, which is
  * a central concept in the Event-Sourced CR introduced with Neos 5.0.
- *
- * @Flow\Proxy(false)
  */
+#[Flow\Proxy(false)]
 final class ContentStreamIdentifier implements \JsonSerializable, CacheAwareInterface
 {
     /**
-     * @var string
+     * @var array<string,self>
      */
-    private $value;
+    private static array $instances;
 
-    private function __construct(string $value)
+    private function __construct(
+        private string $value
+    ) {}
+
+    public static function instance(string $value): self
     {
-        $this->value = $value;
+        if (!isset(self::$instances[$value])) {
+            self::$instances[$value] = new self($value);
+        }
+
+        return self::$instances[$value];
     }
 
     public static function fromString(string $value): self
     {
-        return new static($value);
+        return self::instance($value);
     }
 
     public static function create(): self
     {
-        return new static(Algorithms::generateUUID());
+        return self::instance(Algorithms::generateUUID());
     }
 
     public function jsonSerialize(): string
@@ -61,6 +70,6 @@ final class ContentStreamIdentifier implements \JsonSerializable, CacheAwareInte
 
     public function equals(ContentStreamIdentifier $other): bool
     {
-        return $this->value === $other->value;
+        return $this === $other;
     }
 }
