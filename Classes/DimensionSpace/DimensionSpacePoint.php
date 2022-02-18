6017abc46b40b856737653107d9d1a67a57466c9
Bernhard Schmitt <schmitt@sitegeist.de>
2022-02-18 17:52:39 +0100

    Separate DimensionSpacePoint implementation from others

--- Classes/DimensionSpace/DimensionSpacePoint.php
+++ Classes/DimensionSpace/DimensionSpacePoint.php
@@ -14,9 +14,7 @@ declare(strict_types=1);
 
 namespace Neos\ContentRepository\DimensionSpace\DimensionSpace;
 
-use Neos\Eel\ProtectedContextAwareInterface;
 use Neos\Flow\Annotations as Flow;
-use Neos\Cache\CacheAwareInterface;
 use Neos\ContentRepository\DimensionSpace\Dimension;
 
 /**
@@ -26,71 +24,25 @@ use Neos\ContentRepository\DimensionSpace\Dimension;
  * Implements CacheAwareInterface because of Fusion Runtime caching and Routing
  */
 #[Flow\Proxy(false)]
-class DimensionSpacePoint implements \JsonSerializable, \Stringable, CacheAwareInterface, ProtectedContextAwareInterface
+final class DimensionSpacePoint extends AbstractDimensionSpacePoint
 {
     /**
      * @var array<string,DimensionSpacePoint>
      */
     private static array $instances = [];
 
-    protected function __construct(
-        /**
-         * @var array<string,string>
-         */
-        public readonly array $coordinates,
-        public readonly string $hash
-    ) {}
-
     /**
      * @param array<string,string> $coordinates
      */
     private static function instance(array $coordinates): self
     {
-        $hash = self::hashCoordinates($coordinates);
+        $hash = parent::hashCoordinates($coordinates);
         if (!isset(self::$instances[$hash])) {
-            self::validateCoordinates($coordinates);
+            parent::validateCoordinates($coordinates);
             self::$instances[$hash] = new self($coordinates, $hash);
         }
 
         return self::$instances[$hash];
-    }
-
-    /**
-     * @param array<string,string> $coordinates
-     */
-    final static protected function hashCoordinates(array $coordinates): string
-    {
-        $identityComponents = $coordinates;
-        ksort($identityComponents);
-
-        return md5(json_encode($identityComponents));
-    }
-
-    /**
-     * @param array<string,string> $coordinates
-     */
-    final static protected function validateCoordinates(array $coordinates): void
-    {
-        foreach ($coordinates as $dimensionName => $dimensionValue) {
-            if (!is_string($dimensionName)) {
-                throw new \InvalidArgumentException(
-                    sprintf('Dimension name "%s" is not a string', $dimensionName),
-                    1639733101
-                );
-            }
-            if ($dimensionName === '') {
-                throw new \InvalidArgumentException('Dimension name must not be empty', 1639733123);
-            }
-            if (!is_string($dimensionValue)) {
-                throw new \InvalidArgumentException(
-                    sprintf('Dimension value for %s is not a string', $dimensionName),
-                    1506076562
-                );
-            }
-            if ($dimensionValue === '') {
-                throw new \InvalidArgumentException('Dimension value must not be empty', 1506076563);
-            }
-        }
     }
 
     /**
@@ -140,92 +92,5 @@ class DimensionSpacePoint implements \JsonSerializable, \Stringable, CacheAwareI
         $variedCoordinates[(string)$dimensionIdentifier] = $value;
 
         return self::instance($variedCoordinates);
-    }
-
-    /**
-     * A variant VarA is a "Direct Variant in Dimension Dim" of another variant VarB, if VarA and VarB are sharing all dimension values except in "Dim",
-     * AND they have differing dimension values in "Dim". Thus, VarA and VarB only vary in the given "Dim".
-     * It does not say anything about how VarA and VarB relate (if it is specialization, peer or generalization).
-     */
-    final public function isDirectVariantInDimension(
-        DimensionSpacePoint $other,
-        Dimension\ContentDimensionIdentifier $contentDimensionIdentifier
-    ): bool {
-        if (!$this->hasCoordinate($contentDimensionIdentifier) || !$other->hasCoordinate($contentDimensionIdentifier)) {
-            return false;
-        }
-        if ($this->coordinates[(string)$contentDimensionIdentifier] === $other->coordinates[(string)$contentDimensionIdentifier]) {
-            return false;
-        }
-
-        $theseCoordinates = $this->coordinates;
-        $otherCoordinates = $other->coordinates;
-        unset($theseCoordinates[(string)$contentDimensionIdentifier]);
-        unset($otherCoordinates[(string)$contentDimensionIdentifier]);
-
-        return $theseCoordinates === $otherCoordinates;
-    }
-
-    final public function hasCoordinate(Dimension\ContentDimensionIdentifier $dimensionIdentifier): bool
-    {
-        return isset($this->coordinates[(string)$dimensionIdentifier]);
-    }
-
-    final public function getCoordinate(Dimension\ContentDimensionIdentifier $dimensionIdentifier): ?string
-    {
-        return $this->coordinates[(string)$dimensionIdentifier] ?? null;
-    }
-
-    /**
-     * Equals check (as opposed to === same check, which is usually the preferred variant)
-     * Compares two hashes, since the DSPs themselves might be of different classes
-     */
-    final public function equals(DimensionSpacePoint $other): bool
-    {
-        return $this->hash === $other->hash;
-    }
-
-    /**
-     * @return array<string,array<int,string>>
-     */
-    final public function toLegacyDimensionArray(): array
-    {
-        $legacyDimensions = [];
-        foreach ($this->coordinates as $dimensionName => $dimensionValue) {
-            $legacyDimensions[$dimensionName] = [$dimensionValue];
-        }
-
-        return $legacyDimensions;
-    }
-
-    final public function serializeForUri(): string
-    {
-        return base64_encode(json_encode($this->coordinates));
-    }
-
-    /**
-     * @return array<string,string>
-     */
-    final public function jsonSerialize(): array
-    {
-        return $this->coordinates;
-    }
-
-    final public function getCacheEntryIdentifier(): string
-    {
-        return $this->hash;
-    }
-
-    /**
-     * @param string $methodName
-     */
-    final public function allowsCallOfMethod($methodName): bool
-    {
-        return true;
-    }
-
-    final public function __toString(): string
-    {
-        return json_encode($this);
     }
 }
