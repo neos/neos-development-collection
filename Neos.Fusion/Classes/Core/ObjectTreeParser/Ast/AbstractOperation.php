aba1fc42e761ec5a236ba1b671ace878dced3708
Martin Ficzel <ficzel@sitegeist.de>
2022-03-19 20:30:09 +0100

    TASK: Add package comments to the fusion ast objects

--- Neos.Fusion/Classes/Core/ObjectTreeParser/Ast/AbstractOperation.php
+++ Neos.Fusion/Classes/Core/ObjectTreeParser/Ast/AbstractOperation.php
@@ -3,6 +3,16 @@ declare(strict_types=1);
 
 namespace Neos\Fusion\Core\ObjectTreeParser\Ast;
 
+/*
+ * This file is part of the Neos.Fusion package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
 use Neos\Flow\Annotations as Flow;
 
 #[Flow\Proxy(false)]
--- Neos.Fusion/Classes/Core/ObjectTreeParser/Ast/ObjectStatement.php
+++ Neos.Fusion/Classes/Core/ObjectTreeParser/Ast/ObjectStatement.php
@@ -1,6 +1,16 @@
 <?php
 declare(strict_types=1);
 
+/*
+ * This file is part of the Neos.Fusion package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
 namespace Neos\Fusion\Core\ObjectTreeParser\Ast;
 
 use Neos\Flow\Annotations as Flow;
