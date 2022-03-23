226e77d5b0928b19902e7ab06f58450c6e9eaeca
Markus Günther <info@unikka.de>
2022-03-23 18:59:18 +0100

    TASK: Remove deprecated compatibilty layer for JS API
    Removes the window. Typo3Neos API for Localisation, Notification, Configuration and the Helper.

--- Neos.Neos/Resources/Public/JavaScript/Interfaces/Window.ts
+++ Neos.Neos/Resources/Public/JavaScript/Interfaces/Window.ts
@@ -3,12 +3,6 @@ import NeosNotification from "./NeosNotification";
 import NeosConfiguration from "./NeosConfiguration";
 
 export default interface Window {
-  // @deprecated Can be removed with Neos 8.0
-  Typo3Neos: {
-    I18n: NeosI18n;
-    Notification: NeosNotification;
-    Configuration: NeosConfiguration;
-  };
   NeosCMS: {
     I18n: NeosI18n;
     Notification: NeosNotification;
