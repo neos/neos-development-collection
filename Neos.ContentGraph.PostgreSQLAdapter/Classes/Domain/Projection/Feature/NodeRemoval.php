2842566d23a1bd05a8783b9d72021661f9624ff2
Bernhard Schmitt <schmitt@sitegeist.de>
2022-03-28 16:01:58 +0200

    pacify phpstan

--- Neos.ContentGraph.PostgreSQLAdapter/Classes/Domain/Projection/Feature/NodeRemoval.php
+++ Neos.ContentGraph.PostgreSQLAdapter/Classes/Domain/Projection/Feature/NodeRemoval.php
@@ -34,8 +34,7 @@ trait NodeRemoval
      */
     public function whenNodeAggregateWasRemoved(NodeAggregateWasRemoved $event): void
     {
-        return;
-        $this->transactional(function () use ($event) {
+        /* $this->transactional(function () use ($event) {
             $nodeRecordsToBeRemoved = [];
             foreach ($event->getAffectedCoveredDimensionSpacePoints() as $dimensionSpacePoint) {
                 $nodeRecord = $this->getProjectionHypergraph()->findNodeRecordByCoverage(
@@ -47,7 +46,7 @@ trait NodeRemoval
                     throw EventCouldNotBeAppliedToContentGraph::becauseTheSourceNodeIsMissing(get_class($event));
                 }
 
-                /** @var HierarchyHyperrelationRecord $ingoingHierarchyRelation */
+                @var HierarchyHyperrelationRecord $ingoingHierarchyRelation
                 $ingoingHierarchyRelation = $this->getProjectionHypergraph()
                     ->findHierarchyHyperrelationRecordByChildNodeAnchor(
                         $event->getContentStreamIdentifier(),
@@ -79,7 +78,7 @@ trait NodeRemoval
             foreach ($nodeRecordsToBeRemoved as $nodeRecord) {
                 $nodeRecord->removeFromDatabase($this->getDatabaseConnection());
             }
-        });
+        });*/
     }
 
     /**
