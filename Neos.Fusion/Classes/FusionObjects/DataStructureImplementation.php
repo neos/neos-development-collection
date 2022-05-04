f877c0df01ee4077f520274fdfaf4ab10808f61f
Marvin Kuhn <marvin.kuhn92@gmail.com>
2022-05-04 13:11:16 +0200

    BUGFIX: Removed unused class import

--- Neos.Fusion/Classes/FusionObjects/DataStructureImplementation.php
+++ Neos.Fusion/Classes/FusionObjects/DataStructureImplementation.php
@@ -11,7 +11,6 @@ namespace Neos\Fusion\FusionObjects;
  * source code.
  */
 
-use Neos\Fusion\Core\Parser;
 use Neos\Fusion\Core\Runtime;
 use Neos\Utility\Exception\InvalidPositionException;
 use Neos\Utility\PositionalArraySorter;
