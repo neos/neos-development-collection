3340c93f1969b7ee8720284cb4e72753403b8e77
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2022-05-05 11:40:06 +0200

    TASK: move NodeHelper and around

--- Neos.Neos/Classes/Fusion/ExceptionHandlers/NodeWrappingHandler.php
+++ Neos.Neos/Classes/Fusion/ExceptionHandlers/NodeWrappingHandler.php
@@ -11,13 +11,14 @@ namespace Neos\Neos\Fusion\ExceptionHandlers;
  * source code.
  */
 
+use Neos\ContentRepository\Projection\Content\NodeInterface;
+use Neos\ContentRepository\Projection\Workspace\WorkspaceFinder;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Security\Authorization\PrivilegeManagerInterface;
 use Neos\Flow\Utility\Environment;
-use Neos\Neos\Service\ContentElementWrappingService;
-use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Fusion\Core\ExceptionHandlers\AbstractRenderingExceptionHandler;
 use Neos\Fusion\Core\ExceptionHandlers\ContextDependentHandler;
+use Neos\Neos\Service\ContentElementWrappingService;
 
 /**
  * Provides a nicely formatted html error message
@@ -45,6 +46,12 @@ class NodeWrappingHandler extends AbstractRenderingExceptionHandler
     protected $privilegeManager;
 
     /**
+     * @Flow\Inject
+     * @var WorkspaceFinder
+     */
+    protected $workspaceFinder;
+
+    /**
      * renders the exception to nice html content element to display, edit, remove, ...
      *
      * @param string $fusionPath - path causing the exception
@@ -52,7 +59,7 @@ class NodeWrappingHandler extends AbstractRenderingExceptionHandler
      * @param integer $referenceCode - might be unset
      * @return string
      */
-    protected function handle($fusionPath, \Exception $exception, $referenceCode)
+    protected function handle($fusionPath, \Exception $exception, $referenceCode): string
     {
         $handler = new ContextDependentHandler();
         $handler->setRuntime($this->runtime);
@@ -62,12 +69,22 @@ class NodeWrappingHandler extends AbstractRenderingExceptionHandler
         if (isset($currentContext['node'])) {
             /** @var NodeInterface $node */
             $node = $currentContext['node'];
+            $workspace = $this->workspaceFinder->findOneByCurrentContentStreamIdentifier(
+                $node->getContentStreamIdentifier()
+            );
             $applicationContext = $this->environment->getContext();
-            if ($applicationContext->isProduction() && $this->privilegeManager->isPrivilegeTargetGranted('Neos.Neos:Backend.GeneralAccess') && $node->getContext()->getWorkspaceName() !== 'live') {
-                $output = '<div class="neos-rendering-exception"><div class="neos-rendering-exception-title">Failed to render element' . $output . '</div></div>';
+
+            if ($applicationContext->isProduction()
+                && $this->privilegeManager->isPrivilegeTargetGranted('Neos.Neos:Backend.GeneralAccess')
+                && !is_null($workspace)
+                && !$workspace->getWorkspaceName()->isLive()
+            ) {
+                $output = '<div class="neos-rendering-exception">
+    <div class="neos-rendering-exception-title">Failed to render element' . $output . '</div>
+</div>';
             }
 
-            return $this->contentElementWrappingService->wrapContentObject($node, $output, $fusionPath);
+            return $this->contentElementWrappingService->wrapContentObject($node, $output, $fusionPath) ?: '';
         }
 
         return $output;
@@ -76,12 +93,8 @@ class NodeWrappingHandler extends AbstractRenderingExceptionHandler
     /**
      * appends the given reference code to the exception's message
      * unless it is unset
-     *
-     * @param \Exception $exception
-     * @param $referenceCode
-     * @return string
      */
-    protected function getMessage(\Exception $exception, $referenceCode)
+    protected function getMessage(\Exception $exception, int|string|null $referenceCode = null): string
     {
         if (isset($referenceCode)) {
             return sprintf('%s (%s)', $exception->getMessage(), $referenceCode);
--- Neos.Neos/Classes/Fusion/Helper/NodeHelper.php
+++ Neos.Neos/Classes/Fusion/Helper/NodeHelper.php
@@ -10,9 +10,12 @@ namespace Neos\Neos\Fusion\Helper;
  * information, please view the LICENSE file which was distributed with this
  * source code.
  */
-
+use Neos\ContentRepository\SharedModel\Node\NodePath;
+use Neos\ContentRepository\NodeAccess\NodeAccessorManager;
+use Neos\ContentRepository\SharedModel\NodeAddressFactory;
+use Neos\ContentRepository\Projection\Content\NodeInterface;
+use Neos\Flow\Annotations as Flow;
 use Neos\Eel\ProtectedContextAwareInterface;
-use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Neos\Domain\Exception;
 
 /**
@@ -21,37 +24,60 @@ use Neos\Neos\Domain\Exception;
 class NodeHelper implements ProtectedContextAwareInterface
 {
     /**
+     * @Flow\Inject
+     * @var NodeAddressFactory
+     */
+    protected $nodeAddressFactory;
+
+    /**
+     * @Flow\Inject
+     * @var NodeAccessorManager
+     */
+    protected $nodeAccessorManager;
+
+    /**
      * Check if the given node is already a collection, find collection by nodePath otherwise, throw exception
      * if no content collection could be found
      *
-     * @param NodeInterface $node
-     * @param string $nodePath
-     * @return NodeInterface
      * @throws Exception
      */
-    public function nearestContentCollection(NodeInterface $node, $nodePath)
+    public function nearestContentCollection(NodeInterface $node, string $nodePath): NodeInterface
     {
         $contentCollectionType = 'Neos.Neos:ContentCollection';
         if ($node->getNodeType()->isOfType($contentCollectionType)) {
             return $node;
         } else {
-            if ((string)$nodePath === '') {
-                throw new Exception(sprintf('No content collection of type %s could be found in the current node and no node path was provided. You might want to configure the nodePath property with a relative path to the content collection.', $contentCollectionType), 1409300545);
+            if ($nodePath === '') {
+                throw new Exception(sprintf(
+                    'No content collection of type %s could be found in the current node and no node path was provided.'
+                    . ' You might want to configure the nodePath property'
+                    . ' with a relative path to the content collection.',
+                    $contentCollectionType
+                ), 1409300545);
             }
-            $subNode = $node->getNode($nodePath);
+            $subNode = $this->findNodeByNodePath(
+                $node,
+                NodePath::fromString($nodePath)
+            );
+
             if ($subNode !== null && $subNode->getNodeType()->isOfType($contentCollectionType)) {
                 return $subNode;
             } else {
-                throw new Exception(sprintf('No content collection of type %s could be found in the current node (%s) or at the path "%s". You might want to adjust your node type configuration and create the missing child node through the "flow node:repair --node-type %s" command.', $contentCollectionType, $node->getPath(), $nodePath, (string)$node->getNodeType()), 1389352984);
+                throw new Exception(sprintf(
+                    'No content collection of type %s could be found in the current node (%s) or at the path "%s".'
+                    . ' You might want to adjust your node type configuration and create the missing child node'
+                    . ' through the "flow node:repair --node-type %s" command.',
+                    $contentCollectionType,
+                    $this->findNodePath($node),
+                    $nodePath,
+                    $node->getNodeType()
+                ), 1389352984);
             }
         }
     }
 
     /**
      * Generate a label for a node with a chaining mechanism. To be used in nodetype definitions.
-     *
-     * @param NodeInterface|null $node
-     * @return NodeLabelToken
      */
     public function labelForNode(NodeInterface $node = null): NodeLabelToken
     {
@@ -71,6 +97,73 @@ class NodeHelper implements ProtectedContextAwareInterface
         return $node->getNodeType()->isOfType($nodeType);
     }
 
+
+    public function nodeAddressToString(NodeInterface $node): string
+    {
+        return $this->nodeAddressFactory->createFromNode($node)->serializeForUri();
+    }
+
+    private function findNodeByNodePath(NodeInterface $node, NodePath $nodePath): ?NodeInterface
+    {
+        if ($nodePath->isAbsolute()) {
+            $node = $this->findRootNode($node);
+        }
+
+
+        return $this->findNodeByPath($node, $nodePath);
+    }
+
+
+
+    private function findRootNode(NodeInterface $node): NodeInterface
+    {
+        while (true) {
+            $nodeAccessor = $this->nodeAccessorManager->accessorFor(
+                $node->getContentStreamIdentifier(),
+                $node->getDimensionSpacePoint(),
+                $node->getVisibilityConstraints()
+            );
+            $parentNode = $nodeAccessor->findParentNode($node);
+            if ($parentNode === null) {
+                // there is no parent, so the root node was the node before
+                return $node;
+            } else {
+                $node = $parentNode;
+            }
+        }
+    }
+
+    private function findNodePath(NodeInterface $node): NodePath
+    {
+        $nodeAccessor = $this->nodeAccessorManager->accessorFor(
+            $node->getContentStreamIdentifier(),
+            $node->getDimensionSpacePoint(),
+            $node->getVisibilityConstraints()
+        );
+
+        return $nodeAccessor->findNodePath($node);
+    }
+
+    private function findNodeByPath(NodeInterface $node, NodePath $nodePath): ?NodeInterface
+    {
+        foreach ($nodePath->getParts() as $nodeName) {
+            $nodeAccessor = $this->nodeAccessorManager->accessorFor(
+                $node->getContentStreamIdentifier(),
+                $node->getDimensionSpacePoint(),
+                $node->getVisibilityConstraints()
+            );
+            $childNode = $nodeAccessor->findChildNodeConnectedThroughEdgeName($node, $nodeName);
+            if ($childNode === null) {
+                // we cannot find the child node, so there is no node on this path
+                return null;
+            }
+            $node = $childNode;
+        }
+
+        return $node;
+    }
+
+
     /**
      * @param string $methodName
      * @return boolean
@@ -80,3 +173,4 @@ class NodeHelper implements ProtectedContextAwareInterface
         return true;
     }
 }
+
