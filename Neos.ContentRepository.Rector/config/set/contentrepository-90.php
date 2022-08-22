ae712e87cb8f25f3b45a58e83787b0ae1c296608
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2022-08-22 16:57:08 +0200

    ToDo list for 9.0 migration with Rector (NodeInterface)

--- Neos.ContentRepository.Rector/config/set/contentrepository-90.php
+++ Neos.ContentRepository.Rector/config/set/contentrepository-90.php
@@ -18,11 +18,120 @@ return static function (RectorConfig $rectorConfig): void {
         'Neos\\ContentRepository\\Domain\\Projection\\Content\\TraversableNodeInterface' => Node::class,
     ]);
 
-    $rectorConfig->ruleWithConfiguration(MethodCallToPropertyFetchRector::class, [
-        new MethodCallToPropertyFetch(Node::class, 'getIdentifier', 'nodeAggregateIdentifier')
-    ]);
 
+    /** @var $methodCallToPropertyFetches MethodCallToPropertyFetch[] */
+    $methodCallToPropertyFetches = [];
+
+
+    /**
+     * Neos\ContentRepository\Domain\Model\NodeInterface
+     */
+    // setName
+    // getName
+    $methodCallToPropertyFetches[] = new MethodCallToPropertyFetch(Node::class, 'getName', 'nodeName');
+    // getLabel -> compatible with ES CR node (nothing to do)
+    // setProperty
+    // hasProperty -> compatible with ES CR Node (nothing to do)
+    // getProperty -> compatible with ES CR Node (nothing to do)
+    // removeProperty
+    // getProperties -> PropertyCollectionInterface
+    $methodCallToPropertyFetches[] = new MethodCallToPropertyFetch(Node::class, 'getProperties', 'properties');
+    // getPropertyNames
+    // setContentObject -> DEPRECATED / NON-FUNCTIONAL
+    // getContentObject -> DEPRECATED / NON-FUNCTIONAL
+    // unsetContentObject -> DEPRECATED / NON-FUNCTIONAL
+    // setNodeType
+    // getNodeType: NodeType
+    $methodCallToPropertyFetches[] = new MethodCallToPropertyFetch(Node::class, 'getNodeType', 'nodeType');
+    // setHidden
+    // isHidden
+    // setHiddenBeforeDateTime
+    // getHiddenBeforeDateTime
+    // setHiddenAfterDateTime
+    // getHiddenAfterDateTime
+    // getHiddenAfterDateTime
+    // setHiddenInIndex
+    // isHiddenInIndex
+    // isHiddenInIndex
+    // setAccessRoles
+    // getAccessRoles
+    // getPath
+    // getContextPath
+    // getDepth
+    // setWorkspace -> internal
+    // getWorkspace
+    // getIdentifier
+    $methodCallToPropertyFetches[] = new MethodCallToPropertyFetch(Node::class, 'getIdentifier', 'nodeAggregateIdentifier');
+    // setIndex -> internal
+    // getIndex
+    // getParent -> Node
+    // getParentPath - deprecated
+    // createNode
+    // createSingleNode -> internal
+    // createNodeFromTemplate
+    // getNode(relative path) - deprecated
+    // getPrimaryChildNode() - deprecated
+    // getChildNodes($nodeTypeFilter, $limit, $offset) - deprecated
     $rectorConfig->rule(NodeGetChildNodesRector::class);
+        // - TODO: NodeTypeFilter
+        // - TODO: Limit
+        // - TODO: Offset
+    // hasChildNodes($nodeTypeFilter) - deprecated
+    // remove()
+    // setRemoved()
+    // isRemoved()
+    // isVisible()
+    // isAccessible()
+    // hasAccessRestrictions()
+    // isNodeTypeAllowedAsChildNode()
+    // moveBefore()
+    // moveAfter()
+    // moveInto()
+    // copyBefore()
+    // copyAfter()
+    // copyInto()
+    // getNodeData()
+    // getContext()
+    // getDimensions()
+    // createVariantForContext()
+    // isAutoCreated()
+    // getOtherNodeVariants()
+
+    /**
+     * Neos\ContentRepository\Domain\Projection\Content\NodeInterface
+     */
+    // isRoot()
+    // isTethered()
+    // getContentStreamIdentifier() -> threw exception in <= Neos 8.0 - so nobody could have used this
+    // getNodeAggregateIdentifier()
+    $methodCallToPropertyFetches[] = new MethodCallToPropertyFetch(Node::class, 'getNodeAggregateIdentifier', 'nodeAggregateIdentifier');
+    // getNodeTypeName()
+    $methodCallToPropertyFetches[] = new MethodCallToPropertyFetch(Node::class, 'getNodeTypeName', 'nodeTypeName');
+    // getNodeType() ** (included/compatible in old NodeInterface)
+    // getNodeName()
+    $methodCallToPropertyFetches[] = new MethodCallToPropertyFetch(Node::class, 'getNodeName', 'nodeName');
+    // getOriginDimensionSpacePoint() -> threw exception in <= Neos 8.0 - so nobody could have used this
+    // getProperties() ** (included/compatible in old NodeInterface)
+    // getProperty() ** (included/compatible in old NodeInterface)
+    // hasProperty() ** (included/compatible in old NodeInterface)
+    // getLabel() ** (included/compatible in old NodeInterface)
+
+    /**
+     * Neos\ContentRepository\Domain\Projection\Content\TraversableNodeInterface
+     */
+    // getDimensionSpacePoint() -> threw exception in <= Neos 8.0 - so nobody could have used this
+    // findParentNode() -> TraversableNodeInterface
+    // findNodePath() -> NodePath
+    // findNamedChildNode(NodeName $nodeName): TraversableNodeInterface;
+    // findChildNodes(NodeTypeConstraints $nodeTypeConstraints = null, int $limit = null, int $offset = null): TraversableNodes;
+    // countChildNodes(NodeTypeConstraints $nodeTypeConstraints = null): int;
+    // findReferencedNodes(): TraversableNodes;
+    // findNamedReferencedNodes(PropertyName $edgeName): TraversableNodes;
+    // findReferencingNodes() -> threw exception in <= Neos 8.0 - so nobody could have used this
+    // findNamedReferencingNodes() -> threw exception in <= Neos 8.0 - so nobody could have used this
+
+
+    $rectorConfig->ruleWithConfiguration(MethodCallToPropertyFetchRector::class, $methodCallToPropertyFetches);
 
     // Should run LAST - as other rules above might create $this->contentRepositoryRegistry calls.
     $rectorConfig->rule(InjectContentRepositoryRegistryIfNeededRector::class);
