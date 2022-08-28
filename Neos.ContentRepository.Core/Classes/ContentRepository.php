e231102718405e7d27ff7e20c4d366ccfcc0e716
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2022-08-28 12:49:25 +0200

    fix loading of classes

--- Neos.ContentRepository.Core/Classes/ContentRepository.php
+++ Neos.ContentRepository.Core/Classes/ContentRepository.php
@@ -12,7 +12,7 @@
 
 declare(strict_types=1);
 
-namespace Neos\ContentRepository;
+namespace Neos\ContentRepository\Core;
 
 use Neos\ContentRepository\Core\CommandHandler\CommandBus;
 use Neos\ContentRepository\Core\CommandHandler\CommandInterface;
