d44a0e8e9dd735a2f5dc30c7e242a67c083ca44b
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-11-30 20:11:24 +0100

    TASK: Undo refactoring LazyProps
    partly reverts 61f2c47bf8ab4ca07de191de80280d21fd3d7bd4

--- Neos.Fusion/Classes/FusionObjects/Helpers/LazyProps.php
+++ Neos.Fusion/Classes/FusionObjects/Helpers/LazyProps.php
@@ -4,21 +4,62 @@ namespace Neos\Fusion\FusionObjects\Helpers;
 
 use Neos\Flow\Annotations as Flow;
 use Neos\Fusion\Core\Runtime;
-use Neos\Fusion\FusionObjects\AbstractFusionObject;
 
-/** @Flow\Proxy(false) */
-final class LazyProps extends AbstractLazyProps
+/**
+ * @Flow\Proxy(false)
+ */
+final class LazyProps implements \ArrayAccess, \Iterator, \JsonSerializable
 {
-    private array $valueCache = [];
+
+    /**
+     * @var array
+     */
+    private $valueCache = [];
+
+    /**
+     * @var string
+     */
+    private $parentPath;
+
+    /**
+     * @var Runtime
+     */
+    private $runtime;
+
+    /**
+     * Index of keys
+     *
+     * @var array
+     */
+    private $keys;
+
+    /**
+     * @var object
+     */
+    private $fusionObject;
+
+    /**
+     * @var array
+     */
+    private $effectiveContext;
 
     public function __construct(
-        private AbstractFusionObject $fusionObject,
-        private string $parentPath,
-        private Runtime $runtime,
+        object $fusionObject,
+        string $parentPath,
+        Runtime $runtime,
         array $keys,
-        private array $effectiveContext
+        array $effectiveContext
     ) {
-        parent::__construct($keys);
+        $this->fusionObject = $fusionObject;
+        $this->parentPath = $parentPath;
+        $this->runtime = $runtime;
+        $this->keys = array_flip($keys);
+        $this->effectiveContext = $effectiveContext;
+    }
+
+    public function offsetExists($path): bool
+    {
+        return array_key_exists($path, $this->keys);
     }
 
     public function offsetGet($path): mixed
@@ -32,5 +73,49 @@ final class LazyProps extends AbstractLazyProps
             }
         }
         return $this->valueCache[$path];
+    }
+
+    public function offsetSet($path, $value): void
+    {
+        throw new BadMethodCallException('Lazy props can not be set.', 1588182804);
+    }
+
+    public function offsetUnset($path): void
+    {
+        throw new BadMethodCallException('Lazy props can not be unset.', 1588182805);
+    }
+
+    public function current(): mixed
+    {
+        $path = key($this->keys);
+        if ($path === null) {
+            return null;
+        }
+        return $this->offsetGet($path);
+    }
+
+    public function next(): void
+    {
+        next($this->keys);
+    }
+
+    public function key(): mixed
+    {
+        return key($this->keys);
+    }
+
+    public function valid(): bool
+    {
+        return current($this->keys) !== false;
+    }
+
+    public function rewind(): void
+    {
+        reset($this->keys);
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return iterator_to_array($this);
     }
 }
