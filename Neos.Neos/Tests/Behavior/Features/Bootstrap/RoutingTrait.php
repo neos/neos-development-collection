d9f918149fc44cae4331e545a2ff7209661d4544
bwaidelich <b.waidelich@wwwision.de>
2023-04-03 18:48:49 +0200

    Fix more usages

--- Neos.Neos/Tests/Behavior/Features/Bootstrap/RoutingTrait.php
+++ Neos.Neos/Tests/Behavior/Features/Bootstrap/RoutingTrait.php
@@ -193,14 +193,14 @@ trait RoutingTrait
         $nodeAddress = $this->match($this->requestUrl);
         Assert::assertNotNull($nodeAddress, 'Routing result does not have "node" key - this probably means that the FrontendNodeRoutePartHandler did not properly resolve the result.');
         Assert::assertTrue($nodeAddress->isInLiveWorkspace());
-        Assert::assertSame($nodeAggregateId, (string)$nodeAddress->nodeAggregateId);
-        Assert::assertSame($contentStreamId, (string)$nodeAddress->contentStreamId);
+        Assert::assertSame($nodeAggregateId, $nodeAddress->nodeAggregateId->value);
+        Assert::assertSame($contentStreamId, $nodeAddress->contentStreamId->value);
         Assert::assertSame(
             DimensionSpacePoint::fromJsonString($dimensionSpacePoint),
             $nodeAddress->dimensionSpacePoint,
             sprintf(
                 'Dimension space point "%s" did not match the expected "%s"',
-                json_encode($nodeAddress->dimensionSpacePoint),
+                $nodeAddress->dimensionSpacePoint->toJson(),
                 $dimensionSpacePoint
             )
         );
