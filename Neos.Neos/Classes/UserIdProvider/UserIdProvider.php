9de9c7dc2f7594aacb85b085240acb4ea76ca1bc
bwaidelich <b.waidelich@wwwision.de>
2023-04-05 18:05:49 +0200

    FEATURE: Set InitiatingUserId from authenticated Neos user
    Resolves: #4173

---
+++ Neos.Neos/Classes/UserIdProvider/UserIdProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neos\Neos\UserIdProvider;
+
+use Neos\ContentRepository\Core\SharedModel\User\UserId;
+use Neos\ContentRepository\Core\SharedModel\User\UserIdProviderInterface;
+use Neos\Neos\Domain\Service\UserService;
+
+/**
+ * @api
+ */
+final class UserIdProvider implements UserIdProviderInterface
+{
+
+    public function __construct(
+        private readonly UserService $userService
+    ) {
+    }
+
+    public function getUserId(): UserId
+    {
+        $userId = $this->userService->getCurrentUserIdentifier();
+        if ($userId === null) {
+            return UserId::forSystemUser();
+        }
+        return UserId::fromString($userId->value);
+    }
+}
