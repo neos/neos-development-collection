339512685a080b4f94bca113b53ec7f1522b36ee
Sebastian Helzle <sebastian@helzle.it>
2023-06-01 13:11:39 +0200

    TASK: Adjust migration for parent collections
    Co-authored-by: Bastian Waidelich <b.waidelich@wwwision.de>

--- Neos.Media/Migrations/Mysql/Version20230401055019.php
+++ Neos.Media/Migrations/Mysql/Version20230401055019.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Neos\Flow\Persistence\Doctrine\Migrations;
 
+use Doctrine\DBAL\Platforms\MySqlPlatform;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -16,7 +17,7 @@ final class Version20230401055019 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->abortIf(!$this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on "mysql".');
+        $this->abortIf(!$this->connection->getDatabasePlatform() instanceof MySqlPlatform, 'Migration can only be executed safely on "mysql".');
 
         $this->addSql('ALTER TABLE neos_media_domain_model_assetcollection ADD parent VARCHAR(40) DEFAULT NULL');
         $this->addSql('ALTER TABLE neos_media_domain_model_assetcollection ADD CONSTRAINT FK_74C770A3D8E604F FOREIGN KEY (parent) REFERENCES neos_media_domain_model_assetcollection (persistence_object_identifier) ON DELETE SET NULL');
@@ -25,7 +26,7 @@ final class Version20230401055019 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->abortIf(!$this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on "mysql".');
+        $this->abortIf(!$this->connection->getDatabasePlatform() instanceof MySqlPlatform, 'Migration can only be executed safely on "mysql".');
 
         $this->addSql('ALTER TABLE neos_media_domain_model_assetcollection DROP FOREIGN KEY FK_74C770A3D8E604F');
         $this->addSql('DROP INDEX IDX_74C770A3D8E604F ON neos_media_domain_model_assetcollection');
--- Neos.Media/Migrations/Postgresql/Version20230401171612.php
+++ Neos.Media/Migrations/Postgresql/Version20230401171612.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Neos\Flow\Persistence\Doctrine\Migrations;
 
+use Doctrine\DBAL\Platforms\PostgreSQL100Platform;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -16,7 +17,7 @@ final class Version20230401171612 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on "postgresql".');
+        $this->abortIf(!$this->connection->getDatabasePlatform() instanceof PostgreSQL100Platform, 'Migration can only be executed safely on "postgresql".');
 
         $this->addSql('ALTER TABLE neos_media_domain_model_assetcollection ADD parent VARCHAR(40) DEFAULT NULL');
         $this->addSql('ALTER TABLE neos_media_domain_model_assetcollection ADD CONSTRAINT FK_74C770A3D8E604F FOREIGN KEY (parent) REFERENCES neos_media_domain_model_assetcollection (persistence_object_identifier) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -25,7 +26,7 @@ final class Version20230401171612 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on "postgresql".');
+        $this->abortIf(!$this->connection->getDatabasePlatform()->getName() instanceof PostgreSQL100Platform, 'Migration can only be executed safely on "postgresql".');
 
         $this->addSql('ALTER TABLE neos_media_domain_model_assetcollection DROP CONSTRAINT FK_74C770A3D8E604F');
         $this->addSql('DROP INDEX IDX_74C770A3D8E604F');
