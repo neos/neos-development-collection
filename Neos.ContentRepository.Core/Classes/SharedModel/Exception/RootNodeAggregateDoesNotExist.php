9d6393238062ec3eeb5e34291baa15acbb3c9cae
Bernhard Schmitt <schmitt@sitegeist.de>
2023-06-17 15:41:53 +0200

    Enforce type-uniqueness of root nodes

---
+++ Neos.ContentRepository.Core/Classes/SharedModel/Exception/RootNodeAggregateDoesNotExist.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neos\ContentRepository\Core\SharedModel\Exception;
+
+use Neos\ContentRepository\Core\NodeType\NodeTypeName;
+
+/**
+ * @api Userland code might have to react to this
+ */
+final class RootNodeAggregateDoesNotExist extends \DomainException
+{
+    public static function butWasExpectedTo(NodeTypeName $nodeTypeName): self
+    {
+        return new self(
+            'No root node aggregate could be found for node type '. $nodeTypeName->value,
+            1687008819
+        );
+    }
+}
---
+++ Neos.ContentRepository.Core/Classes/SharedModel/Exception/RootNodeAggregateTypeIsAlreadyOccupied.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neos\ContentRepository\Core\SharedModel\Exception;
+
+use Neos\ContentRepository\Core\NodeType\NodeTypeName;
+
+/**
+ * @api Userland code might have to react to this
+ */
+final class RootNodeAggregateTypeIsAlreadyOccupied extends \DomainException
+{
+    public static function butWasExpectedNotTo(NodeTypeName $nodeTypeName): self
+    {
+        return new self(
+            'Root node type  '. $nodeTypeName->value . ' is already occupied',
+            1687009058
+        );
+    }
+}
