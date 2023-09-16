neos/neos-development-collection

commit 266fc1a34bc3aa988f8e2ab040dda5f77164499c

REFACTOR: use NodeType->name; make NodeType final

Neos.ContentRepository.Core/Classes/NodeType/NodeType.php
@@ -35,12 +35,12 @@ use Neos\ContentRepository\Core\SharedModel\Exception\InvalidNodeTypePostprocess
  *
  * @api
  */
-class NodeType
+final class NodeType
 {
     /**
      * Name of this node type. Example: "ContentRepository:Folder"
      */
-    protected string $name;
+    public readonly NodeTypeName $name;
 
     /**
      * Configuration for this node type, can be an arbitrarily nested array. Does not include inherited configuration.
@@ -59,7 +59,7 @@ class NodeType
     /**
      * Is this node type marked abstract
      */
-    protected bool $abstract = false;
+    public bool $abstract = false;
 
     /**
      * Is this node type marked final
@@ -86,13 +86,13 @@ class NodeType
     /**
      * Constructs this node type
      *
-     * @param string $name Name of the node type
+     * @param NodeTypeName $name Name of the node type
      * @param array<string,mixed> $declaredSuperTypes Parent types of this node type
      * @param array<string,mixed> $configuration the configuration for this node type which is defined in the schema
      * @throws \InvalidArgumentException
      */
     public function __construct(
-        string $name,
+        NodeTypeName $name,
         array $declaredSuperTypes,
         array $configuration,
         private readonly NodeTypeManager $nodeTypeManager,
@@ -225,11 +225,11 @@ class NodeType
 
     /**
      * Returns the name of this node type
-     * @api
+     * @deprecated use "name" property directly
      */
     public function getName(): string
     {
-        return $this->name;
+        return $this->name->getValue();
     }
 
     /**
@@ -292,7 +292,7 @@ class NodeType
      */
     public function isOfType(string $nodeType): bool
     {
-        if ($nodeType === $this->name) {
+        if ($nodeType === $this->name->getValue()) {
             return true;
         }
         if (array_key_exists($nodeType, $this->declaredSuperTypes) && $this->declaredSuperTypes[$nodeType] === null) {
@@ -613,11 +613,17 @@ class NodeType
             return true;
         }
 
-        if (array_key_exists($nodeType->getName(), $constraints) && $constraints[$nodeType->getName()] === true) {
+        if (
+            array_key_exists($nodeType->name->getValue(), $constraints)
+            && $constraints[$nodeType->name->getValue()] === true
+        ) {
             return true;
         }
 
-        if (array_key_exists($nodeType->getName(), $constraints) && $constraints[$nodeType->getName()] === false) {
+        if (
+            array_key_exists($nodeType->name->getValue(), $constraints)
+            && $constraints[$nodeType->name->getValue()] === false
+        ) {
             return false;
         }
 
@@ -708,11 +714,11 @@ class NodeType
     }
 
     /**
-     * Alias for getName().
+     * Alias for name.
      * @api
      */
     public function __toString(): string
     {
-        return $this->getName();
+        return $this->name->getValue();
     }
 }

Neos.ContentRepository.Core/Classes/Feature/Common/ConstraintChecks.php
@@ -110,7 +110,7 @@ trait ConstraintChecks
     protected function requireNodeTypeToNotBeAbstract(NodeType $nodeType): void
     {
         if ($nodeType->isAbstract()) {
-            throw NodeTypeIsAbstract::butWasNotSupposedToBe(NodeTypeName::fromString($nodeType->getName()));
+            throw NodeTypeIsAbstract::butWasNotSupposedToBe($nodeType->name);
         }
     }
 
@@ -133,7 +133,7 @@ trait ConstraintChecks
     {
         if ($nodeType->isOfType(NodeTypeName::ROOT_NODE_TYPE_NAME)) {
             throw new NodeTypeIsOfTypeRoot(
-                'Node type "' . $nodeType->getName() . '" is of type root.',
+                'Node type "' . $nodeType->name . '" is of type root.',
                 1541765806
             );
         }
@@ -159,7 +159,7 @@ trait ConstraintChecks
         foreach ($nodeType->getAutoCreatedChildNodes() as $tetheredChildNodeType) {
             if ($tetheredChildNodeType->isOfType(NodeTypeName::ROOT_NODE_TYPE_NAME)) {
                 throw new NodeTypeIsOfTypeRoot(
-                    'Node type "' . $nodeType->getName() . '" for tethered descendant is of type root.',
+                    'Node type "' . $nodeType->name . '" for tethered descendant is of type root.',
                     1541767062
                 );
             }
@@ -276,17 +276,17 @@ trait ConstraintChecks
         if (!$parentsNodeType->allowsChildNodeType($nodeType)) {
             throw new NodeConstraintException(
                 'Node type "' . $nodeType . '" is not allowed for child nodes of type '
-                    . $parentsNodeType->getName()
+                    . $parentsNodeType->name
             );
         }
         if (
             $nodeName
             && $parentsNodeType->hasAutoCreatedChildNode($nodeName)
-            && $parentsNodeType->getTypeOfAutoCreatedChildNode($nodeName)?->getName() !== $nodeType->getName()
+            && !$parentsNodeType->getTypeOfAutoCreatedChildNode($nodeName)?->name->equals($nodeType->name)
         ) {
             throw new NodeConstraintException(
                 'Node type "' . $nodeType . '" does not match configured "'
-                    . $parentsNodeType->getTypeOfAutoCreatedChildNode($nodeName)?->getName()
+                    . $parentsNodeType->getTypeOfAutoCreatedChildNode($nodeName)?->name
                     . '" for auto created child nodes for parent type "' . $parentsNodeType
                     . '" with name "' . $nodeName . '"'
             );
@@ -305,7 +305,7 @@ trait ConstraintChecks
         if (
             $nodeName
             && $parentsNodeType->hasAutoCreatedChildNode($nodeName)
-            && $parentsNodeType->getTypeOfAutoCreatedChildNode($nodeName)?->getName() !== $nodeType->getName()
+            && !$parentsNodeType->getTypeOfAutoCreatedChildNode($nodeName)?->name->equals($nodeType->name)
         ) {
             return false;
         }
@@ -329,7 +329,7 @@ trait ConstraintChecks
         ) {
             throw new NodeConstraintException(
                 'Node type "' . $nodeType . '" is not allowed below tethered child nodes "' . $parentNodeName
-                    . '" of nodes of type "' . $grandParentsNodeType->getName() . '"',
+                    . '" of nodes of type "' . $grandParentsNodeType->name . '"',
                 1520011791
             );
         }

Neos.ContentRepository.Core/Classes/Feature/Common/TetheredNodeInternals.php
@@ -81,7 +81,7 @@ trait TetheredNodeInternals
                 new NodeAggregateWithNodeWasCreated(
                     $parentNode->subgraphIdentity->contentStreamIdentifier,
                     $tetheredNodeAggregateIdentifier ?: NodeAggregateIdentifier::create(),
-                    NodeTypeName::fromString($expectedTetheredNodeType->getName()),
+                    $expectedTetheredNodeType->name,
                     $parentNode->originDimensionSpacePoint,
                     $parentNodeAggregate->getCoverageByOccupant($parentNode->originDimensionSpacePoint),
                     $parentNode->nodeAggregateIdentifier,

Neos.ContentRepository.Core/Classes/Feature/NodeAggregateCommandHandler.php
@@ -217,7 +217,7 @@ final class NodeAggregateCommandHandler implements CommandHandlerInterface
             if (
                 $nodeAggregate->nodeName
                 && $parentsNodeType->hasAutoCreatedChildNode($nodeAggregate->nodeName)
-                && $parentsNodeType->getTypeOfAutoCreatedChildNode($nodeAggregate->nodeName)?->getName()
+                && $parentsNodeType->getTypeOfAutoCreatedChildNode($nodeAggregate->nodeName)?->name
                     !== (string)$command->newNodeTypeName
             ) {
                 throw new NodeConstraintException(

Neos.ContentRepository.Core/Classes/Feature/NodeCreation/NodeCreation.php
@@ -300,6 +300,7 @@ trait NodeCreation
     ): Events {
         $events = [];
         foreach ($nodeType->getAutoCreatedChildNodes() as $rawNodeName => $childNodeType) {
+            assert($childNodeType instanceof NodeType);
             $nodeName = NodeName::fromString($rawNodeName);
             $childNodePath = $nodePath
                 ? $nodePath->appendPathSegment($nodeName)
@@ -312,7 +313,7 @@ trait NodeCreation
             $events[] = $this->createTetheredWithNode(
                 $command,
                 $childNodeAggregateIdentifier,
-                NodeTypeName::fromString($childNodeType->getName()),
+                $childNodeType->name,
                 $coveredDimensionSpacePoints,
                 $parentNodeAggregateIdentifier,
                 $nodeName,

Neos.ContentRepository.Core/Classes/Infrastructure/Property/PropertyConverter.php
@@ -51,7 +51,7 @@ final class PropertyConverter
                 $serializedPropertyValues[$propertyName] = $this->serializePropertyValue(
                     $nodeType->getPropertyType($propertyName),
                     PropertyName::fromString($propertyName),
-                    NodeTypeName::fromString($nodeType->getName()),
+                    $nodeType->name,
                     $propertyValue
                 );
             }
@@ -109,7 +109,7 @@ final class PropertyConverter
             $serializedPropertyValues[$propertyName] = $this->serializePropertyValue(
                 $declaredType,
                 PropertyName::fromString($propertyName),
-                NodeTypeName::fromString($nodeType->getName()),
+                $nodeType->name,
                 $propertyValue
             );
         }

Neos.ContentRepository.Core/Classes/NodeType/NodeTypeManager.php
@@ -243,7 +243,13 @@ class NodeTypeManager
             unset($nodeTypeConfiguration['properties']);
         }
 
-        $nodeType = new NodeType($nodeTypeName, $superTypes, $nodeTypeConfiguration, $this, $this->objectManager);
+        $nodeType = new NodeType(
+            NodeTypeName::fromString($nodeTypeName),
+            $superTypes,
+            $nodeTypeConfiguration,
+            $this,
+            $this->objectManager
+        );
 
         $this->cachedNodeTypes[$nodeTypeName] = $nodeType;
         return $nodeType;
@@ -297,7 +303,7 @@ class NodeTypeManager
 
         $superType = $this->loadNodeType($superTypeName, $completeNodeTypeConfiguration);
         if ($superType->isFinal() === true) {
-            throw new NodeTypeIsFinalException($superType->getName(), 1444944148);
+            throw new NodeTypeIsFinalException($superType->name->getValue(), 1444944148);
         }
 
         return $superType;

Neos.ContentRepository.Core/Classes/Projection/ContentGraph/NodeTypeConstraintsWithSubNodeTypes.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 
 namespace Neos\ContentRepository\Core\Projection\ContentGraph;
 
+use Neos\ContentRepository\Core\NodeType\NodeType;
 use Neos\ContentRepository\Core\NodeType\NodeTypeManager;
 use Neos\ContentRepository\Core\NodeType\NodeTypeName;
 use Neos\ContentRepository\Core\NodeType\NodeTypeNames;
@@ -106,14 +107,15 @@ final class NodeTypeConstraintsWithSubNodeTypes
     ): NodeTypeNames {
         $processedNodeTypeNames = [];
         foreach ($nodeTypeNames as $nodeTypeName) {
-            $processedNodeTypeNames[$nodeTypeName->getValue()] = $nodeTypeName->getValue();
+            $processedNodeTypeNames[$nodeTypeName->getValue()] = $nodeTypeName;
             $subNodeTypes = $nodeTypeManager->getSubNodeTypes($nodeTypeName->getValue(), true);
             foreach ($subNodeTypes as $subNodeType) {
-                $processedNodeTypeNames[$subNodeType->getName()] = $subNodeType->getName();
+                assert($subNodeType instanceof NodeType);
+                $processedNodeTypeNames[$subNodeType->name->getValue()] = $subNodeType->name;
             }
         }
 
-        return NodeTypeNames::fromStringArray($processedNodeTypeNames);
+        return NodeTypeNames::fromArray($processedNodeTypeNames);
     }
 
     public function matches(NodeTypeName $nodeTypeName): bool

commit 5acb733e73a79f917e9e2dc713ee31d554a81996

TASK: Remove accidental `public` declaration of NodeType::$abstract
regression from 266fc1a34bc3aa988f8e2ab040dda5f77164499c

Neos.ContentRepository.Core/Classes/NodeType/NodeType.php
@@ -58,7 +58,7 @@ class NodeType
     /**
      * Is this node type marked abstract
      */
-    public bool $abstract = false;
+    protected bool $abstract = false;
 
     /**
      * Is this node type marked final