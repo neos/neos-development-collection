neos/neos-development-collection

commit 0f24c721c8a303e39542c69b1eae0056acd16998

TASK: Avoid deprecated FusionService::createRuntime

Neos.Neos/Classes/View/FusionExceptionView.php
@@ -23,17 +23,20 @@ use Neos\ContentRepositoryRegistry\ContentRepositoryRegistry;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Core\Bootstrap;
 use Neos\Flow\Http\RequestHandler as HttpRequestHandler;
-use Neos\Flow\Mvc\ActionResponse;
-use Neos\Flow\Mvc\View\AbstractView;
-use Neos\Fusion\Exception\RuntimeException;
-use Neos\Neos\Domain\Service\FusionService;
-use Neos\Fusion\Core\Runtime as FusionRuntime;
-use Neos\Flow\Security\Context as SecurityContext;
-use Neos\Flow\ObjectManagement\ObjectManagerInterface;
 use Neos\Flow\Mvc\ActionRequest;
-use Neos\Flow\Mvc\Routing\UriBuilder;
-use Neos\Flow\Mvc\Controller\ControllerContext;
+use Neos\Flow\Mvc\ActionResponse;
 use Neos\Flow\Mvc\Controller\Arguments;
+use Neos\Flow\Mvc\Controller\ControllerContext;
+use Neos\Flow\Mvc\Routing\UriBuilder;
+use Neos\Flow\Mvc\View\AbstractView;
+use Neos\Flow\ObjectManagement\ObjectManagerInterface;
+use Neos\Flow\Security\Context as SecurityContext;
+use Neos\Fusion\Core\FusionGlobals;
+use Neos\Fusion\Core\Runtime as FusionRuntime;
+use Neos\Fusion\Core\RuntimeFactory;
+use Neos\Fusion\Exception\RuntimeException;
+use Neos\Neos\Domain\Repository\SiteRepository;
+use Neos\Neos\Domain\Service\FusionService;
 use Neos\Neos\Domain\Service\SiteNodeUtility;
 use Neos\Neos\FrontendRouting\SiteDetection\SiteDetectionResult;
 
@@ -71,6 +74,12 @@ class FusionExceptionView extends AbstractView
      * @var FusionRuntime
      */
     protected $fusionRuntime;
+
+    #[Flow\Inject]
+    protected RuntimeFactory $runtimeFactory;
+
+    #[Flow\Inject]
+    protected SiteRepository $siteRepository;
 
     #[Flow\Inject]
     protected SiteNodeUtility $siteNodeUtility;
@@ -185,7 +194,18 @@ class FusionExceptionView extends AbstractView
         ControllerContext $controllerContext
     ): FusionRuntime {
         if ($this->fusionRuntime === null) {
-            $this->fusionRuntime = $this->fusionService->createRuntime($currentSiteNode, $controllerContext);
+            $site = $this->siteRepository->findSiteBySiteNode($currentSiteNode);
+
+            $fusionConfiguration = $this->fusionService->createFusionConfigurationFromSite($site);
+
+            $fusionGlobals = FusionGlobals::fromArray([
+                'request' => $this->controllerContext->getRequest()
+            ]);
+            $this->fusionRuntime = $this->runtimeFactory->createFromConfiguration(
+                $fusionConfiguration,
+                $fusionGlobals
+            );
+            $this->fusionRuntime->setControllerContext($this->controllerContext);
 
             if (isset($this->options['enableContentCache']) && $this->options['enableContentCache'] !== null) {
                 $this->fusionRuntime->setEnableContentCache($this->options['enableContentCache']);

Neos.Neos/Classes/Domain/Repository/SiteRepository.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 
 namespace Neos\Neos\Domain\Repository;
 
+use Neos\ContentRepository\Core\Projection\ContentGraph\Node;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Persistence\QueryInterface;
 use Neos\Flow\Persistence\QueryResultInterface;
@@ -92,6 +93,15 @@ class SiteRepository extends Repository
             ->getFirst();
 
         return $site;
+    }
+
+    public function findSiteBySiteNode(Node $siteNode): Site
+    {
+        if ($siteNode->nodeName === null) {
+            throw new \Neos\Neos\Domain\Exception(sprintf('Site node "%s" is unnamed', $siteNode->nodeAggregateId->value), 1681286146);
+        }
+        return $this->findOneByNodeName(SiteNodeName::fromNodeName($siteNode->nodeName))
+            ?? throw new \Neos\Neos\Domain\Exception(sprintf('No site found for nodeNodeName "%s"', $siteNode->nodeName->value), 1677245517);
     }
 
     /**

Neos.Neos/Classes/Domain/Service/FusionService.php
@@ -153,12 +153,12 @@ class FusionService
         Node $currentSiteNode,
         ControllerContext $controllerContext
     ) {
-        $defaultContextVariables = FusionGlobals::fromArray(
+        $fusionGlobals = FusionGlobals::fromArray(
             ['request' => $controllerContext->getRequest()]
         );
         $runtime = $this->runtimeFactory->createFromConfiguration(
             $this->createFusionConfigurationFromSite($this->findSiteBySiteNode($currentSiteNode)),
-            $defaultContextVariables
+            $fusionGlobals
         );
         $runtime->setControllerContext($controllerContext);
         return $runtime;

Neos.Neos/Classes/View/FusionView.php
@@ -20,8 +20,11 @@ use Neos\ContentRepositoryRegistry\ContentRepositoryRegistry;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Mvc\View\AbstractView;
 use Neos\Flow\Security\Context;
+use Neos\Fusion\Core\FusionGlobals;
 use Neos\Fusion\Core\Runtime;
+use Neos\Fusion\Core\RuntimeFactory;
 use Neos\Fusion\Exception\RuntimeException;
+use Neos\Neos\Domain\Repository\SiteRepository;
 use Neos\Neos\Domain\Service\FusionService;
 use Neos\Neos\Domain\Service\SiteNodeUtility;
 use Neos\Neos\Exception;
@@ -39,6 +42,12 @@ class FusionView extends AbstractView
      * @var SiteNodeUtility
      */
     protected $siteNodeUtility;
+
+    #[Flow\Inject]
+    protected RuntimeFactory $runtimeFactory;
+
+    #[Flow\Inject]
+    protected SiteRepository $siteRepository;
 
     /**
      * @Flow\Inject
@@ -217,7 +226,17 @@ class FusionView extends AbstractView
     protected function getFusionRuntime(Node $currentSiteNode)
     {
         if ($this->fusionRuntime === null) {
-            $this->fusionRuntime = $this->fusionService->createRuntime($currentSiteNode, $this->controllerContext);
+            $site = $this->siteRepository->findSiteBySiteNode($currentSiteNode);
+            $fusionConfiguration = $this->fusionService->createFusionConfigurationFromSite($site);
+
+            $fusionGlobals = FusionGlobals::fromArray([
+                'request' => $this->controllerContext->getRequest()
+            ]);
+            $this->fusionRuntime = $this->runtimeFactory->createFromConfiguration(
+                $fusionConfiguration,
+                $fusionGlobals
+            );
+            $this->fusionRuntime->setControllerContext($this->controllerContext);
 
             if (isset($this->options['enableContentCache']) && $this->options['enableContentCache'] !== null) {
                 $this->fusionRuntime->setEnableContentCache($this->options['enableContentCache']);

Neos.Neos/Tests/Unit/View/FusionViewTest.php
@@ -82,7 +82,7 @@ class FusionViewTest extends UnitTestCase
         $this->mockSecurityContext = $this->getMockBuilder(Context::class)->disableOriginalConstructor()->getMock();
 
         $mockFusionService = $this->createMock(FusionService::class);
-        $mockFusionService->expects(self::any())->method('createRuntime')->will(self::returnValue($this->mockRuntime));
+        //  $mockFusionService->expects(self::any())->method('createRuntime')->will(self::returnValue($this->mockRuntime));
 
         $this->mockView = $this->getAccessibleMock(FusionView::class, ['getClosestDocumentNode']);
         $this->mockView->expects(self::any())->method('getClosestDocumentNode')->will(self::returnValue($this->mockContextualizedNode));
@@ -152,7 +152,7 @@ class FusionViewTest extends UnitTestCase
         $mockRuntime->expects(self::any())->method('getControllerContext')->will(self::returnValue($mockControllerContext));
 
         $mockFusionService = $this->createMock(FusionService::class);
-        $mockFusionService->expects(self::any())->method('createRuntime')->will(self::returnValue($mockRuntime));
+        // $mockFusionService->expects(self::any())->method('createRuntime')->will(self::returnValue($mockRuntime));
 
         $mockSecurityContext = $this->getMockBuilder(Context::class)->disableOriginalConstructor()->getMock();
 

commit 56f5174cfa8a4221c6a90948e539213e23e90ffc

BUGFIX: FusionExceptionView must use mocked controller context
0f24c721c8a303e39542c69b1eae0056acd16998 (#4425) introduced a regression where we accidentally used `$this->controllerContext` instead of `$controllerContext`.

For $some reason we have to use the mocked controller context, as otherwise the `${request.format}` will be empty "" in fusion. And this causes trouble in the `/root` matcher.

This fix uses the mocked controller context again.

Neos.Neos/Classes/View/FusionExceptionView.php
@@ -204,14 +204,14 @@ class FusionExceptionView extends AbstractView
             $fusionConfiguration = $this->fusionService->createFusionConfigurationFromSite($site);
 
             $fusionGlobals = FusionGlobals::fromArray([
-                'request' => $this->controllerContext->getRequest(),
+                'request' => $controllerContext->getRequest(),
                 'renderingModeName' => RenderingMode::FRONTEND
             ]);
             $this->fusionRuntime = $this->runtimeFactory->createFromConfiguration(
                 $fusionConfiguration,
                 $fusionGlobals
             );
-            $this->fusionRuntime->setControllerContext($this->controllerContext);
+            $this->fusionRuntime->setControllerContext($controllerContext);
 
             if (isset($this->options['enableContentCache']) && $this->options['enableContentCache'] !== null) {
                 $this->fusionRuntime->setEnableContentCache($this->options['enableContentCache']);