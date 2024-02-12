db64f78be04416748fefdb03ca80ac5fd79fc941
Bastian Waidelich <b.waidelich@wwwision.de>
2024-02-12 18:24:18 +0100

    Fix collation in `ContentSubgraph::searchPropertyValueStatement()`
    as a follow-up to #4729

--- Neos.ContentGraph.DoctrineDbalAdapter/src/Domain/Repository/ContentSubgraph.php
+++ Neos.ContentGraph.DoctrineDbalAdapter/src/Domain/Repository/ContentSubgraph.php
@@ -613,9 +613,9 @@ final class ContentSubgraph implements ContentSubgraphInterface
         $paramName = $this->createUniqueParameterName();
         $queryBuilder->setParameter($paramName, $value);
         if ($caseSensitive) {
-            return 'JSON_SEARCH(' . $nodeTableAlias . '.properties COLLATE utf8mb3_bin, \'one\', :' . $paramName . ' COLLATE utf8mb4_bin, NULL, \'$.' . $escapedPropertyName . '.value\') IS NOT NULL';
+            return 'JSON_SEARCH(' . $nodeTableAlias . '.properties COLLATE utf8mb4_bin, \'one\', :' . $paramName . ' COLLATE utf8mb4_bin, NULL, \'$.' . $escapedPropertyName . '.value\') IS NOT NULL';
         }
-        return 'JSON_SEARCH(' . $nodeTableAlias . '.properties COLLATE utf8mb3_unicode_ci, \'one\', :' . $paramName . ' COLLATE utf8mb4_unicode_ci, NULL, \'$.' . $escapedPropertyName . '.value\') IS NOT NULL';
+        return 'JSON_SEARCH(' . $nodeTableAlias . '.properties, \'one\', :' . $paramName . ', NULL, \'$.' . $escapedPropertyName . '.value\') IS NOT NULL';
     }
 
     private function buildChildNodesQuery(NodeAggregateId $parentNodeAggregateId, FindChildNodesFilter|CountChildNodesFilter $filter): QueryBuilder
