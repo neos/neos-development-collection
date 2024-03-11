fe98c8c7149321edf210c93b89a4c7503a774590
Marc Henry Schultz <85400359+mhsdesign@users.noreply.github.com>
2024-03-11 16:11:21 +0100

    TASK: Simplify comment

--- Neos.Fusion/Classes/Core/ObjectTreeParser/Exception/ParserException.php
+++ Neos.Fusion/Classes/Core/ObjectTreeParser/Exception/ParserException.php
@@ -18,7 +18,7 @@ use Neos\Fusion\Exception;
 
 /**
  * 'Fluent' exception for the Fusion Parser.
- * @internal although ${exception.headingMessagePart} ${exception.asciiPreviewMessagePart} and ${exception.helperMessagePart} are used outside for custom error display
+ * @internal
  */
 class ParserException extends Exception
 {
