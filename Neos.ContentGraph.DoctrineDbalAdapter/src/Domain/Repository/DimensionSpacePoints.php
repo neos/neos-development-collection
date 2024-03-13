0c18558ffc9069c7f95d1812b1f2286c729a6c17
Christian Müller <christian@flownative.com>
2024-03-13 14:47:49 +0100

    Make stan happy

--- Neos.ContentGraph.DoctrineDbalAdapter/src/Domain/Repository/DimensionSpacePoints.php
+++ Neos.ContentGraph.DoctrineDbalAdapter/src/Domain/Repository/DimensionSpacePoints.php
@@ -44,7 +44,6 @@ final class DimensionSpacePoints
 
         $this->dimensionspacePointsRuntimeCache[$dimensionSpacePoint->hash] = $dimensionSpacePoint->toJson();
         $this->writeDimensionSpacePoint($dimensionSpacePoint->hash, $dimensionSpacePoint->toJson());
-
     }
 
     /**
