b418924dea957076da84fd893c022a55ec59ae9b
Bastian Waidelich <b.waidelich@wwwision.de>
2024-03-18 18:26:51 +0100

    Remove PHPCS and add rules to PHP-CS-Fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -12,6 +12,7 @@ $finder = (new PhpCsFixer\Finder())
 
 return (new PhpCsFixer\Config())
     ->setRules([
+        '@PSR12' => true,
         'no_unused_imports' => true,
         'ordered_imports' => [
             'sort_algorithm' => 'alpha',
