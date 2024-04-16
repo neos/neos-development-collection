e94c1614914a232ad7fdf62ee783bc3e8f0a96bf
Bernhard Schmitt <schmitt@sitegeist.de>
2024-04-16 18:30:05 +0200

    make SubtreeTags private again

--- Neos.ContentRepository.Core/Classes/Projection/ContentGraph/NodeTags.php
+++ Neos.ContentRepository.Core/Classes/Projection/ContentGraph/NodeTags.php
@@ -33,8 +33,8 @@ use Traversable;
 final readonly class NodeTags implements \IteratorAggregate, \Countable, \JsonSerializable
 {
     private function __construct(
-        public SubtreeTags $tags,
-        public SubtreeTags $inheritedTags,
+        private SubtreeTags $tags,
+        private SubtreeTags $inheritedTags,
     ) {
     }
 
