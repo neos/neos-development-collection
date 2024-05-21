b6787ba43870109d9f794211059bcd19f89f5fad
Denny Lubitz <lubitz@vivomedia.de>
2024-05-21 08:56:13 +0200

    BUGFIX Workspace aware NodeCacheEntryIdentifier

--- Neos.Neos/Classes/Domain/Model/NodeCacheEntryIdentifier.php
+++ Neos.Neos/Classes/Domain/Model/NodeCacheEntryIdentifier.php
@@ -32,7 +32,7 @@ final class NodeCacheEntryIdentifier implements CacheAwareInterface
 
     public static function fromNode(Node $node): self
     {
-        return new self('Node_' . $node->subgraphIdentity->contentStreamId->value
+        return new self('Node_' . $node->workspaceName->value
             . '_' . $node->dimensionSpacePoint->hash
             . '_' .  $node->aggregateId->value);
     }
