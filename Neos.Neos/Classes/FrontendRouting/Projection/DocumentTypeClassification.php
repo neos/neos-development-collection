07c3cd0cb8d722d8af32684721a10e6c3d6da59a
Bernhard Schmitt <schmitt@sitegeist.de>
2024-07-30 17:16:47 +0200

    5077 - Flag DocumentTypeClassification as internal and remove obsolete backing values

--- Neos.Neos/Classes/FrontendRouting/Projection/DocumentTypeClassification.php
+++ Neos.Neos/Classes/FrontendRouting/Projection/DocumentTypeClassification.php
@@ -18,32 +18,35 @@ use Neos\ContentRepository\Core\NodeType\NodeTypeManager;
 use Neos\ContentRepository\Core\NodeType\NodeTypeName;
 use Neos\Neos\Domain\Service\NodeTypeNameFactory;
 
-enum DocumentTypeClassification: string
+/**
+ * @internal is subject to change and may be removed / refactored beyond recognition at any time
+ */
+enum DocumentTypeClassification
 {
     /**
      * Satisfied if a node type is an actual document, meaning explicitly no shortcut or site
      */
-    case CLASSIFICATION_DOCUMENT = 'document';
+    case CLASSIFICATION_DOCUMENT;
 
     /**
      * Satisfied if a node type is a shortcut
      */
-    case CLASSIFICATION_SHORTCUT = 'shortcut';
+    case CLASSIFICATION_SHORTCUT;
 
     /**
      * Satisfied if a node type is a site
      */
-    case CLASSIFICATION_SITE = 'site';
+    case CLASSIFICATION_SITE;
 
     /**
      * Satisfied if a node type is neither of the above
      */
-    case CLASSIFICATION_NONE = 'none';
+    case CLASSIFICATION_NONE;
 
     /**
      * Satisfied if a node type does no longer exist and we can't be certain
      */
-    case CLASSIFICATION_UNKNOWN = 'unknown';
+    case CLASSIFICATION_UNKNOWN;
 
     public static function forNodeType(NodeTypeName $nodeTypeName, NodeTypeManager $nodeTypeManager): self
     {
