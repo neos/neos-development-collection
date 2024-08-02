f9c0b7ae997bf635db64aac6c7277f2c83785be3
Bastian Waidelich <b.waidelich@wwwision.de>
2024-08-02 11:46:37 +0200

    FEATURE: Expose content repository ids from registry
    Adds a `getContentRepositoryIds()` getter to the `ContentRepositoryRegistry` that allows to iterate over all configured content repositories.
    
    This will be useful for enforcing ACL, cross-CR-linking, etc..
    
    Related: #4726

--- Neos.ContentRepositoryRegistry/Classes/ContentRepositoryRegistry.php
+++ Neos.ContentRepositoryRegistry/Classes/ContentRepositoryRegistry.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Neos\ContentRepositoryRegistry;
@@ -17,6 +18,7 @@ use Neos\ContentRepository\Core\Projection\ContentGraph\Node;
 use Neos\ContentRepository\Core\Projection\ProjectionCatchUpTriggerInterface;
 use Neos\ContentRepository\Core\Projection\ProjectionFactoryInterface;
 use Neos\ContentRepository\Core\SharedModel\ContentRepository\ContentRepositoryId;
+use Neos\ContentRepository\Core\SharedModel\ContentRepository\ContentRepositoryIds;
 use Neos\ContentRepository\Core\SharedModel\User\UserIdProviderInterface;
 use Neos\ContentRepositoryRegistry\Exception\ContentRepositoryNotFoundException;
 use Neos\ContentRepositoryRegistry\Exception\InvalidConfigurationException;
@@ -81,6 +83,11 @@ final class ContentRepositoryRegistry
         return $this->getFactory($contentRepositoryId)->getOrBuild();
     }
 
+    public function getContentRepositoryIds(): ContentRepositoryIds
+    {
+        return ContentRepositoryIds::fromArray(array_keys($this->settings['contentRepositories'] ?? []));
+    }
+
     /**
      * @internal for test cases only
      */
@@ -130,7 +137,8 @@ final class ContentRepositoryRegistry
     /**
      * @throws ContentRepositoryNotFoundException | InvalidConfigurationException
      */
-    private function buildFactory(ContentRepositoryId $contentRepositoryId): ContentRepositoryFactory {
+    private function buildFactory(ContentRepositoryId $contentRepositoryId): ContentRepositoryFactory
+    {
         if (!is_array($this->settings['contentRepositories'] ?? null)) {
             throw InvalidConfigurationException::fromMessage('No Content Repositories are configured');
         }
@@ -202,7 +210,6 @@ final class ContentRepositoryRegistry
             $options['contentDimensions'] = Arrays::arrayMergeRecursiveOverrule($contentRepositorySettings['contentDimensions'], $options['contentDimensions'] ?? []);
         }
         return $contentDimensionSourceFactory->build($contentRepositoryId, $options);
-
     }
 
     /** @param array<string, mixed> $contentRepositorySettings */
