96c427b41a3a4ab23d64d94032c574d56bee4610
Bastian Waidelich <b.waidelich@wwwision.de>
2024-09-27 10:44:47 +0200

    Remove UI specific publishable node info array from `Changes` VO

--- Neos.Neos/Classes/PendingChangesProjection/Changes.php
+++ Neos.Neos/Classes/PendingChangesProjection/Changes.php
@@ -14,16 +14,7 @@ declare(strict_types=1);
 
 namespace Neos\Neos\PendingChangesProjection;
 
-use Neos\ContentRepository\Core\ContentRepository;
-use Neos\ContentRepository\Core\Projection\ContentGraph\Filter\FindClosestNodeFilter;
-use Neos\ContentRepository\Core\Projection\ContentGraph\Node;
-use Neos\ContentRepository\Core\Projection\ContentGraph\VisibilityConstraints;
-use Neos\ContentRepository\Core\SharedModel\Workspace\WorkspaceName;
-use Neos\ContentRepositoryRegistry\ContentRepositoryRegistry;
 use Neos\Flow\Annotations as Flow;
-use Neos\Neos\Domain\Service\NodeTypeNameFactory;
-use Neos\Neos\FrontendRouting\NodeAddress;
-use Neos\Neos\FrontendRouting\NodeAddressFactory;
 
 /**
  * Read model for a set of pending changes
@@ -34,12 +25,6 @@ use Neos\Neos\FrontendRouting\NodeAddressFactory;
  */
 final readonly class Changes implements \IteratorAggregate, \Countable
 {
-    private const NODE_HAS_BEEN_CREATED = 0b0001;
-    private const NODE_HAS_BEEN_CHANGED = 0b0010;
-    private const NODE_HAS_BEEN_MOVED = 0b0100;
-    private const NODE_HAS_BEEN_DELETED = 0b1000;
-
-
     /**
      * @param list<Change> $changes
      */
@@ -59,82 +44,6 @@ final readonly class Changes implements \IteratorAggregate, \Countable
             }
         }
         return new self($changes);
-    }
-
-    /**
-     * @deprecated
-     * @return list<array{contextPath:string,documentContextPath:string,typeOfChange:int}>
-     */
-    public function toPublishableNodeInfo(ContentRepository $contentRepository, WorkspaceName $workspaceName): array
-    {
-        /** @var array{contextPath:string,documentContextPath:string,typeOfChange:int}[] $unpublishedNodes */
-        $unpublishedNodes = [];
-        foreach ($this->changes as $change) {
-            if ($change->removalAttachmentPoint) {
-                $nodeAddress = new NodeAddress(
-                    $change->contentStreamId,
-                    $change->originDimensionSpacePoint->toDimensionSpacePoint(),
-                    $change->nodeAggregateId,
-                    $workspaceName
-                );
-
-                /**
-                 * See {@see Remove::apply} -> Removal Attachment Point == closest document node.
-                 */
-                $documentNodeAddress = new NodeAddress(
-                    $change->contentStreamId,
-                    $change->originDimensionSpacePoint->toDimensionSpacePoint(),
-                    $change->removalAttachmentPoint,
-                    $workspaceName
-                );
-
-                $unpublishedNodes[] = [
-                    'contextPath' => $nodeAddress->serializeForUri(),
-                    'documentContextPath' => $documentNodeAddress->serializeForUri(),
-                    'typeOfChange' => self::getTypeOfChange($change)
-                ];
-            } else {
-                $subgraph = $contentRepository->getContentGraph($workspaceName)->getSubgraph(
-                    $change->originDimensionSpacePoint->toDimensionSpacePoint(),
-                    VisibilityConstraints::withoutRestrictions()
-                );
-                $node = $subgraph->findNodeById($change->nodeAggregateId);
-
-                if ($node instanceof Node) {
-                    $documentNode = $subgraph->findClosestNode($node->aggregateId, FindClosestNodeFilter::create(nodeTypes: NodeTypeNameFactory::NAME_DOCUMENT));
-                    if ($documentNode instanceof Node) {
-                        $nodeAddressFactory = NodeAddressFactory::create($contentRepository);
-                        $unpublishedNodes[] = [
-                            'contextPath' => $nodeAddressFactory->createFromNode($node)->serializeForUri(),
-                            'documentContextPath' => $nodeAddressFactory->createFromNode($documentNode)
-                                ->serializeForUri(),
-                            'typeOfChange' => self::getTypeOfChange($change)
-                        ];
-                    }
-                }
-            }
-        }
-        return array_values(array_filter($unpublishedNodes, function ($item) {
-            return (bool)$item;
-        }));
-    }
-
-    private static function getTypeOfChange(Change $change): int
-    {
-        $result = 0;
-        if ($change->created) {
-            $result |= self::NODE_HAS_BEEN_CREATED;
-        }
-        if ($change->changed) {
-            $result |= self::NODE_HAS_BEEN_CHANGED;
-        }
-        if ($change->moved) {
-            $result |= self::NODE_HAS_BEEN_MOVED;
-        }
-        if ($change->deleted) {
-            $result |= self::NODE_HAS_BEEN_DELETED;
-        }
-        return $result;
     }
 
     /**
