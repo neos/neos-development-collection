0c382442e0c409a0c26ad5d7996a298e8c60313f
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-10-08 17:39:06 +0200

    BUGFIX: Prevent augmenter from applying data of multiple nodes into the same element
    9.0 port of https://github.com/neos/neos-ui/pull/3856
    
    Original Commit MSG:
    
    This fixes a regression introduced in #b56135a01ecf59ae3a4990e3fd54ac766732e0e6 which
    removed the script tag, causing the augmenter to add the data of multiple nodes into the same
    html element in certain cases instead of adding an outer div.
    
    With this change this behaviour is now more explicit instead of relying on the inner workings
    of the augmenter.

--- Neos.Neos/Classes/Service/ContentElementWrappingService.php
+++ Neos.Neos/Classes/Service/ContentElementWrappingService.php
@@ -81,7 +81,15 @@ class ContentElementWrappingService
         $attributes['data-__neos-fusion-path'] = $fusionPath;
         $attributes['data-__neos-node-contextpath'] = $nodeAddress->serializeForUri();
 
-        return $this->htmlAugmenter->addAttributes($content, $attributes, 'div');
+        // Define all attribute names as exclusive via the `exclusiveAttributes` parameter, to prevent the data of
+        // two different nodes to be concatenated into the attributes of a single html node.
+        // This way an outer div is added, if the wrapped content already has node related data-attributes set.
+        return $this->htmlAugmenter->addAttributes(
+            $content,
+            $attributes,
+            'div',
+            array_keys($attributes)
+        );
     }
 
     /**
