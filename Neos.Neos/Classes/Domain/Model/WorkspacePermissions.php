904c716bb4bb5e62f963a98e74f635956ba252ba
Bastian Waidelich <b.waidelich@wwwision.de>
2024-10-08 16:09:00 +0200

    `WorkspacePermissions` doc comments

--- Neos.Neos/Classes/Domain/Model/WorkspacePermissions.php
+++ Neos.Neos/Classes/Domain/Model/WorkspacePermissions.php
@@ -7,18 +7,22 @@ namespace Neos\Neos\Domain\Model;
 use Neos\Flow\Annotations as Flow;
 
 /**
+ * Calculated permissions a specific user has on a workspace
+ *
+ * - read: Permission to read data from the corresponding workspace (e.g. get hold of and traverse the content graph)
+ * - write: Permission to write to the corresponding workspace, including publishing a derived workspace to it
+ * - manage: Permission to change the metadata and roles of the corresponding workspace (e.g. change description/title or add/remove workspace roles)
+ *
  * @api
  */
 #[Flow\Proxy(false)]
 final readonly class WorkspacePermissions
 {
-    private function __construct(
-        public bool $read,
-        public bool $write,
-        public bool $manage,
-    ) {
-    }
-
+    /**
+     * @param bool $read Permission to read data from the corresponding workspace (e.g. get hold of and traverse the content graph)
+     * @param bool $write Permission to write to the corresponding workspace, including publishing a derived workspace to it
+     * @param bool $manage Permission to change the metadata and roles of the corresponding workspace (e.g. change description/title or add/remove workspace roles)
+     */
     public static function create(
         bool $read,
         bool $write,
@@ -27,13 +31,20 @@ final readonly class WorkspacePermissions
         return new self($read, $write, $manage);
     }
 
+    /**
+     * @param bool $read Permission to read data from the corresponding workspace (e.g. get hold of and traverse the content graph)
+     * @param bool $write Permission to write to the corresponding workspace, including publishing a derived workspace to it
+     * @param bool $manage Permission to change the metadata and roles of the corresponding workspace (e.g. change description/title or add/remove workspace roles)
+     */
+    private function __construct(
+        public bool $read,
+        public bool $write,
+        public bool $manage,
+    ) {
+    }
+
     public static function all(): self
     {
         return new self(true, true, true);
-    }
-
-    public static function none(): self
-    {
-        return new self(false, false, false);
     }
 }
