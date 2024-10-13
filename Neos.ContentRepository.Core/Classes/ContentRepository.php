b0cb5be93e2251856f4d5635557ee08b5ce411e9
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-10-13 19:29:17 +0200

    TASK: Introduce `ContentRepository::catchupProjections`
    ... and remove `CatchUpTriggerWithSynchronousOption` which is currently hardcoded to `true`

--- Neos.ContentRepository.Core/Classes/ContentRepository.php
+++ Neos.ContentRepository.Core/Classes/ContentRepository.php
@@ -200,6 +200,15 @@ final class ContentRepository
         $catchUpHook?->onAfterCatchUp();
     }
 
+    public function catchupProjections(): void
+    {
+        foreach ($this->projectionsAndCatchUpHooks->projections as $projection) {
+            // FIXME optimise by only loading required events once and not per projection
+            // see https://github.com/neos/neos-development-collection/pull/4988/
+            $this->catchUpProjection($projection::class, CatchUpOptions::create());
+        }
+    }
+
     public function setUp(): void
     {
         $this->eventStore->setup();
