cf1273c006daeed0f754675e05f64be1e2e26259
Bastian Waidelich <b.waidelich@wwwision.de>
2024-10-17 18:15:47 +0200

    Fix ContentRepositoryAuthProviderFactory

--- Neos.Neos/Classes/Security/ContentRepositoryAuthProvider/ContentRepositoryAuthProviderFactory.php
+++ Neos.Neos/Classes/Security/ContentRepositoryAuthProvider/ContentRepositoryAuthProviderFactory.php
@@ -7,6 +7,8 @@ namespace Neos\Neos\Security\ContentRepositoryAuthProvider;
 use Neos\ContentRepository\Core\SharedModel\ContentRepository\ContentRepositoryId;
 use Neos\ContentRepositoryRegistry\Factory\AuthProvider\AuthProviderFactoryInterface;
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Security\Context as SecurityContext;
+use Neos\Flow\Security\Policy\PolicyService;
 use Neos\Neos\Domain\Service\UserService;
 use Neos\Neos\Domain\Service\WorkspaceService;
 
@@ -16,11 +18,13 @@ use Neos\Neos\Domain\Service\WorkspaceService;
  * @api
  */
 #[Flow\Scope('singleton')]
-final class ContentRepositoryAuthProviderFactory implements AuthProviderFactoryInterface
+final readonly class ContentRepositoryAuthProviderFactory implements AuthProviderFactoryInterface
 {
     public function __construct(
-        private readonly UserService $userService,
-        private readonly WorkspaceService $workspaceService,
+        private UserService $userService,
+        private WorkspaceService $workspaceService,
+        private SecurityContext $securityContext,
+        private PolicyService $policyService,
     ) {
     }
 
@@ -29,6 +33,6 @@ final class ContentRepositoryAuthProviderFactory implements AuthProviderFactoryI
      */
     public function build(ContentRepositoryId $contentRepositoryId, array $options): ContentRepositoryAuthProvider
     {
-        return new ContentRepositoryAuthProvider($contentRepositoryId, $this->userService, $this->workspaceService);
+        return new ContentRepositoryAuthProvider($contentRepositoryId, $this->userService, $this->workspaceService, $this->securityContext, $this->policyService);
     }
 }
