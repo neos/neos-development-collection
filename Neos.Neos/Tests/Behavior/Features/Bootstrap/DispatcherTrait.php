08193796b7ecf810b39b32dd34c17553b2f9e29f
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-10-20 14:11:36 +0200

    TASK: Introduce full behat test for FrontendNodeController

---
+++ Neos.Neos/Tests/Behavior/Features/Bootstrap/DispatcherTrait.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Neos.ContentRepository package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Behat\Gherkin\Node\PyStringNode;
+use GuzzleHttp\Psr7\Message;
+use Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\CRTestSuiteRuntimeVariables;
+use Neos\Fusion\Core\Cache\ContentCache;
+use Neos\Neos\Domain\Service\FusionService;
+use PHPUnit\Framework\Assert;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+
+/**
+ * @internal only for behat tests within the Neos.Neos package
+ */
+trait DispatcherTrait
+{
+    use CRTestSuiteRuntimeVariables;
+
+    private ResponseInterface|null $response = null;
+
+    /**
+     * @template T of object
+     * @param class-string<T> $className
+     *
+     * @return T
+     */
+    abstract private function getObject(string $className): object;
+
+    /**
+     * @BeforeScenario
+     */
+    public function setupDispatcherTest(): void
+    {
+        $this->getObject(ContentCache::class)->flush();
+        $this->response = null;
+    }
+
+    /**
+     * @When the sites Fusion code is:
+     */
+    public function iHaveTheFollowingFusionCodeForTheSite(PyStringNode $fusionCode)
+    {
+        $this->getObject(
+            FusionService::class
+        )->unsafeSetAdditionalFusionSourceCodeToThisSingleton(
+            $fusionCode->getRaw()
+        );
+        // $fakeFusionService = new class ($original) extends \Neos\Neos\Domain\Service\FusionService
+        // {
+        //     public function __construct(
+        //         private \Neos\Neos\Domain\Service\FusionService $original,
+        //         private \Neos\Fusion\Core\FusionSourceCode $additionalFusion
+        //     ) {
+        //     }
+        //     public function createFusionConfigurationFromSite(\Neos\Neos\Domain\Model\Site $site): \Neos\Fusion\Core\FusionConfiguration
+        //     {
+        //         $this->original->createFusionConfigurationFromSite($site)-> ... doest work
+        //     }
+        // };
+    }
+
+    /**
+     * @When I dispatch the following request :requestUri
+     */
+    public function iDispatchTheFollowingRequest(string $requestUri)
+    {
+        $httpRequest = $this->getObject(ServerRequestFactoryInterface::class)->createServerRequest('GET', $requestUri);
+
+        $this->response = $this->getObject(\Neos\Flow\Http\Middleware\MiddlewaresChain::class)->handle(
+            $httpRequest
+        );
+    }
+
+    /**
+     * @Then I expect the following response header:
+     */
+    public function iExpectTheFollowingResponseHeader(PyStringNode $expectedResult): void
+    {
+        Assert::assertNotNull($this->response);
+        Assert::assertSame($expectedResult->getRaw(), $this->response->getBody()->getContents());
+    }
+
+    /**
+     * @Then I expect the following response:
+     */
+    public function iExpectTheFollowingResponse(PyStringNode $expectedResult): void
+    {
+        Assert::assertNotNull($this->response);
+        Assert::assertEquals($expectedResult->getRaw(), str_replace("\r\n", "\n", Message::toString($this->response)));
+    }
+}
