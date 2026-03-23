22eb64f7c91b579881d7bfc00320e602a6234678
Christian Müller <christian@flownative.com>
2026-03-23 14:44:54 +0100

    Remove unused use statement

--- Neos.Neos/Classes/Package.php
+++ Neos.Neos/Classes/Package.php
@@ -30,7 +30,6 @@ use Neos\Neos\Utility\NodeUriPathSegmentGenerator;
 use Neos\ContentRepository\Domain\Model\Node;
 use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\ContentRepository\Domain\Model\Workspace;
-use Neos\ContentRepository\Domain\Repository\NodeDataRepository;
 use Neos\ContentRepository\Domain\Service\Context;
 use Neos\Fusion\Core\Cache\ContentCache;
 
