fb77b69972c7bd1982114ecda3b533eba6a6bc7f
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2026-05-02 10:07:25 +0200

    TASK: Remove unused the event RootNodeAggregateWithNodeWasCreated|NodeAggregateWasRemoved was published
    The change https://github.com/neos/neos-development-collection/pull/5810 allows `theEventRootNodeAggregateWithNodeWasCreatedWasPublishedToStreamWithPayload` and `theEventNodeAggregateWasRemovedWasPublishedWithPayload` to be removed.

--- Neos.ContentRepository.TestSuite/Classes/Behavior/Features/Bootstrap/CRTestSuiteTrait.php
+++ Neos.ContentRepository.TestSuite/Classes/Behavior/Features/Bootstrap/CRTestSuiteTrait.php
@@ -38,7 +38,6 @@ use Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\Features\NodeCr
 use Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\Features\NodeModification;
 use Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\Features\NodeMove;
 use Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\Features\NodeReferencing;
-use Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\Features\NodeRemoval;
 use Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\Features\NodeRenaming;
 use Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\Features\SubtreeTagging;
 use Neos\ContentRepository\TestSuite\Behavior\Features\Bootstrap\Features\WorkspaceCreation;
@@ -65,7 +64,6 @@ trait CRTestSuiteTrait
     use NodeModification;
     use NodeMove;
     use NodeReferencing;
-    use NodeRemoval;
     use NodeRenaming;
 
     use WorkspaceCreation;
