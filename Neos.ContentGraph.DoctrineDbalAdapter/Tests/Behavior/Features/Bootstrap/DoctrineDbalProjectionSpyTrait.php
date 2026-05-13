51995f858f05bfc92db7fbf0a5ad736b0ec39708
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2026-05-13 12:49:42 +0200

    TASK: Provide tests that subtree tag moving does not create obsolete edges

---
+++ Neos.ContentGraph.DoctrineDbalAdapter/Tests/Behavior/Features/Bootstrap/DoctrineDbalProjectionSpyTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\Assert;
+
+/**
+ * @internal custom non api inspections for the Doctrine DBAL content graph adapter to make assertions
+ */
+trait DoctrineDbalProjectionSpyTrait
+{
+    /**
+     * @Then I expect :number hierarchies to exist in the active write layer
+     */
+    public function iExpectNumberHierarchiesToExistInTheActiveWriteLayer(int $number)
+    {
+        $contentGraph = $this->currentContentRepository->getContentGraph($this->currentWorkspaceName);
+
+        $layers = $this->getContentStreamLayers($contentGraph->getContentStreamId());
+
+        $count = $this->dbal->fetchOne(<<<SQL
+        SELECT COUNT(*) FROM {$this->tableNames()->hierarchyRelation()} WHERE contentStreamLayer = :contentStreamLayer
+        SQL, ['contentStreamLayer' => $layers->getWriteLayer()->value]);
+
+        Assert::assertEquals($number, $count);
+    }
+}
