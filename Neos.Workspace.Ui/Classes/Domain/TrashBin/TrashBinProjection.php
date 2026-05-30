6d7a96e1b9d6f316a4deac1560c927a8aa2b0088
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2026-05-30 16:05:54 +0200

    TASK: Use bound parameter for `as` instead of quote hack

--- Neos.Workspace.Ui/Classes/Domain/TrashBin/TrashBinProjection.php
+++ Neos.Workspace.Ui/Classes/Domain/TrashBin/TrashBinProjection.php
@@ -359,7 +359,7 @@ class TrashBinProjection implements ProjectionInterface
                 affected_dimension_space_points
             )
             SELECT
-                '{$workspaceName->value}' AS workspace_name,
+                :workspaceName AS workspace_name,
                 i.node_aggregate_id,
                 i.affected_dimension_space_points_hash,
                 i.user_id,
@@ -372,6 +372,7 @@ class TrashBinProjection implements ProjectionInterface
         $this->dbal->executeStatement(
             $copyStatement,
             [
+                'workspaceName' => $workspaceName->value,
                 'baseWorkspaceName' => $baseWorkspaceName->value,
             ]
         );
