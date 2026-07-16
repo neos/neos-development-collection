2faa3b3d4c86824012e8004edf37756b1283df9f
Benjamin Klix <benjamin.klix@die-wegmeister.com>
2026-07-16 10:26:17 +0200

    feat: Update frontend build of Neos.Media.Browser
    - Remove old Grunt and webpack build and replace it with new esbuild setup.
    - Remove/replace Ruby files (Gemfile, config.rb)
    - Combine Main.js and Vendor.js file into one file.
    - Only use yarn.lock, remove package-lock.json

---
+++ Neos.Media.Browser/esbuild.js
@@ -0,0 +1,103 @@
+const esbuild = require("esbuild");
+const path = require("path");
+const fs = require("fs");
+const { sassPlugin } = require("esbuild-sass-plugin");
+
+const projectRoot = __dirname;
+const isWatch = process.argv.includes("--watch");
+
+const moveMainCssPlugin = {
+	name: "move-main-css",
+	setup(build) {
+		build.onEnd(() => {
+			const fromPath = path.join(
+				projectRoot,
+				"Resources/Public/JavaScript/Main.css",
+			);
+			const fromMapPath = path.join(
+				projectRoot,
+				"Resources/Public/JavaScript/Main.css.map",
+			);
+			const toPath = path.join(projectRoot, "Resources/Public/Styles/Main.css");
+			const toMapPath = path.join(
+				projectRoot,
+				"Resources/Public/Styles/Main.css.map",
+			);
+
+			if (fs.existsSync(fromPath)) {
+				fs.mkdirSync(path.dirname(toPath), { recursive: true });
+				fs.renameSync(fromPath, toPath);
+			}
+
+			if (fs.existsSync(fromMapPath)) {
+				fs.mkdirSync(path.dirname(toMapPath), { recursive: true });
+				fs.renameSync(fromMapPath, toMapPath);
+			}
+		});
+	},
+};
+
+const jsConfig = {
+	entryPoints: ["packages/neos-media-browser/src/index.js"],
+	bundle: true,
+	outfile: path.join(projectRoot, "Resources/Public/JavaScript/Main.js"),
+	format: "iife",
+	platform: "browser",
+	target: ["es2018"],
+	sourcemap: true,
+	minify: true,
+	loader: {
+		".js": "jsx",
+		".vanilla-css": "css",
+	},
+	plugins: [moveMainCssPlugin],
+};
+
+const cssConfig = {
+	entryPoints: ["Resources/Private/Styles/MediaBrowser.scss"],
+	bundle: true,
+	outfile: path.join(projectRoot, "Resources/Public/Styles/MediaBrowser.css"),
+	platform: "browser",
+	target: ["es2018"],
+	sourcemap: false,
+	minify: true,
+	external: [
+		"../Fonts/*",
+		"../../Neos.Neos/*",
+		"*.eot",
+		"*.woff",
+		"*.woff2",
+		"*.ttf",
+		"*.svg",
+		"*.gif",
+	],
+	plugins: [
+		sassPlugin({
+			loadPaths: [
+				path.join(projectRoot, "node_modules"),
+				path.join(projectRoot, "node_modules/compass-mixins/lib"),
+			],
+		}),
+	],
+};
+
+async function build() {
+	if (isWatch) {
+		const [jsContext, cssContext] = await Promise.all([
+			esbuild.context(jsConfig),
+			esbuild.context(cssConfig),
+		]);
+
+		await Promise.all([jsContext.watch(), cssContext.watch()]);
+
+		console.log("Watching JS and SCSS assets with esbuild...");
+		return;
+	}
+
+	await Promise.all([esbuild.build(jsConfig), esbuild.build(cssConfig)]);
+}
+
+build().catch((error) => {
+	console.error(error);
+	process.exit(1);
+});
