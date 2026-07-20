196a0180fc9b67131b65c4faf30bd52e7f86df8e
Benjamin Klix <benjamin.klix@die-wegmeister.com>
2026-07-20 15:42:15 +0200

    TASK: Apply coding styles from .editorconfig to Neos.Media.Browser js files

--- Neos.Media.Browser/packages/neos-media-browser/src/Variants/ImageCropper.js
+++ Neos.Media.Browser/packages/neos-media-browser/src/Variants/ImageCropper.js
@@ -1,17 +1,19 @@
-import React, {PureComponent} from 'react';
-import ReactCrop from 'react-image-crop';
-import './react_crop.vanilla-css';
+import React, { PureComponent } from "react";
+import ReactCrop from "react-image-crop";
+import "./react_crop.vanilla-css";
 
 export default class ImageCropper extends PureComponent {
-    render() {
-        const {src, cropConfiguration, onComplete} = this.props;
+	render() {
+		const { src, cropConfiguration, onComplete } = this.props;
 
-        return (<ReactCrop
-            src={src}
-            crop={cropConfiguration}
-            onComplete={onComplete}
-            onAspectRatioChange={onComplete}
-            onImageLoaded={onComplete}
-        />);
-    }
+		return (
+			<ReactCrop
+				src={src}
+				crop={cropConfiguration}
+				onComplete={onComplete}
+				onAspectRatioChange={onComplete}
+				onImageLoaded={onComplete}
+			/>
+		);
+	}
 }
--- Neos.Media.Browser/packages/neos-media-browser/src/index.js
+++ Neos.Media.Browser/packages/neos-media-browser/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import VariantsApp from "./Variants/index";
 
-const variantsInformation = JSON.parse(document.getElementById('variants-information').innerHTML);
-const originalInformation = JSON.parse(document.getElementById('original-information').innerHTML);
+const variantsInformation = JSON.parse(
+	document.getElementById("variants-information").innerHTML,
+);
+const originalInformation = JSON.parse(
+	document.getElementById("original-information").innerHTML,
+);
 
-ReactDOM.render(<VariantsApp variantsInformation={variantsInformation} originalInformation={originalInformation} />, document.getElementById('variants-app'));
+ReactDOM.render(
+	<VariantsApp
+		variantsInformation={variantsInformation}
+		originalInformation={originalInformation}
+	/>,
+	document.getElementById("variants-app"),
+);
