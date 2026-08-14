9406c500551990b18118e4c13f8ba25d84e9529c
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2026-08-14 12:43:10 +0200

    TASK: Use simplified English for "Normalise" spelling

--- Neos.ContentRepository.Core/Classes/EventStore/EventAugmenter.php
+++ Neos.ContentRepository.Core/Classes/EventStore/EventAugmenter.php
@@ -8,10 +8,10 @@ use Neos\ContentRepository\Core\CommandHandler\CommandInterface;
 use Neos\ContentRepository\Core\Feature\Security\AuthProviderInterface;
 use Neos\ContentRepository\Core\Feature\Security\Dto\UserId;
 use Neos\EventStore\Model\Event\CorrelationId;
-use Neos\EventStore\Model\Events as NormalisedEvents;
-use Neos\EventStore\Model\EventsForCommit as NormalisedEventsForCommit;
-use Neos\EventStore\Model\EventsForStream as NormalisedEventsForStream;
-use Neos\EventStore\Model\EventsForStreams as NormalisedEventsForStreams;
+use Neos\EventStore\Model\Events as NormalizedEvents;
+use Neos\EventStore\Model\EventsForCommit as NormalizedEventsForCommit;
+use Neos\EventStore\Model\EventsForStream as NormalizedEventsForStream;
+use Neos\EventStore\Model\EventsForStreams as NormalizedEventsForStreams;
 use Neos\EventStore\Model\EventStream\ExpectedStreamConstraints;
 use Psr\Clock\ClockInterface;
 
@@ -35,27 +35,27 @@ final readonly class EventAugmenter
         return CorrelationId::fromString(sprintf('%s_%s', substr($commandClassName, strrpos($commandClassName, '\\') + 1, 20), bin2hex(random_bytes(9))));
     }
 
-    public function augmentAndNormalizeEventsToPublish(EventsToPublish $eventsToPublish, CorrelationId $correlationId): NormalisedEventsForCommit
+    public function augmentAndNormalizeEventsToPublish(EventsToPublish $eventsToPublish, CorrelationId $correlationId): NormalizedEventsForCommit
     {
         $initiatingUserId = $this->authProvider->getAuthenticatedUserId() ?? UserId::forSystemUser();
         $initiatingTimestamp = $this->clock->now();
 
-        $normalisedEventsForStreams = [];
+        $normalizedEventsForStreams = [];
 
         foreach ($eventsToPublish->eventsForStreams as $eventsForStream) {
-            $normalisedEventsForStreams[] = NormalisedEventsForStream::create(
+            $normalizedEventsForStreams[] = NormalizedEventsForStream::create(
                 streamName: $eventsForStream->streamName,
                 events: $this->enrichAndNormalizeEvents($eventsForStream->events, $initiatingUserId, $initiatingTimestamp, $correlationId)
             );
         }
 
-        return NormalisedEventsForCommit::create(
-            NormalisedEventsForStreams::create(...$normalisedEventsForStreams),
+        return NormalizedEventsForCommit::create(
+            NormalizedEventsForStreams::create(...$normalizedEventsForStreams),
             ExpectedStreamConstraints::create(...$eventsToPublish->expectedStreamConstraints)
         );
     }
 
-    private function enrichAndNormalizeEvents(Events $events, UserId $initiatingUserId, \DateTimeImmutable $initiatingTimestamp, CorrelationId $correlationId): NormalisedEvents
+    private function enrichAndNormalizeEvents(Events $events, UserId $initiatingUserId, \DateTimeImmutable $initiatingTimestamp, CorrelationId $correlationId): NormalizedEvents
     {
         $eventsWithMetaData = InitiatingEventMetadata::enrichEventsWithInitiatingMetadata(
             $events,
@@ -63,7 +63,7 @@ final readonly class EventAugmenter
             $initiatingTimestamp
         );
 
-        return NormalisedEvents::fromArray($eventsWithMetaData->map(function (EventInterface|DecoratedEvent $event) use ($correlationId) {
+        return NormalizedEvents::fromArray($eventsWithMetaData->map(function (EventInterface|DecoratedEvent $event) use ($correlationId) {
             $decoratedEvent = DecoratedEvent::create($event, correlationId: $correlationId);
             return $this->eventNormalizer->normalize($decoratedEvent);
         }));
