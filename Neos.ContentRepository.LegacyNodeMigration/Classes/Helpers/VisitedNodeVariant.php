cba1df04fd9944471042a9ab31a6605b850b428c
Christian Müller <christian@flownative.com>
2026-09-12 14:03:58 +0200

    Fix linting errors for 9.2

--- Neos.ContentRepository.LegacyNodeMigration/Classes/Helpers/VisitedNodeVariant.php
+++ Neos.ContentRepository.LegacyNodeMigration/Classes/Helpers/VisitedNodeVariant.php
@@ -15,7 +15,6 @@ use Neos\Flow\Annotations as Flow;
  */
 final readonly class VisitedNodeVariant
 {
-
     /**
      * @param DimensionSpacePointSet $claimedDimensionSpacePoints The dimension space points the exported creation/variation event of this variant covers
      */
@@ -25,5 +24,6 @@ final readonly class VisitedNodeVariant
         // the property names this variant holds; a variant created from it copies exactly these
         public PropertyNames $propertyNames,
         public DimensionSpacePointSet $claimedDimensionSpacePoints
-    ) {}
+    ) {
+    }
 }
